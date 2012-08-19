varshithkonakanchi/django

commit 9436665e304096853aed27d577ab90e84e037cdd

[py3] Fixed regression introduced in 09cbecfa8e.
Refs #18764.

Reverted 09cbecfa8e and switched to a more explicit way of avoiding
calling bytes(<int>).

This definitely deserves a refactoring. Specifically, _get_content
should just return b''.join(self). Unfortunately that's impossible
with the current tests.

django/http/__init__.py
@@ -85,7 +85,7 @@ from django.core.files import uploadhandler
 from django.http.multipartparser import MultiPartParser
 from django.http.utils import *
 from django.utils.datastructures import MultiValueDict, ImmutableList
-from django.utils.encoding import force_bytes, force_text, iri_to_uri, smart_bytes, smart_str
+from django.utils.encoding import smart_bytes, smart_str, iri_to_uri, force_text
 from django.utils.http import cookie_date
 from django.utils import six
 from django.utils import timezone
@@ -670,12 +670,16 @@ class HttpResponse(object):
                         expires='Thu, 01-Jan-1970 00:00:00 GMT')
 
     def _get_content(self):
-        # The logic below obeys the following constraints:
-        # - do not perform any encoding if a Content-Encoding is set (#4969)
-        # - force string conversion of non-string types (#16494)
-        # - avoid simply calling bytes() with Python 3 (#18764)
-        encoding = 'ascii' if self.has_header('Content-Encoding') else self._charset
-        return b''.join(force_bytes(e, encoding) for e in self._container)
+        if self.has_header('Content-Encoding'):
+            def make_bytes(value):
+                if isinstance(value, int):
+                    return six.text_type(value).encode()
+                elif isinstance(value, six.text_type):
+                    return value.encode('ascii')
+                else:
+                    return bytes(value)
+            return b''.join(make_bytes(e) for e in self._container)
+        return b''.join(smart_bytes(e, self._charset) for e in self._container)
 
     def _set_content(self, value):
         if hasattr(value, '__iter__') and not isinstance(value, (bytes, six.string_types)):
@@ -692,9 +696,12 @@ class HttpResponse(object):
         return self
 
     def __next__(self):
-        # Use the same logic as _get_content.
-        encoding = 'ascii' if self.has_header('Content-Encoding') else self._charset
-        return force_bytes(next(self._iterator), encoding)
+        chunk = next(self._iterator)
+        if isinstance(chunk, int):
+            return six.text_type(chunk).encode()
+        if isinstance(chunk, six.text_type):
+            return chunk.encode(self._charset)
+        return bytes(chunk)
 
     next = __next__             # Python 2 compatibility
 

commit fb7c7a15e06a3b09fe9ff1956da99db97377f0d3

[py3] Fixed another regression from 9436665e30.

django/http/__init__.py
@@ -673,11 +673,11 @@ class HttpResponse(object):
         if self.has_header('Content-Encoding'):
             def make_bytes(value):
                 if isinstance(value, int):
-                    return six.text_type(value).encode()
-                elif isinstance(value, six.text_type):
-                    return value.encode('ascii')
-                else:
-                    return bytes(value)
+                    value = six.text_type(value)
+                if isinstance(value, six.text_type):
+                    value = value.encode('ascii')
+                # force conversion to bytes in case chunk is a subclass
+                return bytes(value)
             return b''.join(make_bytes(e) for e in self._container)
         return b''.join(smart_bytes(e, self._charset) for e in self._container)
 
@@ -698,9 +698,10 @@ class HttpResponse(object):
     def __next__(self):
         chunk = next(self._iterator)
         if isinstance(chunk, int):
-            return six.text_type(chunk).encode()
+            chunk = six.text_type(chunk)
         if isinstance(chunk, six.text_type):
-            return chunk.encode(self._charset)
+            chunk = chunk.encode(self._charset)
+        # force conversion to bytes in case chunk is a subclass
         return bytes(chunk)
 
     next = __next__             # Python 2 compatibility