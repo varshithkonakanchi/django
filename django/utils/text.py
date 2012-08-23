varshithkonakanchi/django

commit 79d93140273294eb8acb31caff126615cdcb5235

[py3] Removed duplicate imports.
Fixed #18837. Refs #18791.

django/utils/text.py
@@ -1,11 +1,9 @@
 from __future__ import unicode_literals
 
 import re
-from django.utils import six
 import unicodedata
 import warnings
 from gzip import GzipFile
-from django.utils.six.moves import html_entities
 from io import BytesIO
 
 from django.utils.encoding import force_text
@@ -15,6 +13,7 @@ if not six.PY3:
     from django.utils.encoding import force_unicode
 from django.utils.functional import allow_lazy, SimpleLazyObject
 from django.utils import six
+from django.utils.six.moves import html_entities
 from django.utils.translation import ugettext_lazy, ugettext as _, pgettext
 from django.utils.safestring import mark_safe
 

django/contrib/gis/db/models/query.py
@@ -1,15 +1,14 @@
 from django.db import connections
 from django.db.models.query import QuerySet, ValuesQuerySet, ValuesListQuerySet
-from django.utils import six
 
 from django.contrib.gis.db.models import aggregates
 from django.contrib.gis.db.models.fields import get_srid_info, PointField, LineStringField
 from django.contrib.gis.db.models.sql import AreaField, DistanceField, GeomField, GeoQuery
 from django.contrib.gis.geometry.backend import Geometry
 from django.contrib.gis.measure import Area, Distance
-from django.utils import six
 
 from django.utils import six
+
 
 class GeoQuerySet(QuerySet):
     "The Geographic QuerySet."

django/db/backends/oracle/base.py
@@ -10,8 +10,6 @@ import decimal
 import sys
 import warnings
 
-from django.utils import six
-
 def _setup_environment(environ):
     import platform
     # Cygwin requires some special voodoo to set the environment variables

commit f6fe491c080b6059aefae13a11ba1044c7560e35

Reordered imports
to avoid an error introduced in 79d9314027.

django/utils/text.py
@@ -7,15 +7,16 @@ from gzip import GzipFile
 from io import BytesIO
 
 from django.utils.encoding import force_text
-if not six.PY3:
-    # Import force_unicode even though this module doesn't use it, because some
-    # people rely on it being here.
-    from django.utils.encoding import force_unicode
 from django.utils.functional import allow_lazy, SimpleLazyObject
 from django.utils import six
 from django.utils.six.moves import html_entities
 from django.utils.translation import ugettext_lazy, ugettext as _, pgettext
 from django.utils.safestring import mark_safe
+
+if not six.PY3:
+    # Import force_unicode even though this module doesn't use it, because some
+    # people rely on it being here.
+    from django.utils.encoding import force_unicode
 
 # Capitalizes the first letter of a string.
 capfirst = lambda x: x and force_text(x)[0].upper() + force_text(x)[1:]