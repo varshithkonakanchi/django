varshithkonakanchi/django

commit 6471c273fc7f94a9186f58f38375fbf3f77a765b

Fixed #10494 -- Added kwargs to QuerySet.get() error message in the case no objects were found.
Thanks brondsem for the report, Szymon Pyzalski for the patch and oinopion for review.


git-svn-id: http://code.djangoproject.com/svn/django/trunk@17917 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/db/models/query.py
@@ -363,10 +363,14 @@ class QuerySet(object):
         if num == 1:
             return clone._result_cache[0]
         if not num:
-            raise self.model.DoesNotExist("%s matching query does not exist."
-                    % self.model._meta.object_name)
-        raise self.model.MultipleObjectsReturned("get() returned more than one %s -- it returned %s! Lookup parameters were %s"
-                % (self.model._meta.object_name, num, kwargs))
+            raise self.model.DoesNotExist(
+                "%s matching query does not exist. "
+                "Lookup parameters were %s" %
+                (self.model._meta.object_name, kwargs))
+        raise self.model.MultipleObjectsReturned(
+            "get() returned more than one %s -- it returned %s! "
+            "Lookup parameters were %s" %
+            (self.model._meta.object_name, num, kwargs))
 
     def create(self, **kwargs):
         """

tests/modeltests/basic/tests.py
@@ -83,14 +83,23 @@ class ModelTest(TestCase):
         # parameters don't match any object.
         self.assertRaisesRegexp(
             ObjectDoesNotExist,
-            "Article matching query does not exist.",
+            "Article matching query does not exist. Lookup parameters were "
+            "{'id__exact': 2000}",
             Article.objects.get,
             id__exact=2000,
         )
-
+        # To avoid dict-ordering related errors check only one lookup
+        # in single assert.
         self.assertRaisesRegexp(
             ObjectDoesNotExist,
-            "Article matching query does not exist.",
+            ".*'pub_date__year': 2005.*",
+            Article.objects.get,
+            pub_date__year=2005,
+            pub_date__month=8,
+        )
+        self.assertRaisesRegexp(
+            ObjectDoesNotExist,
+            ".*'pub_date__month': 8.*",
             Article.objects.get,
             pub_date__year=2005,
             pub_date__month=8,
@@ -98,7 +107,8 @@ class ModelTest(TestCase):
 
         self.assertRaisesRegexp(
             ObjectDoesNotExist,
-            "Article matching query does not exist.",
+            "Article matching query does not exist. Lookup parameters were "
+            "{'pub_date__week_day': 6}",
             Article.objects.get,
             pub_date__week_day=6,
         )

commit 777fe25d47ecdc87fb05a7966e5853f40265fd5c

Fixed #20278 -- ensured .get() exceptions do not recurse infinitely
A regression caused by 6471c273fc7f94a9186f58f383 made .get() error
reporting recurse infinitely on certain rare conditions. Fixed this by
not trying to print the given lookup kwargs.

django/db/models/query.py
@@ -388,13 +388,11 @@ class QuerySet(object):
             return clone._result_cache[0]
         if not num:
             raise self.model.DoesNotExist(
-                "%s matching query does not exist. "
-                "Lookup parameters were %s" %
-                (self.model._meta.object_name, kwargs))
+                "%s matching query does not exist." %
+                self.model._meta.object_name)
         raise self.model.MultipleObjectsReturned(
-            "get() returned more than one %s -- it returned %s! "
-            "Lookup parameters were %s" %
-            (self.model._meta.object_name, num, kwargs))
+            "get() returned more than one %s -- it returned %s!" %
+            (self.model._meta.object_name, num))
 
     def create(self, **kwargs):
         """

tests/basic/models.py
@@ -18,3 +18,11 @@ class Article(models.Model):
 
     def __str__(self):
         return self.headline
+
+@python_2_unicode_compatible
+class SelfRef(models.Model):
+    selfref = models.ForeignKey('self', null=True, blank=True,
+                                related_name='+')
+
+    def __str__(self):
+        return SelfRef.objects.get(selfref=self).pk

tests/basic/tests.py
@@ -11,7 +11,7 @@ from django.test import TestCase, TransactionTestCase, skipIfDBFeature, skipUnle
 from django.utils import six
 from django.utils.translation import ugettext_lazy
 
-from .models import Article
+from .models import Article, SelfRef
 
 
 class ModelTest(TestCase):
@@ -87,23 +87,14 @@ class ModelTest(TestCase):
         # parameters don't match any object.
         six.assertRaisesRegex(self,
             ObjectDoesNotExist,
-            "Article matching query does not exist. Lookup parameters were "
-            "{'id__exact': 2000}",
+            "Article matching query does not exist.",
             Article.objects.get,
             id__exact=2000,
         )
         # To avoid dict-ordering related errors check only one lookup
         # in single assert.
-        six.assertRaisesRegex(self,
+        self.assertRaises(
             ObjectDoesNotExist,
-            ".*'pub_date__year': 2005.*",
-            Article.objects.get,
-            pub_date__year=2005,
-            pub_date__month=8,
-        )
-        six.assertRaisesRegex(self,
-            ObjectDoesNotExist,
-            ".*'pub_date__month': 8.*",
             Article.objects.get,
             pub_date__year=2005,
             pub_date__month=8,
@@ -111,8 +102,7 @@ class ModelTest(TestCase):
 
         six.assertRaisesRegex(self,
             ObjectDoesNotExist,
-            "Article matching query does not exist. Lookup parameters were "
-            "{'pub_date__week_day': 6}",
+            "Article matching query does not exist.",
             Article.objects.get,
             pub_date__week_day=6,
         )
@@ -688,6 +678,12 @@ class ModelTest(TestCase):
         Article.objects.create(headline='foo', pub_date=datetime.now())
         with self.assertNumQueries(0):
             self.assertEqual(len(Article.objects.none().distinct('headline', 'pub_date')), 0)
+
+    def test_ticket_20278(self):
+        sr = SelfRef.objects.create()
+        with self.assertRaises(ObjectDoesNotExist):
+            SelfRef.objects.get(selfref=sr)
+
 
 class ConcurrentSaveTests(TransactionTestCase):
     @skipUnlessDBFeature('test_db_allows_multiple_connections')