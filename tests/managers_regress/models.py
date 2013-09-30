varshithkonakanchi/django

commit a4571d64991d1011ee2dc514859e4ab4413ae740

Fixed #3871 -- Custom managers when traversing reverse relations.

django/contrib/contenttypes/generic.py
@@ -319,6 +319,23 @@ def create_generic_related_manager(superclass):
                 '%s__exact' % object_id_field_name: instance._get_pk_val(),
             }
 
+        def __call__(self, **kwargs):
+            # We use **kwargs rather than a kwarg argument to enforce the
+            # `manager='manager_name'` syntax.
+            manager = getattr(self.model, kwargs.pop('manager'))
+            manager_class = create_generic_related_manager(manager.__class__)
+            return manager_class(
+                model = self.model,
+                instance = self.instance,
+                symmetrical = self.symmetrical,
+                source_col_name = self.source_col_name,
+                target_col_name = self.target_col_name,
+                content_type = self.content_type,
+                content_type_field_name = self.content_type_field_name,
+                object_id_field_name = self.object_id_field_name,
+                prefetch_cache_name = self.prefetch_cache_name,
+            )
+
         def get_queryset(self):
             try:
                 return self.instance._prefetched_objects_cache[self.prefetch_cache_name]

django/db/models/fields/related.py
@@ -365,6 +365,92 @@ class ReverseSingleRelatedObjectDescriptor(six.with_metaclass(RenameRelatedObjec
             setattr(value, self.field.related.get_cache_name(), instance)
 
 
+def create_foreign_related_manager(superclass, rel_field, rel_model):
+    class RelatedManager(superclass):
+        def __init__(self, instance):
+            super(RelatedManager, self).__init__()
+            self.instance = instance
+            self.core_filters = {'%s__exact' % rel_field.name: instance}
+            self.model = rel_model
+
+        def __call__(self, **kwargs):
+            # We use **kwargs rather than a kwarg argument to enforce the
+            # `manager='manager_name'` syntax.
+            manager = getattr(self.model, kwargs.pop('manager'))
+            manager_class = create_foreign_related_manager(manager.__class__, rel_field, rel_model)
+            return manager_class(self.instance)
+
+        def get_queryset(self):
+            try:
+                return self.instance._prefetched_objects_cache[rel_field.related_query_name()]
+            except (AttributeError, KeyError):
+                db = self._db or router.db_for_read(self.model, instance=self.instance)
+                qs = super(RelatedManager, self).get_queryset().using(db).filter(**self.core_filters)
+                empty_strings_as_null = connections[db].features.interprets_empty_strings_as_nulls
+                for field in rel_field.foreign_related_fields:
+                    val = getattr(self.instance, field.attname)
+                    if val is None or (val == '' and empty_strings_as_null):
+                        return qs.none()
+                qs._known_related_objects = {rel_field: {self.instance.pk: self.instance}}
+                return qs
+
+        def get_prefetch_queryset(self, instances):
+            rel_obj_attr = rel_field.get_local_related_value
+            instance_attr = rel_field.get_foreign_related_value
+            instances_dict = dict((instance_attr(inst), inst) for inst in instances)
+            db = self._db or router.db_for_read(self.model, instance=instances[0])
+            query = {'%s__in' % rel_field.name: instances}
+            qs = super(RelatedManager, self).get_queryset().using(db).filter(**query)
+            # Since we just bypassed this class' get_queryset(), we must manage
+            # the reverse relation manually.
+            for rel_obj in qs:
+                instance = instances_dict[rel_obj_attr(rel_obj)]
+                setattr(rel_obj, rel_field.name, instance)
+            cache_name = rel_field.related_query_name()
+            return qs, rel_obj_attr, instance_attr, False, cache_name
+
+        def add(self, *objs):
+            for obj in objs:
+                if not isinstance(obj, self.model):
+                    raise TypeError("'%s' instance expected, got %r" % (self.model._meta.object_name, obj))
+                setattr(obj, rel_field.name, self.instance)
+                obj.save()
+        add.alters_data = True
+
+        def create(self, **kwargs):
+            kwargs[rel_field.name] = self.instance
+            db = router.db_for_write(self.model, instance=self.instance)
+            return super(RelatedManager, self.db_manager(db)).create(**kwargs)
+        create.alters_data = True
+
+        def get_or_create(self, **kwargs):
+            # Update kwargs with the related object that this
+            # ForeignRelatedObjectsDescriptor knows about.
+            kwargs[rel_field.name] = self.instance
+            db = router.db_for_write(self.model, instance=self.instance)
+            return super(RelatedManager, self.db_manager(db)).get_or_create(**kwargs)
+        get_or_create.alters_data = True
+
+        # remove() and clear() are only provided if the ForeignKey can have a value of null.
+        if rel_field.null:
+            def remove(self, *objs):
+                val = rel_field.get_foreign_related_value(self.instance)
+                for obj in objs:
+                    # Is obj actually part of this descriptor set?
+                    if rel_field.get_local_related_value(obj) == val:
+                        setattr(obj, rel_field.name, None)
+                        obj.save()
+                    else:
+                        raise rel_field.rel.to.DoesNotExist("%r is not related to %r." % (obj, self.instance))
+            remove.alters_data = True
+
+            def clear(self):
+                self.update(**{rel_field.name: None})
+            clear.alters_data = True
+
+    return RelatedManager
+
+
 class ForeignRelatedObjectsDescriptor(object):
     # This class provides the functionality that makes the related-object
     # managers available as attributes on a model class, for fields that have
@@ -392,86 +478,11 @@ class ForeignRelatedObjectsDescriptor(object):
     def related_manager_cls(self):
         # Dynamically create a class that subclasses the related model's default
         # manager.
-        superclass = self.related.model._default_manager.__class__
-        rel_field = self.related.field
-        rel_model = self.related.model
-
-        class RelatedManager(superclass):
-            def __init__(self, instance):
-                super(RelatedManager, self).__init__()
-                self.instance = instance
-                self.core_filters = {'%s__exact' % rel_field.name: instance}
-                self.model = rel_model
-
-            def get_queryset(self):
-                try:
-                    return self.instance._prefetched_objects_cache[rel_field.related_query_name()]
-                except (AttributeError, KeyError):
-                    db = self._db or router.db_for_read(self.model, instance=self.instance)
-                    qs = super(RelatedManager, self).get_queryset().using(db).filter(**self.core_filters)
-                    empty_strings_as_null = connections[db].features.interprets_empty_strings_as_nulls
-                    for field in rel_field.foreign_related_fields:
-                        val = getattr(self.instance, field.attname)
-                        if val is None or (val == '' and empty_strings_as_null):
-                            return qs.none()
-                    qs._known_related_objects = {rel_field: {self.instance.pk: self.instance}}
-                    return qs
-
-            def get_prefetch_queryset(self, instances):
-                rel_obj_attr = rel_field.get_local_related_value
-                instance_attr = rel_field.get_foreign_related_value
-                instances_dict = dict((instance_attr(inst), inst) for inst in instances)
-                db = self._db or router.db_for_read(self.model, instance=instances[0])
-                query = {'%s__in' % rel_field.name: instances}
-                qs = super(RelatedManager, self).get_queryset().using(db).filter(**query)
-                # Since we just bypassed this class' get_queryset(), we must manage
-                # the reverse relation manually.
-                for rel_obj in qs:
-                    instance = instances_dict[rel_obj_attr(rel_obj)]
-                    setattr(rel_obj, rel_field.name, instance)
-                cache_name = rel_field.related_query_name()
-                return qs, rel_obj_attr, instance_attr, False, cache_name
-
-            def add(self, *objs):
-                for obj in objs:
-                    if not isinstance(obj, self.model):
-                        raise TypeError("'%s' instance expected, got %r" % (self.model._meta.object_name, obj))
-                    setattr(obj, rel_field.name, self.instance)
-                    obj.save()
-            add.alters_data = True
-
-            def create(self, **kwargs):
-                kwargs[rel_field.name] = self.instance
-                db = router.db_for_write(self.model, instance=self.instance)
-                return super(RelatedManager, self.db_manager(db)).create(**kwargs)
-            create.alters_data = True
-
-            def get_or_create(self, **kwargs):
-                # Update kwargs with the related object that this
-                # ForeignRelatedObjectsDescriptor knows about.
-                kwargs[rel_field.name] = self.instance
-                db = router.db_for_write(self.model, instance=self.instance)
-                return super(RelatedManager, self.db_manager(db)).get_or_create(**kwargs)
-            get_or_create.alters_data = True
-
-            # remove() and clear() are only provided if the ForeignKey can have a value of null.
-            if rel_field.null:
-                def remove(self, *objs):
-                    val = rel_field.get_foreign_related_value(self.instance)
-                    for obj in objs:
-                        # Is obj actually part of this descriptor set?
-                        if rel_field.get_local_related_value(obj) == val:
-                            setattr(obj, rel_field.name, None)
-                            obj.save()
-                        else:
-                            raise rel_field.rel.to.DoesNotExist("%r is not related to %r." % (obj, self.instance))
-                remove.alters_data = True
-
-                def clear(self):
-                    self.update(**{rel_field.name: None})
-                clear.alters_data = True
-
-        return RelatedManager
+        return create_foreign_related_manager(
+            self.related.model._default_manager.__class__,
+            self.related.field,
+            self.related.model,
+        )
 
 
 def create_many_related_manager(superclass, rel):
@@ -512,6 +523,23 @@ def create_many_related_manager(superclass, rel):
                 raise ValueError("%r instance needs to have a primary key value before "
                                  "a many-to-many relationship can be used." %
                                  instance.__class__.__name__)
+
+        def __call__(self, **kwargs):
+            # We use **kwargs rather than a kwarg argument to enforce the
+            # `manager='manager_name'` syntax.
+            manager = getattr(self.model, kwargs.pop('manager'))
+            manager_class = create_many_related_manager(manager.__class__, rel)
+            return manager_class(
+                model=self.model,
+                query_field_name=self.query_field_name,
+                instance=self.instance,
+                symmetrical=self.symmetrical,
+                source_field_name=self.source_field_name,
+                target_field_name=self.target_field_name,
+                reverse=self.reverse,
+                through=self.through,
+                prefetch_cache_name=self.prefetch_cache_name,
+            )
 
         def get_queryset(self):
             try:

tests/custom_managers/models.py
@@ -11,6 +11,7 @@ returns.
 
 from __future__ import unicode_literals
 
+from django.contrib.contenttypes import generic
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -63,12 +64,28 @@ class BaseCustomManager(models.Manager):
 
 CustomManager = BaseCustomManager.from_queryset(CustomQuerySet)
 
+class FunPeopleManager(models.Manager):
+    def get_queryset(self):
+        return super(FunPeopleManager, self).get_queryset().filter(fun=True)
+
+class BoringPeopleManager(models.Manager):
+    def get_queryset(self):
+        return super(BoringPeopleManager, self).get_queryset().filter(fun=False)
+
 @python_2_unicode_compatible
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     fun = models.BooleanField(default=False)
+
+    favorite_book = models.ForeignKey('Book', null=True, related_name='favorite_books')
+    favorite_thing_type = models.ForeignKey('contenttypes.ContentType', null=True)
+    favorite_thing_id = models.IntegerField(null=True)
+    favorite_thing = generic.GenericForeignKey('favorite_thing_type', 'favorite_thing_id')
+
     objects = PersonManager()
+    fun_people = FunPeopleManager()
+    boring_people = BoringPeopleManager()
 
     custom_queryset_default_manager = CustomQuerySet.as_manager()
     custom_queryset_custom_manager = CustomManager('hello')
@@ -83,6 +100,9 @@ class Book(models.Model):
     is_published = models.BooleanField(default=False)
     published_objects = PublishedBookManager()
     authors = models.ManyToManyField(Person, related_name='books')
+
+    favorite_things = generic.GenericRelation(Person,
+        content_type_field='favorite_thing_type', object_id_field='favorite_thing_id')
 
     def __str__(self):
         return self.title

tests/custom_managers/tests.py
@@ -7,10 +7,15 @@ from .models import Person, Book, Car, PersonManager, PublishedBookManager
 
 
 class CustomManagerTests(TestCase):
-    def test_manager(self):
-        Person.objects.create(first_name="Bugs", last_name="Bunny", fun=True)
-        p2 = Person.objects.create(first_name="Droopy", last_name="Dog", fun=False)
+    def setUp(self):
+        self.b1 = Book.published_objects.create(
+            title="How to program", author="Rodney Dangerfield", is_published=True)
+        self.b2 = Book.published_objects.create(
+            title="How to be smart", author="Albert Einstein", is_published=False)
+        self.p1 = Person.objects.create(first_name="Bugs", last_name="Bunny", fun=True)
+        self.p2 = Person.objects.create(first_name="Droopy", last_name="Dog", fun=False)
 
+    def test_manager(self):
         # Test a custom `Manager` method.
         self.assertQuerysetEqual(
             Person.objects.get_fun_people(), [
@@ -61,14 +66,8 @@ class CustomManagerTests(TestCase):
 
         # The RelatedManager used on the 'books' descriptor extends the default
         # manager
-        self.assertIsInstance(p2.books, PublishedBookManager)
+        self.assertIsInstance(self.p2.books, PublishedBookManager)
 
-        Book.published_objects.create(
-            title="How to program", author="Rodney Dangerfield", is_published=True
-        )
-        b2 = Book.published_objects.create(
-            title="How to be smart", author="Albert Einstein", is_published=False
-        )
 
         # The default manager, "objects", doesn't exist, because a custom one
         # was provided.
@@ -76,7 +75,7 @@ class CustomManagerTests(TestCase):
 
         # The RelatedManager used on the 'authors' descriptor extends the
         # default manager
-        self.assertIsInstance(b2.authors, PersonManager)
+        self.assertIsInstance(self.b2.authors, PersonManager)
 
         self.assertQuerysetEqual(
             Book.published_objects.all(), [
@@ -113,4 +112,80 @@ class CustomManagerTests(TestCase):
                 "Neon",
             ],
             lambda c: c.name
+        )
+
+    def test_related_manager_fk(self):
+        self.p1.favorite_book = self.b1
+        self.p1.save()
+        self.p2.favorite_book = self.b1
+        self.p2.save()
+
+        self.assertQuerysetEqual(
+            self.b1.favorite_books.order_by('first_name').all(), [
+                "Bugs",
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.favorite_books(manager='boring_people').all(), [
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.favorite_books(manager='fun_people').all(), [
+                "Bugs",
+            ],
+            lambda c: c.first_name
+        )
+
+    def test_related_manager_gfk(self):
+        self.p1.favorite_thing = self.b1
+        self.p1.save()
+        self.p2.favorite_thing = self.b1
+        self.p2.save()
+
+        self.assertQuerysetEqual(
+            self.b1.favorite_things.order_by('first_name').all(), [
+                "Bugs",
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.favorite_things(manager='boring_people').all(), [
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.favorite_things(manager='fun_people').all(), [
+                "Bugs",
+            ],
+            lambda c: c.first_name
+        )
+
+    def test_related_manager_m2m(self):
+        self.b1.authors.add(self.p1)
+        self.b1.authors.add(self.p2)
+
+        self.assertQuerysetEqual(
+            self.b1.authors.order_by('first_name').all(), [
+                "Bugs",
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.authors(manager='boring_people').all(), [
+                "Droopy",
+            ],
+            lambda c: c.first_name
+        )
+        self.assertQuerysetEqual(
+            self.b1.authors(manager='fun_people').all(), [
+                "Bugs",
+            ],
+            lambda c: c.first_name
         )

commit a7c95f3ad76e71d8646224bec7dccadc38e620cb

Fixed #3871 -- Fixed regression introduced by a4571d6.
Added do_not_call_in_templates=True attribute to RelatedManagers
to prevent them from being called.

Thanks jbg@ for the report.

django/contrib/contenttypes/generic.py
@@ -335,6 +335,7 @@ def create_generic_related_manager(superclass):
                 object_id_field_name = self.object_id_field_name,
                 prefetch_cache_name = self.prefetch_cache_name,
             )
+        do_not_call_in_templates = True
 
         def get_queryset(self):
             try:

django/db/models/fields/related.py
@@ -377,6 +377,7 @@ def create_foreign_related_manager(superclass, rel_field, rel_model):
             manager = getattr(self.model, kwargs.pop('manager'))
             manager_class = create_foreign_related_manager(manager.__class__, rel_field, rel_model)
             return manager_class(self.instance)
+        do_not_call_in_templates = True
 
         def get_queryset(self):
             try:
@@ -545,6 +546,7 @@ def create_many_related_manager(superclass, rel):
                 through=self.through,
                 prefetch_cache_name=self.prefetch_cache_name,
             )
+        do_not_call_in_templates = True
 
         def get_queryset(self):
             try:

tests/managers_regress/models.py
@@ -3,7 +3,9 @@ Various edge-cases for model managers.
 """
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import python_2_unicode_compatible, force_text
 
 
 class OnlyFred(models.Manager):
@@ -119,3 +121,26 @@ class Child6(Child4):
 # Will not inherit default manager from parent.
 class Child7(Parent):
     pass
+
+
+# RelatedManagers
+@python_2_unicode_compatible
+class RelatedModel(models.Model):
+    test_gfk = generic.GenericRelation('RelationModel', content_type_field='gfk_ctype', object_id_field='gfk_id')
+
+    def __str__(self):
+        return force_text(self.pk)
+
+
+@python_2_unicode_compatible
+class RelationModel(models.Model):
+    fk = models.ForeignKey(RelatedModel, related_name='test_fk')
+
+    m2m = models.ManyToManyField(RelatedModel, related_name='test_m2m')
+
+    gfk_ctype = models.ForeignKey(ContentType)
+    gfk_id = models.IntegerField()
+    gfk = generic.GenericForeignKey(ct_field='gfk_ctype', fk_field='gfk_id')
+
+    def __str__(self):
+        return force_text(self.pk)

tests/managers_regress/tests.py
@@ -4,8 +4,9 @@ import copy
 from django.conf import settings
 from django.db import models
 from django.db.models.loading import cache
+from django.template import Context, Template
 from django.test import TestCase
-from django.test.utils import override_settings
+from django.utils.encoding import force_text
 
 from .models import (
     Child1,
@@ -18,6 +19,8 @@ from .models import (
     AbstractBase1,
     AbstractBase2,
     AbstractBase3,
+    RelatedModel,
+    RelationModel,
 )
 
 
@@ -193,3 +196,19 @@ class ManagersRegressionTests(TestCase):
             del settings.TEST_SWAPPABLE_MODEL
             cache.app_models = old_app_models
             cache.app_store = old_app_store
+
+    def test_regress_3871(self):
+        related = RelatedModel.objects.create()
+
+        relation = RelationModel()
+        relation.fk = related
+        relation.gfk = related
+        relation.save()
+        relation.m2m.add(related)
+
+        t = Template('{{ related.test_fk.all.0 }}{{ related.test_gfk.all.0 }}{{ related.test_m2m.all.0 }}')
+
+        self.assertEqual(
+            t.render(Context({'related': related})),
+            ''.join([force_text(relation.pk)] * 3),
+        )