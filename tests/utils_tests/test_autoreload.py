varshithkonakanchi/django

commit 68de2486a529a4eba3dd53aac7672015a46f41ea

Fixed #9722 - used pyinotify as change detection system when available
Used pyinotify (when available) to replace the "pool-every-one-second"
mechanism in `django.utils.autoreload`.

Thanks Chris Lamb and Pascal Hartig for work on the patch.

django/utils/autoreload.py
@@ -28,12 +28,14 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import datetime
 import os
 import signal
 import sys
 import time
 import traceback
 
+from django.core.signals import request_finished
 try:
     from django.utils.six.moves import _thread as thread
 except ImportError:
@@ -51,6 +53,18 @@ try:
 except ImportError:
     termios = None
 
+USE_INOTIFY = False
+try:
+    # Test whether inotify is enabled and likely to work
+    import pyinotify
+
+    fd = pyinotify.INotifyWrapper.create().inotify_init()
+    if fd >= 0:
+        USE_INOTIFY = True
+        os.close(fd)
+except ImportError:
+    pass
+
 RUN_RELOADER = True
 
 _mtimes = {}
@@ -58,14 +72,13 @@ _win = (sys.platform == "win32")
 
 _error_files = []
 
-def code_changed():
-    global _mtimes, _win
-    filenames = []
-    for m in list(sys.modules.values()):
-        try:
-            filenames.append(m.__file__)
-        except AttributeError:
-            pass
+
+def gen_filenames():
+    """
+    Yields a generator over filenames referenced in sys.modules.
+    """
+    filenames = [filename.__file__ for filename in sys.modules.values()
+                if hasattr(filename, '__file__')]
     for filename in filenames + _error_files:
         if not filename:
             continue
@@ -73,8 +86,42 @@ def code_changed():
             filename = filename[:-1]
         if filename.endswith("$py.class"):
             filename = filename[:-9] + ".py"
-        if not os.path.exists(filename):
-            continue # File might be in an egg, so it can't be reloaded.
+        if os.path.exists(filename):
+            yield filename
+
+def inotify_code_changed():
+    """
+    Checks for changed code using inotify. After being called
+    it blocks until a change event has been fired.
+    """
+    wm = pyinotify.WatchManager()
+    notifier = pyinotify.Notifier(wm)
+
+    def update_watch(sender=None, **kwargs):
+        mask = (
+            pyinotify.IN_MODIFY |
+            pyinotify.IN_DELETE |
+            pyinotify.IN_ATTRIB |
+            pyinotify.IN_MOVED_FROM |
+            pyinotify.IN_MOVED_TO |
+            pyinotify.IN_CREATE
+        )
+        for path in gen_filenames():
+            wm.add_watch(path, mask)
+
+    request_finished.connect(update_watch)
+    update_watch()
+
+    # Block forever
+    notifier.check_events(timeout=None)
+    notifier.stop()
+
+    # If we are here the code must have changed.
+    return True
+
+def code_changed():
+    global _mtimes, _win
+    for filename in gen_filenames():
         stat = os.stat(filename)
         mtime = stat.st_mtime
         if _win:
@@ -129,10 +176,15 @@ def ensure_echo_on():
 
 def reloader_thread():
     ensure_echo_on()
+    if USE_INOTIFY:
+        fn = inotify_code_changed
+    else:
+        fn = code_changed
     while RUN_RELOADER:
-        if code_changed():
+        if fn():
             sys.exit(3) # force reload
         time.sleep(1)
+
 
 def restart_with_reloader():
     while True:

AUTHORS
@@ -286,6 +286,7 @@ answer newbie questions, and generally made Django that much better:
     Will Hardy <django@willhardy.com.au>
     Brian Harring <ferringb@gmail.com>
     Brant Harris
+    Pascal Hartig <phartig@rdrei.net>
     Ronny Haryanto <http://ronny.haryan.to/>
     Axel Haustant <noirbizarre@gmail.com>
     Hawkeye
@@ -372,6 +373,7 @@ answer newbie questions, and generally made Django that much better:
     Vladimir Kuzma <vladimirkuzma.ch@gmail.com>
     Denis Kuzmichyov <kuzmichyov@gmail.com>
     Panos Laganakos <panos.laganakos@gmail.com>
+    Chris Lamb <lamby@debian.org>
     Nick Lane <nick.lane.au@gmail.com>
     Łukasz Langa <lukasz@langa.pl>
     Stuart Langridge <http://www.kryogenix.org/>
@@ -666,6 +668,7 @@ answer newbie questions, and generally made Django that much better:
     Jesse Young <adunar@gmail.com>
     Marc Aymerich Gubern
     Wiktor Kołodziej <wiktor@pykonik.org>
+    Unai Zalakain <unai@gisa-elkartea.org>
     Mykola Zamkovoi <nickzam@gmail.com>
     zegor
     Gasper Zejn <zejn@kiberpipa.org>

commit 60392ac74f77c7a240d43fc7fc1ba9206460d472

Fixed pyinotify performance regression in 68de2486a5
Refs #9722. Thanks Tim Graham for the review.

django/contrib/staticfiles/handlers.py
@@ -12,6 +12,10 @@ class StaticFilesHandler(WSGIHandler):
     WSGI middleware that intercepts calls to the static files directory, as
     defined by the STATIC_URL setting, and serves those files.
     """
+    # May be used to differentiate between handler types (e.g. in a
+    # request_finished signal)
+    handles_files = True
+
     def __init__(self, application):
         self.application = application
         self.base_url = urlparse(self.get_base_url())

django/utils/autoreload.py
@@ -77,20 +77,31 @@ _mtimes = {}
 _win = (sys.platform == "win32")
 
 _error_files = []
+_cached_modules = set()
+_cached_filenames = []
 
-
-def gen_filenames():
+def gen_filenames(only_new=False):
     """
-    Yields a generator over filenames referenced in sys.modules and translation
+    Returns a list of filenames referenced in sys.modules and translation
     files.
     """
     # N.B. ``list(...)`` is needed, because this runs in parallel with
     # application code which might be mutating ``sys.modules``, and this will
     # fail with RuntimeError: cannot mutate dictionary while iterating
-    filenames = [filename.__file__ for filename in list(sys.modules.values())
-                if hasattr(filename, '__file__')]
+    global _cached_modules, _cached_filenames
+    module_values = set(sys.modules.values())
+    if _cached_modules == module_values:
+        # No changes in module list, short-circuit the function
+        if only_new:
+            return []
+        else:
+            return _cached_filenames
 
-    if settings.USE_I18N:
+    new_modules = module_values - _cached_modules
+    new_filenames = [filename.__file__ for filename in new_modules
+                     if hasattr(filename, '__file__')]
+
+    if not _cached_filenames and settings.USE_I18N:
         # Add the names of the .mo files that can be generated
         # by compilemessages management command to the list of files watched.
         basedirs = [os.path.join(os.path.dirname(os.path.dirname(__file__)),
@@ -105,9 +116,14 @@ def gen_filenames():
             for dirpath, dirnames, locale_filenames in os.walk(basedir):
                 for filename in locale_filenames:
                     if filename.endswith('.mo'):
-                        filenames.append(os.path.join(dirpath, filename))
+                        new_filenames.append(os.path.join(dirpath, filename))
 
-    for filename in filenames + _error_files:
+    if only_new:
+        filelist = new_filenames
+    else:
+        filelist = _cached_filenames + new_filenames + _error_files
+    filenames = []
+    for filename in filelist:
         if not filename:
             continue
         if filename.endswith(".pyc") or filename.endswith(".pyo"):
@@ -115,7 +131,10 @@ def gen_filenames():
         if filename.endswith("$py.class"):
             filename = filename[:-9] + ".py"
         if os.path.exists(filename):
-            yield filename
+            filenames.append(filename)
+    _cached_modules = _cached_modules.union(new_modules)
+    _cached_filenames += new_filenames
+    return filenames
 
 
 def reset_translations():
@@ -145,6 +164,10 @@ def inotify_code_changed():
     notifier = pyinotify.Notifier(wm, EventHandler())
 
     def update_watch(sender=None, **kwargs):
+        if sender and getattr(sender, 'handles_files', False):
+            # No need to update watches when request serves files.
+            # (sender is supposed to be a django.core.handlers.BaseHandler subclass)
+            return
         mask = (
             pyinotify.IN_MODIFY |
             pyinotify.IN_DELETE |
@@ -153,7 +176,7 @@ def inotify_code_changed():
             pyinotify.IN_MOVED_TO |
             pyinotify.IN_CREATE
         )
-        for path in gen_filenames():
+        for path in gen_filenames(only_new=True):
             wm.add_watch(path, mask)
 
     # New modules may get imported when a request is processed.

tests/utils_tests/test_autoreload.py
@@ -9,6 +9,12 @@ LOCALE_PATH = os.path.join(os.path.dirname(__file__), 'locale')
 
 
 class TestFilenameGenerator(TestCase):
+    def setUp(self):
+        # Empty cached variables
+        from django.utils import autoreload
+        autoreload._cached_modules = set()
+        autoreload._cached_filenames = []
+
     def test_django_locales(self):
         """
         Test that gen_filenames() also yields the built-in django locale files.
@@ -64,3 +70,14 @@ class TestFilenameGenerator(TestCase):
             os.path.join(os.path.dirname(conf.__file__), 'locale', 'nl',
                          'LC_MESSAGES', 'django.mo'),
             filenames)
+
+    def test_only_new_files(self):
+        """
+        When calling a second time gen_filenames with only_new = True, only
+        files from newly loaded modules should be given.
+        """
+        filenames1 = list(gen_filenames())
+        from fractions import Fraction
+        filenames2 = list(gen_filenames(only_new=True))
+        self.assertEqual(len(filenames2), 1)
+        self.assertTrue(filenames2[0].endswith('fractions.py'))