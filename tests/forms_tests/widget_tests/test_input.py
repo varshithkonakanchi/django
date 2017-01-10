varshithkonakanchi/django

commit 49f8bb9da8524dba8f5520175dca88fe35a68464

Fixed #15667 -- Added template-based widget rendering.
Thanks Carl Meyer and Tim Graham for contributing to the patch.

django/conf/global_settings.py
@@ -216,6 +216,9 @@ INSTALLED_APPS = []
 
 TEMPLATES = []
 
+# Default form rendering class.
+FORM_RENDERER = 'django.forms.renderers.DjangoTemplates'
+
 # Default email address to use for various automated correspondence from
 # the site managers.
 DEFAULT_FROM_EMAIL = 'webmaster@localhost'

django/contrib/admin/templates/admin/widgets/clearable_file_input.html
@@ -1,0 +1,6 @@
+{% if is_initial %}<p class="file-upload">{{ initial_text }}: <a href="{{ widget.value.url }}">{{ widget.value }}</a>{% if not widget.required %}
+<span class="clearable-file-input">
+<input type="checkbox" name="{{ checkbox_name }}" id="{{ checkbox_id }}" />
+<label for="{{ checkbox_id }}">{{ clear_checkbox_label }}</label>{% endif %}</span><br />
+{{ input_text }}:{% endif %}
+<input type="{{ widget.type }}" name="{{ widget.name }}"{% include "django/forms/widgets/attrs.html" %} />{% if is_initial %}</p>{% endif %}

django/contrib/admin/templates/admin/widgets/foreign_key_raw_id.html
@@ -1,0 +1,1 @@
+{% include 'django/forms/widgets/input.html' %}{% if related_url %}<a href="{{ related_url }}" class="related-lookup" id="lookup_id_{{ widget.name }}" title="{{ link_title }}"></a>{% endif %}{% if link_label %}&nbsp;<strong>{% if link_url %}<a href="{{ link_url }}">{% endif %}{{ link_label }}{% if link_url %}</a>{% endif %}</strong>{% endif %}

django/contrib/admin/templates/admin/widgets/many_to_many_raw_id.html
@@ -1,0 +1,1 @@
+{% include 'admin/widgets/foreign_key_raw_id.html' %}

django/contrib/admin/templates/admin/widgets/radio.html
@@ -1,0 +1,1 @@
+{% include "django/forms/widgets/multiple_input.html" %}

django/contrib/admin/templates/admin/widgets/related_widget_wrapper.html
@@ -1,0 +1,27 @@
+{% load i18n static %}
+<div class="related-widget-wrapper">
+    {% include widget.template_name %}
+    {% block links %}
+        {% if can_change_related %}
+        <a class="related-widget-wrapper-link change-related" id="change_id_{{ widget.name }}"
+            data-href-template="{{ change_related_template_url }}?{{ url_params }}"
+            title="{% blocktrans %}Change selected {{ model }}{% endblocktrans %}">
+            <img src="{% static 'admin/img/icon-changelink.svg' %}" width="10" height="10" alt="{% trans 'Change' %}"/>
+        </a>
+        {% endif %}
+        {% if can_add_related %}
+        <a class="related-widget-wrapper-link add-related" id="add_id_{{ widget.name }}"
+            href="{{ add_related_url }}?{{ url_params }}"
+            title="{% blocktrans %}Add another {{ model }}{% endblocktrans %}">
+            <img src="{% static 'admin/img/icon-addlink.svg' %}" width="10" height="10" alt="{% trans 'Add' %}"/>
+        </a>
+        {% endif %}
+        {% if can_delete_related %}
+        <a class="related-widget-wrapper-link delete-related" id="delete_id_{{ widget.name }}"
+            data-href-template="{{ delete_related_template_url }}?{{ url_params }}"
+            title="{% blocktrans %}Delete selected {{ model }}{% endblocktrans %}">
+            <img src="{% static 'admin/img/icon-deletelink.svg' %}" width="10" height="10" alt="{% trans 'Delete' %}"/>
+        </a>
+        {% endif %}
+    {% endblock %}
+</div>

django/contrib/admin/templates/admin/widgets/split_datetime.html
@@ -1,0 +1,4 @@
+<p class="datetime">
+  {{ date_label }} {% with widget=widget.subwidgets.0 %}{% include widget.template_name %}{% endwith %}<br />
+  {{ time_label }} {% with widget=widget.subwidgets.1 %}{% include widget.template_name %}{% endwith %}
+</p>

django/contrib/admin/templates/admin/widgets/url.html
@@ -1,0 +1,1 @@
+{% if widget.value %}<p class="url">{{ current_label }} <a href="{{ widget.href }}">{{ widget.value }}</a><br />{{ change_label }} {% endif %}{% include "django/forms/widgets/input.html" %}{% if widget.value %}</p>{% endif %}

commit 77136847b15885966593e8bb15b681e1d2343d6e

Fixed #27712 -- Reallowed Input widget's attrs argument to set the input type.
Regression in 49f8bb9da8524dba8f5520175dca88fe35a68464.

tests/forms_tests/widget_tests/test_input.py
@@ -1,0 +1,15 @@
+from django.forms.widgets import Input
+
+from .base import WidgetTest
+
+
+class InputTests(WidgetTest):
+
+    def test_attrs_with_type(self):
+        attrs = {'type': 'date'}
+        widget = Input(attrs)
+        self.check_html(widget, 'name', 'value', '<input type="date" name="name" value="value" />')
+        # reuse the same attrs for another widget
+        self.check_html(Input(attrs), 'name', 'value', '<input type="date" name="name" value="value" />')
+        attrs['type'] = 'number'  # shouldn't change the widget type
+        self.check_html(widget, 'name', 'value', '<input type="date" name="name" value="value" />')