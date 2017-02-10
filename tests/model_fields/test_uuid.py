varshithkonakanchi/django

commit 33c8bc933133a00fe90e7c817d16effa48996fee

Fixed #27148 -- Fixed ModelMultipleChoiceField crash with invalid UUID.

django/db/models/fields/__init__.py
@@ -2371,20 +2371,17 @@ class UUIDField(Field):
         if value is None:
             return None
         if not isinstance(value, uuid.UUID):
-            try:
-                value = uuid.UUID(value)
-            except AttributeError:
-                raise TypeError(self.error_messages['invalid'] % {'value': value})
+            value = self.to_python(value)
 
         if connection.features.has_native_uuid_field:
             return value
         return value.hex
 
     def to_python(self, value):
-        if value and not isinstance(value, uuid.UUID):
+        if not isinstance(value, uuid.UUID):
             try:
                 return uuid.UUID(value)
-            except ValueError:
+            except (AttributeError, ValueError):
                 raise exceptions.ValidationError(
                     self.error_messages['invalid'],
                     code='invalid',

tests/model_fields/test_uuid.py
@@ -40,17 +40,17 @@ class TestSaveLoad(TestCase):
         self.assertIsNone(loaded.field)
 
     def test_pk_validated(self):
-        with self.assertRaisesMessage(TypeError, 'is not a valid UUID'):
+        with self.assertRaisesMessage(exceptions.ValidationError, 'is not a valid UUID'):
             PrimaryKeyUUIDModel.objects.get(pk={})
 
-        with self.assertRaisesMessage(TypeError, 'is not a valid UUID'):
+        with self.assertRaisesMessage(exceptions.ValidationError, 'is not a valid UUID'):
             PrimaryKeyUUIDModel.objects.get(pk=[])
 
     def test_wrong_value(self):
-        with self.assertRaisesMessage(ValueError, 'badly formed hexadecimal UUID string'):
+        with self.assertRaisesMessage(exceptions.ValidationError, 'is not a valid UUID'):
             UUIDModel.objects.get(field='not-a-uuid')
 
-        with self.assertRaisesMessage(ValueError, 'badly formed hexadecimal UUID string'):
+        with self.assertRaisesMessage(exceptions.ValidationError, 'is not a valid UUID'):
             UUIDModel.objects.create(field='not-a-uuid')
 
 

tests/model_forms/test_uuid.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from .models import UUIDPK
@@ -27,3 +28,8 @@ class ModelFormBaseTest(TestCase):
         msg = "The UUIDPK could not be changed because the data didn't validate."
         with self.assertRaisesMessage(ValueError, msg):
             form.save()
+
+    def test_model_multiple_choice_field_uuid_pk(self):
+        f = forms.ModelMultipleChoiceField(UUIDPK.objects.all())
+        with self.assertRaisesMessage(ValidationError, "'invalid_uuid' is not a valid UUID."):
+            f.clean(['invalid_uuid'])

commit 5e81b0765c2db8785db053527d633521de2f1f6a

Refs #27148 -- Fixed UUIDField.to_python(None) crash.
Regression in 33c8bc933133a00fe90e7c817d16effa48996fee.

django/db/models/fields/__init__.py
@@ -2337,9 +2337,7 @@ class UUIDField(Field):
         return value.hex
 
     def to_python(self, value):
-        if value is None:
-            return None
-        if not isinstance(value, uuid.UUID):
+        if value is not None and not isinstance(value, uuid.UUID):
             try:
                 return uuid.UUID(value)
             except (AttributeError, ValueError):

tests/model_fields/test_uuid.py
@@ -54,12 +54,15 @@ class TestSaveLoad(TestCase):
             UUIDModel.objects.create(field='not-a-uuid')
 
 
-class TestMigrations(SimpleTestCase):
+class TestMethods(SimpleTestCase):
 
     def test_deconstruct(self):
         field = models.UUIDField()
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(kwargs, {})
+
+    def test_to_python(self):
+        self.assertIsNone(models.UUIDField().to_python(None))
 
 
 class TestQuerying(TestCase):
@@ -88,6 +91,10 @@ class TestSerialization(SimpleTestCase):
         '[{"fields": {"field": "550e8400-e29b-41d4-a716-446655440000"}, '
         '"model": "model_fields.uuidmodel", "pk": null}]'
     )
+    nullable_test_data = (
+        '[{"fields": {"field": null}, '
+        '"model": "model_fields.nullableuuidmodel", "pk": null}]'
+    )
 
     def test_dumping(self):
         instance = UUIDModel(field=uuid.UUID('550e8400e29b41d4a716446655440000'))
@@ -97,6 +104,10 @@ class TestSerialization(SimpleTestCase):
     def test_loading(self):
         instance = list(serializers.deserialize('json', self.test_data))[0].object
         self.assertEqual(instance.field, uuid.UUID('550e8400-e29b-41d4-a716-446655440000'))
+
+    def test_nullable_loading(self):
+        instance = list(serializers.deserialize('json', self.nullable_test_data))[0].object
+        self.assertIsNone(instance.field)
 
 
 class TestValidation(SimpleTestCase):