varshithkonakanchi/django

commit 31d606f9e9e5df6d58413bab8b8b4dadca13fe63

Fixed #26844 -- Made formset's validate_min validation ignore empty forms.

tests/forms_tests/tests/test_formsets.py
@@ -377,6 +377,17 @@ class FormsFormsetTestCase(SimpleTestCase):
         self.assertFalse(formset.is_valid())
         self.assertEqual(formset.non_form_errors(), ['Please submit 3 or more forms.'])
 
+    def test_formset_validate_min_excludes_empty_forms(self):
+        data = {
+            'choices-TOTAL_FORMS': '2',
+            'choices-INITIAL_FORMS': '0',
+        }
+        ChoiceFormSet = formset_factory(Choice, extra=2, min_num=1, validate_min=True, can_delete=True)
+        formset = ChoiceFormSet(data, prefix='choices')
+        self.assertFalse(formset.has_changed())
+        self.assertFalse(formset.is_valid())
+        self.assertEqual(formset.non_form_errors(), ['Please submit 1 or more forms.'])
+
     def test_second_form_partially_filled_2(self):
         # And once again, if we try to partially complete a form, validation will fail.
 

commit 90cb9297b43c99135437c88c0d5ec36460c57dbc

Fixed #28130 -- Fixed formset min_num validation with initial, unchanged forms.
Regression in 31d606f9e9e5df6d58413bab8b8b4dadca13fe63.

tests/forms_tests/tests/test_formsets.py
@@ -402,6 +402,31 @@ class FormsFormsetTestCase(SimpleTestCase):
         self.assertFalse(formset.is_valid())
         self.assertEqual(formset.non_form_errors(), ['Please submit 3 or more forms.'])
 
+    def test_formset_validate_min_unchanged_forms(self):
+        """
+        min_num validation doesn't consider unchanged forms with initial data
+        as "empty".
+        """
+        initial = [
+            {'choice': 'Zero', 'votes': 0},
+            {'choice': 'One', 'votes': 0},
+        ]
+        data = {
+            'choices-TOTAL_FORMS': '2',
+            'choices-INITIAL_FORMS': '2',
+            'choices-MIN_NUM_FORMS': '0',
+            'choices-MAX_NUM_FORMS': '2',
+            'choices-0-choice': 'Zero',
+            'choices-0-votes': '0',
+            'choices-1-choice': 'One',
+            'choices-1-votes': '1',  # changed from initial
+        }
+        ChoiceFormSet = formset_factory(Choice, min_num=2, validate_min=True)
+        formset = ChoiceFormSet(data, auto_id=False, prefix='choices', initial=initial)
+        self.assertFalse(formset.forms[0].has_changed())
+        self.assertTrue(formset.forms[1].has_changed())
+        self.assertTrue(formset.is_valid())
+
     def test_formset_validate_min_excludes_empty_forms(self):
         data = {
             'choices-TOTAL_FORMS': '2',