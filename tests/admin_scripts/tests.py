varshithkonakanchi/django

commit 58723fecf43a40572c4dd912ca31521e156e2a86

Fixed #27787 -- Made call_command() validate the options it receives.

tests/user_commands/tests.py
@@ -175,6 +175,25 @@ class CommandTests(SimpleTestCase):
         finally:
             dance.Command.requires_migrations_checks = requires_migrations_checks
 
+    def test_call_command_unrecognized_option(self):
+        msg = (
+            'Unknown option(s) for dance command: unrecognized. Valid options '
+            'are: example, help, integer, no_color, opt_3, option3, '
+            'pythonpath, settings, skip_checks, stderr, stdout, style, '
+            'traceback, verbosity, version.'
+        )
+        with self.assertRaisesMessage(TypeError, msg):
+            management.call_command('dance', unrecognized=1)
+
+        msg = (
+            'Unknown option(s) for dance command: unrecognized, unrecognized2. '
+            'Valid options are: example, help, integer, no_color, opt_3, '
+            'option3, pythonpath, settings, skip_checks, stderr, stdout, '
+            'style, traceback, verbosity, version.'
+        )
+        with self.assertRaisesMessage(TypeError, msg):
+            management.call_command('dance', unrecognized=1, unrecognized2=1)
+
 
 class CommandRunTests(AdminScriptTestCase):
     """

django/contrib/auth/management/commands/createsuperuser.py
@@ -20,6 +20,7 @@ class NotRunningInTTYException(Exception):
 class Command(BaseCommand):
     help = 'Used to create a superuser.'
     requires_migrations_checks = True
+    stealth_options = ('stdin',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

django/core/management/__init__.py
@@ -118,6 +118,20 @@ def call_command(command_name, *args, **options):
     arg_options = {opt_mapping.get(key, key): value for key, value in options.items()}
     defaults = parser.parse_args(args=[force_text(a) for a in args])
     defaults = dict(defaults._get_kwargs(), **arg_options)
+    # Raise an error if any unknown options were passed.
+    stealth_options = set(command.base_stealth_options + command.stealth_options)
+    dest_parameters = {action.dest for action in parser._actions}
+    valid_options = dest_parameters | stealth_options | set(opt_mapping)
+    unknown_options = set(options) - valid_options
+    if unknown_options:
+        raise TypeError(
+            "Unknown option(s) for %s command: %s. "
+            "Valid options are: %s." % (
+                command_name,
+                ', '.join(sorted(unknown_options)),
+                ', '.join(sorted(valid_options)),
+            )
+        )
     # Move positional args out of options to mimic legacy optparse
     args = defaults.pop('args', ())
     if 'skip_checks' not in options:

django/core/management/base.py
@@ -183,6 +183,10 @@ class BaseCommand:
         that is locale-sensitive and such content shouldn't contain any
         translations (like it happens e.g. with django.contrib.auth
         permissions) as activating any locale might cause unintended effects.
+
+    ``stealth_options``
+        A tuple of any options the command uses which aren't defined by the
+        argument parser.
     """
     # Metadata about this command.
     help = ''
@@ -193,6 +197,11 @@ class BaseCommand:
     leave_locale_alone = False
     requires_migrations_checks = False
     requires_system_checks = True
+    # Arguments, common to all commands, which aren't defined by the argument
+    # parser.
+    base_stealth_options = ('skip_checks', 'stderr', 'stdout')
+    # Command-specific options not defined by the argument parser.
+    stealth_options = ()
 
     def __init__(self, stdout=None, stderr=None, no_color=False):
         self.stdout = OutputWrapper(stdout or sys.stdout)

django/core/management/commands/flush.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         'Removes ALL DATA from the database, including data added during '
         'migrations. Does not achieve a "fresh install" state.'
     )
+    stealth_options = ('reset_sequences', 'allow_cascade', 'inhibit_post_migrate')
 
     def add_arguments(self, parser):
         parser.add_argument(

django/core/management/commands/inspectdb.py
@@ -9,9 +9,8 @@ from django.db.models.constants import LOOKUP_SEP
 
 class Command(BaseCommand):
     help = "Introspects the database tables in the given database and outputs a Django model module."
-
     requires_system_checks = False
-
+    stealth_options = ('table_name_filter', )
     db_module = 'django.db'
 
     def add_arguments(self, parser):

tests/auth_tests/test_management.py
@@ -320,7 +320,6 @@ class CreatesuperuserManagementCommandTestCase(TestCase):
             call_command(
                 "createsuperuser",
                 interactive=False,
-                username="joe@somewhere.org",
                 stdout=new_io,
                 stderr=new_io,
             )

commit e6846242afb06015e08b6d8a837be4d22ea3c139

Fixed #28941 -- Fixed crash in testserver command startup.
Regression in 58723fecf43a40572c4dd912ca31521e156e2a86.

django/core/management/commands/runserver.py
@@ -27,6 +27,7 @@ class Command(BaseCommand):
     # Validation is called explicitly each time the server is reloaded.
     requires_system_checks = False
     leave_locale_alone = True
+    stealth_options = ('shutdown_message',)
 
     default_addr = '127.0.0.1'
     default_addr_ipv6 = '::1'

tests/admin_scripts/tests.py
@@ -21,7 +21,9 @@ from django.conf import settings
 from django.core.management import (
     BaseCommand, CommandError, call_command, color,
 )
-from django.db import ConnectionHandler
+from django.core.management.commands.loaddata import Command as LoaddataCommand
+from django.core.management.commands.runserver import Command as RunserverCommand
+from django.db import ConnectionHandler, connection
 from django.db.migrations.recorder import MigrationRecorder
 from django.test import (
     LiveServerTestCase, SimpleTestCase, TestCase, override_settings,
@@ -1414,6 +1416,31 @@ class ManageTestserver(AdminScriptTestCase):
             stdout=out, settings=None, pythonpath=None, verbosity=1,
             traceback=False, addrport='', no_color=False, use_ipv6=False,
             skip_checks=True, interactive=True,
+        )
+
+    @mock.patch('django.db.connection.creation.create_test_db', return_value='test_db')
+    @mock.patch.object(LoaddataCommand, 'handle', return_value='')
+    @mock.patch.object(RunserverCommand, 'handle', return_value='')
+    def test_params_to_runserver(self, mock_runserver_handle, mock_loaddata_handle, mock_create_test_db):
+        out = StringIO()
+        call_command('testserver', 'blah.json', stdout=out)
+        mock_runserver_handle.assert_called_with(
+            addrport='',
+            insecure_serving=False,
+            no_color=False,
+            pythonpath=None,
+            settings=None,
+            shutdown_message=(
+                "\nServer stopped.\nNote that the test database, 'test_db', "
+                "has not been deleted. You can explore it on your own."
+            ),
+            skip_checks=True,
+            traceback=False,
+            use_ipv6=False,
+            use_reloader=False,
+            use_static_handler=True,
+            use_threading=connection.features.test_db_allows_multiple_connections,
+            verbosity=1,
         )
 
 