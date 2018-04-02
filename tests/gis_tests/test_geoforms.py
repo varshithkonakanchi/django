varshithkonakanchi/django

commit c5b60fcabd1cf48c90bb324b266aa097e4680904

Fixed #25873 -- Made GEOSGeometry handle the srid parameter more predictably.

django/contrib/gis/forms/widgets.py
@@ -35,7 +35,7 @@ class BaseGeometryWidget(Widget):
 
     def deserialize(self, value):
         try:
-            return GEOSGeometry(value, self.map_srid)
+            return GEOSGeometry(value)
         except (GEOSException, ValueError) as err:
             logger.error("Error creating geometry from value '%s' (%s)", value, err)
         return None
@@ -48,7 +48,7 @@ class BaseGeometryWidget(Widget):
 
         if value:
             # Check that srid of value and map match
-            if value.srid != self.map_srid:
+            if value.srid and value.srid != self.map_srid:
                 try:
                     ogr = value.ogr
                     ogr.transform(self.map_srid)

django/contrib/gis/geos/geometry.py
@@ -46,6 +46,7 @@ class GEOSGeometry(GEOSBase, ListMixin):
         The `srid` keyword is used to specify the Source Reference Identifier
         (SRID) number for this Geometry.  If not set, the SRID will be None.
         """
+        input_srid = None
         if isinstance(geo_input, bytes):
             geo_input = force_text(geo_input)
         if isinstance(geo_input, str):
@@ -53,7 +54,7 @@ class GEOSGeometry(GEOSBase, ListMixin):
             if wkt_m:
                 # Handling WKT input.
                 if wkt_m.group('srid'):
-                    srid = int(wkt_m.group('srid'))
+                    input_srid = int(wkt_m.group('srid'))
                 g = wkt_r().read(force_bytes(wkt_m.group('wkt')))
             elif hex_regex.match(geo_input):
                 # Handling HEXEWKB input.
@@ -75,14 +76,17 @@ class GEOSGeometry(GEOSBase, ListMixin):
             # Invalid geometry type.
             raise TypeError('Improper geometry input type: %s' % type(geo_input))
 
-        if g:
-            # Setting the pointer object with a valid pointer.
-            self.ptr = g
-        else:
+        if not g:
             raise GEOSException('Could not initialize GEOS Geometry with given input.')
 
+        input_srid = input_srid or capi.geos_get_srid(g) or None
+        if input_srid and srid and input_srid != srid:
+            raise ValueError('Input geometry already has SRID: %d.' % input_srid)
+
+        # Setting the pointer object with a valid pointer.
+        self.ptr = g
         # Post-initialization setup.
-        self._post_init(srid)
+        self._post_init(input_srid or srid)
 
     def _post_init(self, srid):
         "Perform post-initialization setup."

tests/gis_tests/geos_tests/test_geos.py
@@ -708,6 +708,14 @@ class GEOSTest(SimpleTestCase, TestDataMixin):
         pnt_wo_srid = Point(1, 1)
         pnt_wo_srid.srid = pnt_wo_srid.srid
 
+        # Input geometries that have an SRID.
+        self.assertEqual(GEOSGeometry(pnt.ewkt, srid=pnt.srid).srid, pnt.srid)
+        self.assertEqual(GEOSGeometry(pnt.ewkb, srid=pnt.srid).srid, pnt.srid)
+        with self.assertRaisesMessage(ValueError, 'Input geometry already has SRID: %d.' % pnt.srid):
+            GEOSGeometry(pnt.ewkt, srid=1)
+        with self.assertRaisesMessage(ValueError, 'Input geometry already has SRID: %d.' % pnt.srid):
+            GEOSGeometry(pnt.ewkb, srid=1)
+
     @skipUnless(HAS_GDAL, "GDAL is required.")
     def test_custom_srid(self):
         """Test with a null srid and a srid unknown to GDAL."""

commit c90b77f7e7d4db6ecc068b94e466c3ba3a7dbeb9

Fixed #29116 -- Fixed OpenLayersWidget deserialization ignoring the widget map's SRID.
Regression in c5b60fcabd1cf48c90bb324b266aa097e4680904.

django/contrib/gis/forms/fields.py
@@ -35,9 +35,14 @@ class GeometryField(forms.Field):
             return None
 
         if not isinstance(value, GEOSGeometry):
-            try:
-                value = GEOSGeometry(value)
-            except (GEOSException, ValueError, TypeError):
+            if hasattr(self.widget, 'deserialize'):
+                value = self.widget.deserialize(value)
+            else:
+                try:
+                    value = GEOSGeometry(value)
+                except (GEOSException, ValueError, TypeError):
+                    value = None
+            if value is None:
                 raise forms.ValidationError(self.error_messages['invalid_geom'], code='invalid_geom')
 
         # Try to set the srid

django/contrib/gis/forms/widgets.py
@@ -2,6 +2,7 @@ import logging
 
 from django.conf import settings
 from django.contrib.gis import gdal
+from django.contrib.gis.geometry import json_regex
 from django.contrib.gis.geos import GEOSException, GEOSGeometry
 from django.forms.widgets import Widget
 from django.utils import translation
@@ -36,7 +37,7 @@ class BaseGeometryWidget(Widget):
     def deserialize(self, value):
         try:
             return GEOSGeometry(value)
-        except (GEOSException, ValueError) as err:
+        except (GEOSException, ValueError, TypeError) as err:
             logger.error("Error creating geometry from value '%s' (%s)", value, err)
         return None
 
@@ -90,6 +91,13 @@ class OpenLayersWidget(BaseGeometryWidget):
 
     def serialize(self, value):
         return value.json if value else ''
+
+    def deserialize(self, value):
+        geom = super().deserialize(value)
+        # GeoJSON assumes WGS84 (4326). Use the map's SRID instead.
+        if geom and json_regex.match(value) and self.map_srid != 4326:
+            geom.srid = self.map_srid
+        return geom
 
 
 class OSMWidget(OpenLayersWidget):

tests/gis_tests/test_geoforms.py
@@ -1,7 +1,7 @@
 import re
 
 from django.contrib.gis import forms
-from django.contrib.gis.forms import BaseGeometryWidget
+from django.contrib.gis.forms import BaseGeometryWidget, OpenLayersWidget
 from django.contrib.gis.geos import GEOSGeometry
 from django.forms import ValidationError
 from django.test import SimpleTestCase, override_settings
@@ -31,8 +31,9 @@ class GeometryFieldTest(SimpleTestCase):
         fld = forms.GeometryField(srid=32140)
         tol = 0.0000001
         xform_geom = GEOSGeometry('POINT (951640.547328465 4219369.26171664)', srid=32140)
-        # The cleaned geometry should be transformed to 32140.
-        cleaned_geom = fld.clean('SRID=4326;POINT (-95.363151 29.763374)')
+        # The cleaned geometry is transformed to 32140 (the widget map_srid is 3857).
+        cleaned_geom = fld.clean('SRID=3857;POINT (-10615777.40976205 3473169.895707852)')
+        self.assertEqual(cleaned_geom.srid, 32140)
         self.assertTrue(xform_geom.equals_exact(cleaned_geom, tol))
 
     def test_null(self):
@@ -83,6 +84,11 @@ class GeometryFieldTest(SimpleTestCase):
                 with self.assertRaises(forms.ValidationError):
                     fld.to_python(wkt)
 
+    def test_to_python_different_map_srid(self):
+        f = forms.GeometryField(widget=OpenLayersWidget)
+        json = '{ "type": "Point", "coordinates": [ 5.0, 23.0 ] }'
+        self.assertEqual(GEOSGeometry('POINT(5 23)', srid=f.widget.map_srid), f.to_python(json))
+
     def test_field_with_text_widget(self):
         class PointForm(forms.Form):
             pt = forms.PointField(srid=4326, widget=forms.TextInput)
@@ -91,6 +97,8 @@ class GeometryFieldTest(SimpleTestCase):
         cleaned_pt = form.fields['pt'].clean('POINT(5 23)')
         self.assertEqual(cleaned_pt, GEOSGeometry('POINT(5 23)', srid=4326))
         self.assertEqual(4326, cleaned_pt.srid)
+        with self.assertRaisesMessage(ValidationError, 'Invalid geometry value.'):
+            form.fields['pt'].clean('POINT(5)')
 
         point = GEOSGeometry('SRID=4326;POINT(5 23)')
         form = PointForm(data={'pt': 'POINT(5 23)'}, initial={'pt': point})
@@ -132,8 +140,9 @@ class GeometryFieldTest(SimpleTestCase):
             ' rows="10" name="pt3"></textarea>',
             output
         )
-        # Only the invalid PNT(0) should trigger an error log entry
-        self.assertEqual(len(logger_calls), 1)
+        # Only the invalid PNT(0) triggers an error log entry.
+        # Deserialization is called in form clean and in widget rendering.
+        self.assertEqual(len(logger_calls), 2)
         self.assertEqual(
             logger_calls[0],
             "Error creating geometry from value 'PNT(0)' (String input "