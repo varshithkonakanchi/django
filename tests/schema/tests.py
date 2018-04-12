varshithkonakanchi/django

commit 2ce0dac84364a144fd1d54731c5e06b3d75d3e3e

Fixed #28305 -- Fixed "Cannot change column 'x': used in a foreign key constraint" crash on MySQL with a sequence of AlterField or RenameField operations.
Regression in 0b9b539e9709b655d775ff2a8873c3de18abf447.

django/db/backends/base/schema.py
@@ -536,9 +536,15 @@ class BaseDatabaseSchemaEditor:
                 ))
             for constraint_name in constraint_names:
                 self.execute(self._delete_constraint_sql(self.sql_delete_unique, model, constraint_name))
-        # Drop incoming FK constraints if we're a primary key and things are going
-        # to change.
-        if old_field.primary_key and new_field.primary_key and old_type != new_type:
+        # Drop incoming FK constraints if the field is a primary key or unique,
+        # which might be a to_field target, and things are going to change.
+        drop_foreign_keys = (
+            (
+                (old_field.primary_key and new_field.primary_key) or
+                (old_field.unique and new_field.unique)
+            ) and old_type != new_type
+        )
+        if drop_foreign_keys:
             # '_meta.related_field' also contains M2M reverse fields, these
             # will be filtered out
             for _old_rel, new_rel in _related_non_m2m_objects(old_field, new_field):
@@ -727,7 +733,7 @@ class BaseDatabaseSchemaEditor:
                 new_field.db_constraint):
             self.execute(self._create_fk_sql(model, new_field, "_fk_%(to_table)s_%(to_column)s"))
         # Rebuild FKs that pointed to us if we previously had to drop them
-        if old_field.primary_key and new_field.primary_key and old_type != new_type:
+        if drop_foreign_keys:
             for rel in new_field.model._meta.related_objects:
                 if not rel.many_to_many and rel.field.db_constraint:
                     self.execute(self._create_fk_sql(rel.related_model, rel.field, "_fk"))

django/db/migrations/operations/fields.py
@@ -3,6 +3,7 @@ from django.db.models.fields import NOT_PROVIDED
 from django.utils.functional import cached_property
 
 from .base import Operation
+from .utils import is_referenced_by_foreign_key
 
 
 class FieldOperation(Operation):
@@ -196,8 +197,12 @@ class AlterField(FieldOperation):
         ]
         # TODO: investigate if old relational fields must be reloaded or if it's
         # sufficient if the new field is (#27737).
-        # Delay rendering of relationships if it's not a relational field
-        delay = not field.is_relation
+        # Delay rendering of relationships if it's not a relational field and
+        # not referenced by a foreign key.
+        delay = (
+            not field.is_relation and
+            not is_referenced_by_foreign_key(state, self.model_name_lower, self.field, self.name)
+        )
         state.reload_model(app_label, self.model_name_lower, delay=delay)
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
@@ -268,8 +273,12 @@ class RenameField(FieldOperation):
         for index, (name, field) in enumerate(fields):
             if name == self.old_name:
                 fields[index] = (self.new_name, field)
-                # Delay rendering of relationships if it's not a relational field.
-                delay = not field.is_relation
+                # Delay rendering of relationships if it's not a relational
+                # field and not referenced by a foreign key.
+                delay = (
+                    not field.is_relation and
+                    not is_referenced_by_foreign_key(state, self.model_name_lower, field, self.name)
+                )
                 break
         else:
             raise FieldDoesNotExist(

django/db/migrations/operations/utils.py
@@ -1,0 +1,9 @@
+def is_referenced_by_foreign_key(state, model_name_lower, field, field_name):
+    for state_app_label, state_model in state.models:
+        for _, f in state.models[state_app_label, state_model].fields:
+            if (f.related_model and
+                    '%s.%s' % (state_app_label, model_name_lower) == f.related_model.lower() and
+                    hasattr(f, 'to_fields')):
+                if (f.to_fields[0] is None and field.primary_key) or field_name in f.to_fields:
+                    return True
+    return False

tests/migrations/test_operations.py
@@ -1322,6 +1322,83 @@ class OperationTests(OperationTestBase):
             operation.database_backwards("test_alflpkfk", editor, new_state, project_state)
         assertIdTypeEqualsFkType()
 
+    def test_alter_field_reloads_state_on_fk_target_changes(self):
+        """
+        If AlterField doesn't reload state appropriately, the second AlterField
+        crashes on MySQL due to not dropping the PonyRider.pony foreign key
+        constraint before modifying the column.
+        """
+        app_label = 'alter_alter_field_reloads_state_on_fk_target_changes'
+        project_state = self.apply_operations(app_label, ProjectState(), operations=[
+            migrations.CreateModel('Rider', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+            ]),
+            migrations.CreateModel('Pony', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+                ('rider', models.ForeignKey('%s.Rider' % app_label, models.CASCADE)),
+            ]),
+            migrations.CreateModel('PonyRider', fields=[
+                ('id', models.AutoField(primary_key=True)),
+                ('pony', models.ForeignKey('%s.Pony' % app_label, models.CASCADE)),
+            ]),
+        ])
+        project_state = self.apply_operations(app_label, project_state, operations=[
+            migrations.AlterField('Rider', 'id', models.CharField(primary_key=True, max_length=99)),
+            migrations.AlterField('Pony', 'id', models.CharField(primary_key=True, max_length=99)),
+        ])
+
+    def test_alter_field_reloads_state_on_fk_with_to_field_target_changes(self):
+        """
+        If AlterField doesn't reload state appropriately, the second AlterField
+        crashes on MySQL due to not dropping the PonyRider.pony foreign key
+        constraint before modifying the column.
+        """
+        app_label = 'alter_alter_field_reloads_state_on_fk_with_to_field_target_changes'
+        project_state = self.apply_operations(app_label, ProjectState(), operations=[
+            migrations.CreateModel('Rider', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+                ('slug', models.CharField(unique=True, max_length=100)),
+            ]),
+            migrations.CreateModel('Pony', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+                ('rider', models.ForeignKey('%s.Rider' % app_label, models.CASCADE, to_field='slug')),
+                ('slug', models.CharField(unique=True, max_length=100)),
+            ]),
+            migrations.CreateModel('PonyRider', fields=[
+                ('id', models.AutoField(primary_key=True)),
+                ('pony', models.ForeignKey('%s.Pony' % app_label, models.CASCADE, to_field='slug')),
+            ]),
+        ])
+        project_state = self.apply_operations(app_label, project_state, operations=[
+            migrations.AlterField('Rider', 'slug', models.CharField(unique=True, max_length=99)),
+            migrations.AlterField('Pony', 'slug', models.CharField(unique=True, max_length=99)),
+        ])
+
+    def test_rename_field_reloads_state_on_fk_target_changes(self):
+        """
+        If RenameField doesn't reload state appropriately, the AlterField
+        crashes on MySQL due to not dropping the PonyRider.pony foreign key
+        constraint before modifying the column.
+        """
+        app_label = 'alter_rename_field_reloads_state_on_fk_target_changes'
+        project_state = self.apply_operations(app_label, ProjectState(), operations=[
+            migrations.CreateModel('Rider', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+            ]),
+            migrations.CreateModel('Pony', fields=[
+                ('id', models.CharField(primary_key=True, max_length=100)),
+                ('rider', models.ForeignKey('%s.Rider' % app_label, models.CASCADE)),
+            ]),
+            migrations.CreateModel('PonyRider', fields=[
+                ('id', models.AutoField(primary_key=True)),
+                ('pony', models.ForeignKey('%s.Pony' % app_label, models.CASCADE)),
+            ]),
+        ])
+        project_state = self.apply_operations(app_label, project_state, operations=[
+            migrations.RenameField('Rider', 'id', 'id2'),
+            migrations.AlterField('Pony', 'id', models.CharField(primary_key=True, max_length=99)),
+        ])
+
     def test_rename_field(self):
         """
         Tests the RenameField operation.

commit ce68ead4503e7ecf4088cc6185236b79d40f97d5

Fixed #29193 -- Prevented unnecessary foreign key drops when altering a unique field.
Stopped dropping and recreating foreign key constraints on other fields
in the same table as the one which is actually being altered in an
AlterField operation.

Regression in 2ce0dac84364a144fd1d54731c5e06b3d75d3e3e.

AUTHORS
@@ -382,6 +382,7 @@ answer newbie questions, and generally made Django that much better:
     Jensen Cochran <jensen.cochran@gmail.com>
     Jeong-Min Lee <falsetru@gmail.com>
     Jérémie Blaser <blaserje@gmail.com>
+    Jeremy Bowman <https://github.com/jmbowman>
     Jeremy Carbaugh <jcarbaugh@gmail.com>
     Jeremy Dunck <jdunck@gmail.com>
     Jeremy Lainé <jeremy.laine@m4x.org>

django/db/backends/base/schema.py
@@ -14,12 +14,28 @@ from django.utils.encoding import force_bytes
 logger = logging.getLogger('django.db.backends.schema')
 
 
+def _is_relevant_relation(relation, altered_field):
+    """
+    When altering the given field, must constraints on its model from the given
+    relation be temporarily dropped?
+    """
+    field = relation.field
+    if field.many_to_many:
+        # M2M reverse field
+        return False
+    if altered_field.primary_key and field.to_fields == [None]:
+        # Foreign key constraint on the primary key, which is being altered.
+        return True
+    # Is the constraint targeting the field being altered?
+    return altered_field.name in field.to_fields
+
+
 def _related_non_m2m_objects(old_field, new_field):
     # Filter out m2m objects from reverse relations.
     # Return (old_relation, new_relation) tuples.
     return zip(
-        (obj for obj in old_field.model._meta.related_objects if not obj.field.many_to_many),
-        (obj for obj in new_field.model._meta.related_objects if not obj.field.many_to_many)
+        (obj for obj in old_field.model._meta.related_objects if _is_relevant_relation(obj, old_field)),
+        (obj for obj in new_field.model._meta.related_objects if _is_relevant_relation(obj, new_field))
     )
 
 
@@ -731,7 +747,7 @@ class BaseDatabaseSchemaEditor:
         # Rebuild FKs that pointed to us if we previously had to drop them
         if drop_foreign_keys:
             for rel in new_field.model._meta.related_objects:
-                if not rel.many_to_many and rel.field.db_constraint:
+                if _is_relevant_relation(rel, new_field) and rel.field.db_constraint:
                     self.execute(self._create_fk_sql(rel.related_model, rel.field, "_fk"))
         # Does it have check constraints we need to add?
         if old_db_params['check'] != new_db_params['check'] and new_db_params['check']:

tests/schema/tests.py
@@ -1546,6 +1546,40 @@ class SchemaTests(TransactionTestCase):
         Tag.objects.all().delete()
 
     @isolate_apps('schema')
+    @unittest.skipIf(connection.vendor == 'sqlite', 'SQLite naively remakes the table on field alteration.')
+    @skipUnlessDBFeature('supports_foreign_keys')
+    def test_unique_no_unnecessary_fk_drops(self):
+        """
+        If AlterField isn't selective about dropping foreign key constraints
+        when modifying a field with a unique constraint, the AlterField
+        incorrectly drops and recreates the Book.author foreign key even though
+        it doesn't restrict the field being changed (#29193).
+        """
+        class Author(Model):
+            name = CharField(max_length=254, unique=True)
+
+            class Meta:
+                app_label = 'schema'
+
+        class Book(Model):
+            author = ForeignKey(Author, CASCADE)
+
+            class Meta:
+                app_label = 'schema'
+
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+            editor.create_model(Book)
+        new_field = CharField(max_length=255, unique=True)
+        new_field.model = Author
+        new_field.set_attributes_from_name('name')
+        with patch_logger('django.db.backends.schema', 'debug') as logger_calls:
+            with connection.schema_editor() as editor:
+                editor.alter_field(Author, Author._meta.get_field('name'), new_field)
+            # One SQL statement is executed to alter the field.
+            self.assertEqual(len(logger_calls), 1)
+
+    @isolate_apps('schema')
     @unittest.skipIf(connection.vendor == 'sqlite', 'SQLite remakes the table on field alteration.')
     def test_unique_and_reverse_m2m(self):
         """