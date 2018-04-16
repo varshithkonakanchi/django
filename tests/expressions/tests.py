varshithkonakanchi/django

commit e9abea125dd9a91d7b5476573f0a49c272167cc4

Fixed #27857 -- Dropped support for Python 3.4.

django/db/models/expressions.py
@@ -150,12 +150,6 @@ class BaseExpression:
         if output_field is not None:
             self.output_field = output_field
 
-    def __getstate__(self):
-        # This method required only for Python 3.4.
-        state = self.__dict__.copy()
-        state.pop('convert_value', None)
-        return state
-
     def get_db_converters(self, connection):
         return (
             []

tests/handlers/tests.py
@@ -1,5 +1,3 @@
-import unittest
-
 from django.core.exceptions import ImproperlyConfigured
 from django.core.handlers.wsgi import WSGIHandler, WSGIRequest, get_script_name
 from django.core.signals import request_finished, request_started
@@ -7,11 +5,6 @@ from django.db import close_old_connections, connection
 from django.test import (
     RequestFactory, SimpleTestCase, TransactionTestCase, override_settings,
 )
-
-try:
-    from http import HTTPStatus
-except ImportError:  # Python < 3.5
-    HTTPStatus = None
 
 
 class HandlerTests(SimpleTestCase):
@@ -182,7 +175,6 @@ class HandlerRequestTests(SimpleTestCase):
         environ = RequestFactory().get('/%E2%A8%87%87%A5%E2%A8%A0').environ
         self.assertIsInstance(environ['PATH_INFO'], str)
 
-    @unittest.skipIf(HTTPStatus is None, 'HTTPStatus only exists on Python 3.5+')
     def test_handle_accepts_httpstatus_enum_value(self):
         def start_response(status, headers):
             start_response.status = status

tests/mail/tests.py
@@ -61,10 +61,7 @@ class MailTests(HeadersCheckMixin, SimpleTestCase):
 
         def iter_attachments():
             for i in email_message.walk():
-                # Once support for Python<3.5 has been dropped, we can use
-                # i.get_content_disposition() here instead.
-                content_disposition = i.get('content-disposition', '').split(';')[0].lower()
-                if content_disposition == 'attachment':
+                if i.get_content_disposition() == 'attachment':
                     filename = i.get_filename()
                     content = i.get_payload(decode=True)
                     mimetype = i.get_content_type()
@@ -1161,8 +1158,8 @@ class FakeSMTPServer(smtpd.SMTPServer, threading.Thread):
     def __init__(self, *args, **kwargs):
         threading.Thread.__init__(self)
         # New kwarg added in Python 3.5; default switching to False in 3.6.
-        if sys.version_info >= (3, 5):
-            kwargs['decode_data'] = True
+        # Setting a value only silences a deprecation warning in Python 3.5.
+        kwargs['decode_data'] = True
         smtpd.SMTPServer.__init__(self, *args, **kwargs)
         self._sink = []
         self.active = False

tests/servers/tests.py
@@ -5,7 +5,7 @@ import errno
 import os
 import socket
 import sys
-from http.client import HTTPConnection
+from http.client import HTTPConnection, RemoteDisconnected
 from urllib.error import HTTPError
 from urllib.parse import urlencode
 from urllib.request import urlopen
@@ -13,11 +13,6 @@ from urllib.request import urlopen
 from django.test import LiveServerTestCase, override_settings
 
 from .models import Person
-
-try:
-    from http.client import RemoteDisconnected
-except ImportError:  # Python 3.4
-    from http.client import BadStatusLine as RemoteDisconnected
 
 TEST_ROOT = os.path.dirname(__file__)
 TEST_SETTINGS = {

tests/sessions_tests/tests.py
@@ -2,7 +2,6 @@ import base64
 import os
 import shutil
 import string
-import sys
 import tempfile
 import unittest
 from datetime import timedelta
@@ -733,10 +732,9 @@ class SessionMiddlewareTests(TestCase):
         # A deleted cookie header looks like:
         #  Set-Cookie: sessionid=; expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/
         self.assertEqual(
-            'Set-Cookie: {}={}; expires=Thu, 01-Jan-1970 00:00:00 GMT; '
+            'Set-Cookie: {}=""; expires=Thu, 01-Jan-1970 00:00:00 GMT; '
             'Max-Age=0; Path=/'.format(
                 settings.SESSION_COOKIE_NAME,
-                '""' if sys.version_info >= (3, 5) else '',
             ),
             str(response.cookies[settings.SESSION_COOKIE_NAME])
         )
@@ -763,10 +761,9 @@ class SessionMiddlewareTests(TestCase):
         #              expires=Thu, 01-Jan-1970 00:00:00 GMT; Max-Age=0;
         #              Path=/example/
         self.assertEqual(
-            'Set-Cookie: {}={}; Domain=.example.local; expires=Thu, '
+            'Set-Cookie: {}=""; Domain=.example.local; expires=Thu, '
             '01-Jan-1970 00:00:00 GMT; Max-Age=0; Path=/example/'.format(
                 settings.SESSION_COOKIE_NAME,
-                '""' if sys.version_info >= (3, 5) else '',
             ),
             str(response.cookies[settings.SESSION_COOKIE_NAME])
         )

tests/test_utils/tests.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import unittest
 from io import StringIO
 from unittest import mock
@@ -684,9 +683,6 @@ class HTMLEqualTests(SimpleTestCase):
         error_msg = (
             "First argument is not valid HTML:\n"
             "('Unexpected end tag `div` (Line 1, Column 6)', (1, 6))"
-        ) if sys.version_info >= (3, 5) else (
-            "First argument is not valid HTML:\n"
-            "Unexpected end tag `div` (Line 1, Column 6), at line 1, column 7"
         )
         with self.assertRaisesMessage(AssertionError, error_msg):
             self.assertHTMLEqual('< div></ div>', '<div></div>')

INSTALL
@@ -1,6 +1,6 @@
 Thanks for downloading Django.
 
-To install it, make sure you have Python 3.4 or greater installed. Then run
+To install it, make sure you have Python 3.5 or greater installed. Then run
 this command from the command prompt:
 
     python setup.py install

django/http/cookie.py
@@ -1,20 +1,7 @@
-import sys
 from http import cookies
 
-# Cookie pickling bug is fixed in Python 3.4.3+
-# http://bugs.python.org/issue22775
-if sys.version_info >= (3, 4, 3):
-    SimpleCookie = cookies.SimpleCookie
-else:
-    Morsel = cookies.Morsel
-
-    class SimpleCookie(cookies.SimpleCookie):
-        def __setitem__(self, key, value):
-            if isinstance(value, Morsel):
-                # allow assignment of constructed Morsels (e.g. for pickling)
-                dict.__setitem__(self, key, value)
-            else:
-                super().__setitem__(key, value)
+# For backwards compatibility in Django 2.1.
+SimpleCookie = cookies.SimpleCookie
 
 
 def parse_cookie(cookie):

commit e6e693f882df331c83365c49ffcf2eebd1fefa71

Fixed #29330 -- Fixed crash when pickling BaseExpression.
Regression in e9abea125dd9a91d7b5476573f0a49c272167cc4.

django/db/models/expressions.py
@@ -153,6 +153,11 @@ class BaseExpression:
         if output_field is not None:
             self.output_field = output_field
 
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state.pop('convert_value', None)
+        return state
+
     def get_db_converters(self, connection):
         return (
             []

tests/expressions/tests.py
@@ -1,4 +1,5 @@
 import datetime
+import pickle
 import unittest
 import uuid
 from copy import deepcopy
@@ -584,6 +585,11 @@ class BasicExpressionsTests(TestCase):
         inner = Company.objects.filter(num_employees=OuterRef('ceo__salary') + 2)
         outer = Company.objects.filter(pk__in=Subquery(inner.values('pk')))
         self.assertEqual(outer.get().name, 'Test GmbH')
+
+    def test_pickle_expression(self):
+        expr = Value(1, output_field=models.IntegerField())
+        expr.convert_value  # populate cached property
+        self.assertEqual(pickle.loads(pickle.dumps(expr)), expr)
 
 
 class IterableLookupInnerExpressionsTests(TestCase):