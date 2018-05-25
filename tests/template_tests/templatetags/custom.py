varshithkonakanchi/django

commit 5b1174be35e31cae387e48a391bd0a8c0152f0ce

Replaced django.utils.inspect.getargspec() with inspect.getfullargspec().

django/template/base.py
@@ -49,16 +49,15 @@ times with multiple contexts)
 '<html></html>'
 """
 
-import inspect
 import logging
 import re
+from inspect import getcallargs, getfullargspec
 
 from django.template.context import (  # NOQA: imported for backwards compatibility
     BaseContext, Context, ContextPopException, RequestContext,
 )
 from django.utils.formats import localize
 from django.utils.html import conditional_escape, escape
-from django.utils.inspect import getargspec
 from django.utils.safestring import SafeData, mark_safe
 from django.utils.text import (
     get_text_list, smart_split, unescape_string_literal,
@@ -715,7 +714,7 @@ class FilterExpression:
         # Check to see if a decorator is providing the real function.
         func = getattr(func, '_decorated_function', func)
 
-        args, _, _, defaults = getargspec(func)
+        args, _, _, defaults, _, _, _ = getfullargspec(func)
         alen = len(args)
         dlen = len(defaults or [])
         # Not enough OR Too many
@@ -865,7 +864,7 @@ class Variable:
                             current = current()
                         except TypeError:
                             try:
-                                inspect.getcallargs(current)
+                                getcallargs(current)
                             except TypeError:  # arguments *were* required
                                 current = context.template.engine.string_if_invalid  # invalid method call
                             else:

django/template/library.py
@@ -1,8 +1,8 @@
 import functools
 from importlib import import_module
+from inspect import getfullargspec
 
 from django.utils.html import conditional_escape
-from django.utils.inspect import getargspec
 from django.utils.itercompat import is_iterable
 
 from .base import Node, Template, token_kwargs
@@ -106,7 +106,7 @@ class Library:
             return 'world'
         """
         def dec(func):
-            params, varargs, varkw, defaults = getargspec(func)
+            params, varargs, varkw, defaults, _, _, _ = getfullargspec(func)
             function_name = (name or getattr(func, '_decorated_function', func).__name__)
 
             @functools.wraps(func)
@@ -143,7 +143,7 @@ class Library:
             return {'choices': choices}
         """
         def dec(func):
-            params, varargs, varkw, defaults = getargspec(func)
+            params, varargs, varkw, defaults, _, _, _ = getfullargspec(func)
             function_name = (name or getattr(func, '_decorated_function', func).__name__)
 
             @functools.wraps(func)

django/utils/inspect.py
@@ -1,29 +1,6 @@
 import inspect
 
 
-def getargspec(func):
-    sig = inspect.signature(func)
-    args = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
-    ]
-    varargs = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.VAR_POSITIONAL
-    ]
-    varargs = varargs[0] if varargs else None
-    varkw = [
-        p.name for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.VAR_KEYWORD
-    ]
-    varkw = varkw[0] if varkw else None
-    defaults = [
-        p.default for p in sig.parameters.values()
-        if p.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD and p.default is not p.empty
-    ] or None
-    return args, varargs, varkw, defaults
-
-
 def get_func_args(func):
     sig = inspect.signature(func)
     return [

commit 7f364daafb796e97170490a998124a2861525d4a

Fixed #29400 -- Fixed crash in custom template filters that use decorated functions.
Regression in 5b1174be35e31cae387e48a391bd0a8c0152f0ce.

AUTHORS
@@ -722,6 +722,7 @@ answer newbie questions, and generally made Django that much better:
     ryankanno
     Ryan Kelly <ryan@rfk.id.au>
     Ryan Niemeyer <https://profiles.google.com/ryan.niemeyer/about>
+    Ryan Rubin <ryanmrubin@gmail.com>
     Ryno Mathee <rmathee@gmail.com>
     Sam Newman <http://www.magpiebrain.com/>
     Sander Dijkhuis <sander.dijkhuis@gmail.com>

django/template/base.py
@@ -53,7 +53,7 @@ times with multiple contexts)
 import logging
 import re
 from enum import Enum
-from inspect import getcallargs, getfullargspec
+from inspect import getcallargs, getfullargspec, unwrap
 
 from django.template.context import (  # NOQA: imported for backwards compatibility
     BaseContext, Context, ContextPopException, RequestContext,
@@ -707,7 +707,7 @@ class FilterExpression:
         # First argument, filter input, is implied.
         plen = len(provided) + 1
         # Check to see if a decorator is providing the real function.
-        func = getattr(func, '_decorated_function', func)
+        func = unwrap(func)
 
         args, _, _, defaults, _, _, _ = getfullargspec(func)
         alen = len(args)

tests/template_tests/templatetags/custom.py
@@ -3,6 +3,7 @@ import operator
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.html import escape, format_html
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -11,6 +12,13 @@ register = template.Library()
 @stringfilter
 def trim(value, num):
     return value[:num]
+
+
+@register.filter
+@mark_safe
+def make_data_div(value):
+    """A filter that uses a decorator (@mark_safe)."""
+    return '<div data-name="%s"></div>' % value
 
 
 @register.filter

tests/template_tests/test_custom.py
@@ -25,6 +25,11 @@ class CustomFilterTests(SimpleTestCase):
             "abcde"
         )
 
+    def test_decorated_filter(self):
+        engine = Engine(libraries=LIBRARIES)
+        t = engine.from_string('{% load custom %}{{ name|make_data_div }}')
+        self.assertEqual(t.render(Context({'name': 'foo'})), '<div data-name="foo"></div>')
+
 
 class TagTestCase(SimpleTestCase):
 