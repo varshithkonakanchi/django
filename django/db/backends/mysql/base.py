varshithkonakanchi/django

commit 50e6d09a4baf11c6c608748b784b497458805941

Refs #29451 -- Fixed regex/iregex lookups on MySQL 8.

django/db/backends/mysql/base.py
@@ -141,8 +141,6 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         'iexact': 'LIKE %s',
         'contains': 'LIKE BINARY %s',
         'icontains': 'LIKE %s',
-        'regex': 'REGEXP BINARY %s',
-        'iregex': 'REGEXP %s',
         'gt': '> %s',
         'gte': '>= %s',
         'lt': '< %s',

django/db/backends/mysql/operations.py
@@ -282,3 +282,14 @@ class DatabaseOperations(BaseDatabaseOperations):
             # EXTENDED and FORMAT are mutually exclusive options.
             prefix += ' EXTENDED'
         return prefix
+
+    def regex_lookup(self, lookup_type):
+        # REGEXP BINARY doesn't work correctly in MySQL 8+ and REGEXP_LIKE
+        # doesn't exist in MySQL 5.6.
+        if self.connection.mysql_version < (8, 0, 0):
+            if lookup_type == 'regex':
+                return '%s REGEXP BINARY %s'
+            return '%s REGEXP %s'
+
+        match_option = 'c' if lookup_type == 'regex' else 'i'
+        return "REGEXP_LIKE(%%s, %%s, '%s')" % match_option

commit a3ed2edd00eae9c33e265d0b317921a74825e828

Fixed #29544 -- Fixed regex lookup on MariaDB.
Regression in 50e6d09a4baf11c6c608748b784b497458805941.

django/db/backends/mysql/base.py
@@ -326,11 +326,19 @@ class DatabaseWrapper(BaseDatabaseWrapper):
             return True
 
     @cached_property
-    def mysql_version(self):
+    def mysql_server_info(self):
         with self.temporary_connection() as cursor:
             cursor.execute('SELECT VERSION()')
-            server_info = cursor.fetchone()[0]
-        match = server_version_re.match(server_info)
+            return cursor.fetchone()[0]
+
+    @cached_property
+    def mysql_version(self):
+        match = server_version_re.match(self.mysql_server_info)
         if not match:
-            raise Exception('Unable to determine MySQL version from version string %r' % server_info)
+            raise Exception('Unable to determine MySQL version from version string %r' % self.mysql_server_info)
         return tuple(int(x) for x in match.groups())
+
+    @cached_property
+    def mysql_is_mariadb(self):
+        # MariaDB isn't officially supported.
+        return 'mariadb' in self.mysql_server_info.lower()

django/db/backends/mysql/operations.py
@@ -285,8 +285,8 @@ class DatabaseOperations(BaseDatabaseOperations):
 
     def regex_lookup(self, lookup_type):
         # REGEXP BINARY doesn't work correctly in MySQL 8+ and REGEXP_LIKE
-        # doesn't exist in MySQL 5.6.
-        if self.connection.mysql_version < (8, 0, 0):
+        # doesn't exist in MySQL 5.6 or in MariaDB.
+        if self.connection.mysql_version < (8, 0, 0) or self.connection.mysql_is_mariadb:
             if lookup_type == 'regex':
                 return '%s REGEXP BINARY %s'
             return '%s REGEXP %s'