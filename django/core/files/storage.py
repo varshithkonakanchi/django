varshithkonakanchi/django

commit de4bf3ee42f07e3908aca66be2ae31cdd4142b70

Refs #23919 -- Replaced errno checking with PEP 3151 exceptions.

django/core/files/storage.py
@@ -1,4 +1,3 @@
-import errno
 import os
 from datetime import datetime
 from urllib.parse import urljoin
@@ -224,9 +223,6 @@ class FileSystemStorage(Storage):
         full_path = self.path(name)
 
         # Create any intermediate directories that do not exist.
-        # Note that there is a race between os.path.exists and os.makedirs:
-        # if os.makedirs fails with EEXIST, the directory was created
-        # concurrently, and we can continue normally. Refs #16082.
         directory = os.path.dirname(full_path)
         if not os.path.exists(directory):
             try:
@@ -240,9 +236,11 @@ class FileSystemStorage(Storage):
                         os.umask(old_umask)
                 else:
                     os.makedirs(directory)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
+            except FileNotFoundError:
+                # There's a race between os.path.exists() and os.makedirs().
+                # If os.makedirs() fails with FileNotFoundError, the directory
+                # was created concurrently.
+                pass
         if not os.path.isdir(directory):
             raise IOError("%s exists and is not a directory." % directory)
 
@@ -280,13 +278,10 @@ class FileSystemStorage(Storage):
                             _file.close()
                         else:
                             os.close(fd)
-            except OSError as e:
-                if e.errno == errno.EEXIST:
-                    # Ooops, the file exists. We need a new file name.
-                    name = self.get_available_name(name)
-                    full_path = self.path(name)
-                else:
-                    raise
+            except FileExistsError:
+                # A new name is needed if the file exists.
+                name = self.get_available_name(name)
+                full_path = self.path(name)
             else:
                 # OK, the file save worked. Break out of the loop.
                 break
@@ -301,13 +296,12 @@ class FileSystemStorage(Storage):
         assert name, "The name argument is not allowed to be empty."
         name = self.path(name)
         # If the file exists, delete it from the filesystem.
-        # If os.remove() fails with ENOENT, the file may have been removed
-        # concurrently, and it's safe to continue normally.
         try:
             os.remove(name)
-        except OSError as e:
-            if e.errno != errno.ENOENT:
-                raise
+        except FileNotFoundError:
+            # If os.remove() fails with FileNotFoundError, the file may have
+            # been removed concurrently.
+            pass
 
     def exists(self, name):
         return os.path.exists(self.path(name))

tests/file_storage/tests.py
@@ -1,4 +1,3 @@
-import errno
 import os
 import shutil
 import sys
@@ -416,9 +415,9 @@ class FileStorageTests(SimpleTestCase):
                 real_makedirs(path)
             elif path == os.path.join(self.temp_dir, 'raced'):
                 real_makedirs(path)
-                raise OSError(errno.EEXIST, 'simulated EEXIST')
+                raise FileNotFoundError()
             elif path == os.path.join(self.temp_dir, 'error'):
-                raise OSError(errno.EACCES, 'simulated EACCES')
+                raise FileExistsError()
             else:
                 self.fail('unexpected argument %r' % path)
 
@@ -433,8 +432,8 @@ class FileStorageTests(SimpleTestCase):
             with self.storage.open('raced/test.file') as f:
                 self.assertEqual(f.read(), b'saved with race')
 
-            # OSErrors aside from EEXIST are still raised.
-            with self.assertRaises(OSError):
+            # Exceptions aside from FileNotFoundError are raised.
+            with self.assertRaises(FileExistsError):
                 self.storage.save('error/test.file', ContentFile('not saved'))
         finally:
             os.makedirs = real_makedirs
@@ -452,9 +451,9 @@ class FileStorageTests(SimpleTestCase):
                 real_remove(path)
             elif path == os.path.join(self.temp_dir, 'raced.file'):
                 real_remove(path)
-                raise OSError(errno.ENOENT, 'simulated ENOENT')
+                raise FileNotFoundError()
             elif path == os.path.join(self.temp_dir, 'error.file'):
-                raise OSError(errno.EACCES, 'simulated EACCES')
+                raise PermissionError()
             else:
                 self.fail('unexpected argument %r' % path)
 
@@ -469,9 +468,9 @@ class FileStorageTests(SimpleTestCase):
             self.storage.delete('raced.file')
             self.assertFalse(self.storage.exists('normal.file'))
 
-            # OSErrors aside from ENOENT are still raised.
+            # Exceptions aside from FileNotFoundError are raised.
             self.storage.save('error.file', ContentFile('delete with error'))
-            with self.assertRaises(OSError):
+            with self.assertRaises(PermissionError):
                 self.storage.delete('error.file')
         finally:
             os.remove = real_remove

tests/cache/tests.py
@@ -1360,7 +1360,7 @@ class FileBasedCacheTests(BaseCacheTests, TestCase):
         # Returns the default instead of erroring.
         self.assertEqual(cache.get('foo', 'baz'), 'baz')
 
-    def test_get_does_not_ignore_non_enoent_errno_values(self):
+    def test_get_does_not_ignore_non_filenotfound_exceptions(self):
         with mock.patch('builtins.open', side_effect=IOError):
             with self.assertRaises(IOError):
                 cache.get('foo')

tests/file_uploads/tests.py
@@ -1,5 +1,4 @@
 import base64
-import errno
 import hashlib
 import json
 import os
@@ -564,9 +563,8 @@ class DirectoryCreationTests(SimpleTestCase):
         """Permission errors are not swallowed"""
         os.chmod(MEDIA_ROOT, 0o500)
         self.addCleanup(os.chmod, MEDIA_ROOT, 0o700)
-        with self.assertRaises(OSError) as cm:
+        with self.assertRaises(PermissionError):
             self.obj.testfile.save('foo.txt', SimpleUploadedFile('foo.txt', b'x'), save=False)
-        self.assertEqual(cm.exception.errno, errno.EACCES)
 
     def test_not_a_directory(self):
         """The correct IOError is raised when the upload directory name exists but isn't a directory"""

tests/staticfiles_tests/storage.py
@@ -1,4 +1,3 @@
-import errno
 import os
 from datetime import datetime, timedelta
 
@@ -51,9 +50,8 @@ class PathNotImplementedStorage(storage.Storage):
         name = self._path(name)
         try:
             os.remove(name)
-        except OSError as e:
-            if e.errno != errno.ENOENT:
-                raise
+        except FileNotFoundError:
+            pass
 
     def path(self, name):
         raise NotImplementedError

django/contrib/sessions/backends/file.py
@@ -1,5 +1,4 @@
 import datetime
-import errno
 import logging
 import os
 import shutil
@@ -133,13 +132,12 @@ class SessionStore(SessionBase):
                 flags |= os.O_EXCL | os.O_CREAT
             fd = os.open(session_file_name, flags)
             os.close(fd)
-
-        except OSError as e:
-            if must_create and e.errno == errno.EEXIST:
-                raise CreateError
-            if not must_create and e.errno == errno.ENOENT:
+        except FileNotFoundError:
+            if not must_create:
                 raise UpdateError
-            raise
+        except FileExistsError:
+            if must_create:
+                raise CreateError
 
         # Write the session file without interfering with other threads
         # or processes.  By writing to an atomically generated temporary

django/core/cache/backends/filebased.py
@@ -1,5 +1,4 @@
 "File-based cache backend"
-import errno
 import glob
 import hashlib
 import os
@@ -34,9 +33,8 @@ class FileBasedCache(BaseCache):
             with open(fname, 'rb') as f:
                 if not self._is_expired(f):
                     return pickle.loads(zlib.decompress(f.read()))
-        except IOError as e:
-            if e.errno != errno.ENOENT:
-                raise
+        except FileNotFoundError:
+            pass
         return default
 
     def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
@@ -64,11 +62,9 @@ class FileBasedCache(BaseCache):
             return
         try:
             os.remove(fname)
-        except OSError as e:
-            # ENOENT can happen if the cache file is removed (by another
-            # process) after the os.path.exists check.
-            if e.errno != errno.ENOENT:
-                raise
+        except FileNotFoundError:
+            # The file may have been removed by another process.
+            pass
 
     def has_key(self, key, version=None):
         fname = self._key_to_file(key, version)
@@ -99,11 +95,8 @@ class FileBasedCache(BaseCache):
         if not os.path.exists(self._dir):
             try:
                 os.makedirs(self._dir, 0o700)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise EnvironmentError(
-                        "Cache directory '%s' does not exist "
-                        "and could not be created'" % self._dir)
+            except FileExistsError:
+                pass
 
     def _key_to_file(self, key, version=None):
         """

django/core/files/move.py
@@ -71,10 +71,10 @@ def file_move_safe(old_file_name, new_file_name, chunk_size=1024 * 64, allow_ove
 
     try:
         os.remove(old_file_name)
-    except OSError as e:
+    except PermissionError as e:
         # Certain operating systems (Cygwin and Windows)
         # fail when deleting opened files, ignore it.  (For the
         # systems where this happens, temporary files will be auto-deleted
         # on close anyway.)
-        if getattr(e, 'winerror', 0) != 32 and getattr(e, 'errno', 0) != 13:
+        if getattr(e, 'winerror', 0) != 32:
             raise

commit b0519ac55622e78d57039dbb81e054096241026f

Fixed #29890 -- Fixed FileSystemStorage crash if concurrent saves try to create the same directory.
Regression in de4bf3ee42f07e3908aca66be2ae31cdd4142b70.

django/core/files/storage.py
@@ -240,9 +240,9 @@ class FileSystemStorage(Storage):
                         os.umask(old_umask)
                 else:
                     os.makedirs(directory)
-            except FileNotFoundError:
+            except FileExistsError:
                 # There's a race between os.path.exists() and os.makedirs().
-                # If os.makedirs() fails with FileNotFoundError, the directory
+                # If os.makedirs() fails with FileExistsError, the directory
                 # was created concurrently.
                 pass
         if not os.path.isdir(directory):

tests/file_storage/tests.py
@@ -415,9 +415,9 @@ class FileStorageTests(SimpleTestCase):
                 real_makedirs(path)
             elif path == os.path.join(self.temp_dir, 'raced'):
                 real_makedirs(path)
-                raise FileNotFoundError()
-            elif path == os.path.join(self.temp_dir, 'error'):
                 raise FileExistsError()
+            elif path == os.path.join(self.temp_dir, 'error'):
+                raise PermissionError()
             else:
                 self.fail('unexpected argument %r' % path)
 
@@ -432,8 +432,8 @@ class FileStorageTests(SimpleTestCase):
             with self.storage.open('raced/test.file') as f:
                 self.assertEqual(f.read(), b'saved with race')
 
-            # Exceptions aside from FileNotFoundError are raised.
-            with self.assertRaises(FileExistsError):
+            # Exceptions aside from FileExistsError are raised.
+            with self.assertRaises(PermissionError):
                 self.storage.save('error/test.file', ContentFile('not saved'))
         finally:
             os.makedirs = real_makedirs