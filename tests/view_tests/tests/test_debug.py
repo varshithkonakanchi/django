varshithkonakanchi/django

commit cb435529ade375ea8e59d7daea1461205f480849

Refs #29654 -- Replaced three dots with ellipsis character in output strings.

django/views/debug.py
@@ -269,7 +269,7 @@ class ExceptionReporter:
                     v = pprint(v)
                     # Trim large blobs of data
                     if len(v) > 4096:
-                        v = '%s... <trimmed %d bytes string>' % (v[0:4096], len(v))
+                        v = '%s… <trimmed %d bytes string>' % (v[0:4096], len(v))
                     frame_vars.append((k, v))
                 frame['vars'] = frame_vars
             frames[i] = frame

django/contrib/admin/templates/admin/popup_response.html
@@ -1,6 +1,6 @@
 {% load i18n static %}<!DOCTYPE html>
 <html>
-  <head><title>{% trans 'Popup closing...' %}</title></head>
+  <head><title>{% trans 'Popup closing…' %}</title></head>
   <body>
     <script type="text/javascript"
             id="django-admin-popup-response-constants"

django/contrib/admin/templatetags/admin_list.py
@@ -32,7 +32,7 @@ def paginator_number(cl, i):
     Generate an individual page index link in a paginated list.
     """
     if i == DOT:
-        return '... '
+        return '… '
     elif i == cl.page_num:
         return format_html('<span class="this-page">{}</span> ', i + 1)
     else:

django/contrib/gis/utils/layermapping.py
@@ -594,7 +594,7 @@ class LayerMapping:
 
                 # Printing progress information, if requested.
                 if progress and num_feat % progress_interval == 0:
-                    stream.write('Processed %d features, saved %d ...\n' % (num_feat, num_saved))
+                    stream.write('Processed %d features, saved %d …\n' % (num_feat, num_saved))
 
             # Only used for status output purposes -- incremental saving uses the
             # values returned here.

django/core/management/commands/loaddata.py
@@ -226,7 +226,7 @@ class Command(BaseCommand):
         ser_fmts = serializers.get_public_serializer_formats() if ser_fmt is None else [ser_fmt]
 
         if self.verbosity >= 2:
-            self.stdout.write("Loading '%s' fixtures..." % fixture_name)
+            self.stdout.write("Loading '%s' fixtures…" % fixture_name)
 
         if os.path.isabs(fixture_name):
             fixture_dirs = [os.path.dirname(fixture_name)]
@@ -247,7 +247,7 @@ class Command(BaseCommand):
         fixture_files = []
         for fixture_dir in fixture_dirs:
             if self.verbosity >= 2:
-                self.stdout.write("Checking %s for fixtures..." % humanize(fixture_dir))
+                self.stdout.write("Checking %s for fixtures…" % humanize(fixture_dir))
             fixture_files_in_dir = []
             path = os.path.join(fixture_dir, fixture_name)
             for candidate in glob.iglob(glob.escape(path) + '*'):

django/core/management/commands/migrate.py
@@ -250,7 +250,7 @@ class Command(BaseCommand):
             if action == "apply_start":
                 if compute_time:
                     self.start = time.time()
-                self.stdout.write("  Applying %s..." % migration, ending="")
+                self.stdout.write("  Applying %s…" % migration, ending="")
                 self.stdout.flush()
             elif action == "apply_success":
                 elapsed = " (%.3fs)" % (time.time() - self.start) if compute_time else ""
@@ -261,7 +261,7 @@ class Command(BaseCommand):
             elif action == "unapply_start":
                 if compute_time:
                     self.start = time.time()
-                self.stdout.write("  Unapplying %s..." % migration, ending="")
+                self.stdout.write("  Unapplying %s…" % migration, ending="")
                 self.stdout.flush()
             elif action == "unapply_success":
                 elapsed = " (%.3fs)" % (time.time() - self.start) if compute_time else ""
@@ -272,7 +272,7 @@ class Command(BaseCommand):
             elif action == "render_start":
                 if compute_time:
                     self.start = time.time()
-                self.stdout.write("  Rendering model states...", ending="")
+                self.stdout.write("  Rendering model states…", ending="")
                 self.stdout.flush()
             elif action == "render_success":
                 elapsed = " (%.3fs)" % (time.time() - self.start) if compute_time else ""
@@ -308,7 +308,7 @@ class Command(BaseCommand):
 
         # Create the tables for each model
         if self.verbosity >= 1:
-            self.stdout.write("  Creating tables...\n")
+            self.stdout.write("  Creating tables…\n")
         with connection.schema_editor() as editor:
             for app_name, model_list in manifest.items():
                 for model in model_list:
@@ -325,7 +325,7 @@ class Command(BaseCommand):
 
             # Deferred SQL is executed when exiting the editor's context.
             if self.verbosity >= 1:
-                self.stdout.write("    Running deferred SQL...\n")
+                self.stdout.write("    Running deferred SQL…\n")
 
     @staticmethod
     def describe_operation(operation, backwards):

django/core/management/commands/runserver.py
@@ -113,7 +113,7 @@ class Command(BaseCommand):
         shutdown_message = options.get('shutdown_message', '')
         quit_command = 'CTRL-BREAK' if sys.platform == 'win32' else 'CONTROL-C'
 
-        self.stdout.write("Performing system checks...\n\n")
+        self.stdout.write("Performing system checks…\n\n")
         self.check(display_num_errors=True)
         # Need to check migrations here, so can't use the
         # requires_migrations_check attribute.

django/core/management/commands/showmigrations.py
@@ -118,7 +118,7 @@ class Command(BaseCommand):
             for parent in sorted(node.parents):
                 out.append("%s.%s" % parent.key)
             if out:
-                return " ... (%s)" % ", ".join(out)
+                return " … (%s)" % ", ".join(out)
             return ""
 
         for node in plan:

commit ac025b1934fd7abf12812c94b30997a0a9b0d38f

Fixed #30324 -- Forced utf-8 encoding when loading the template for the technical 500 debug page.
Regression in cb43552.
Related to 984f93e.

django/views/debug.py
@@ -328,14 +328,14 @@ class ExceptionReporter:
 
     def get_traceback_html(self):
         """Return HTML version of debug 500 HTTP error page."""
-        with Path(CURRENT_DIR, 'templates', 'technical_500.html').open() as fh:
+        with Path(CURRENT_DIR, 'templates', 'technical_500.html').open(encoding='utf-8') as fh:
             t = DEBUG_ENGINE.from_string(fh.read())
         c = Context(self.get_traceback_data(), use_l10n=False)
         return t.render(c)
 
     def get_traceback_text(self):
         """Return plain text version of debug 500 HTTP error page."""
-        with Path(CURRENT_DIR, 'templates', 'technical_500.txt').open() as fh:
+        with Path(CURRENT_DIR, 'templates', 'technical_500.txt').open(encoding='utf-8') as fh:
             t = DEBUG_ENGINE.from_string(fh.read())
         c = Context(self.get_traceback_data(), autoescape=False, use_l10n=False)
         return t.render(c)

tests/view_tests/tests/test_debug.py
@@ -7,6 +7,7 @@ import tempfile
 import threading
 from io import StringIO
 from pathlib import Path
+from unittest import mock
 
 from django.core import mail
 from django.core.files.uploadedfile import SimpleUploadedFile
@@ -20,7 +21,8 @@ from django.utils.functional import SimpleLazyObject
 from django.utils.safestring import mark_safe
 from django.views.debug import (
     CLEANSED_SUBSTITUTE, CallableSettingWrapper, ExceptionReporter,
-    cleanse_setting, technical_500_response,
+    Path as DebugPath, cleanse_setting, default_urlconf,
+    technical_404_response, technical_500_response,
 )
 
 from ..views import (
@@ -647,6 +649,20 @@ class ExceptionReporterTests(SimpleTestCase):
 
         text = reporter.get_traceback_text()
         self.assertIn('USER: [unable to retrieve the current user]', text)
+
+    def test_template_encoding(self):
+        """
+        The templates are loaded directly, not via a template loader, and
+        should be opened as utf-8 charset as is the default specified on
+        template engines.
+        """
+        reporter = ExceptionReporter(None, None, None, None)
+        with mock.patch.object(DebugPath, 'open') as m:
+            reporter.get_traceback_html()
+            m.assert_called_once_with(encoding='utf-8')
+            m.reset_mock()
+            reporter.get_traceback_text()
+            m.assert_called_once_with(encoding='utf-8')
 
 
 class PlainTextReportTests(SimpleTestCase):