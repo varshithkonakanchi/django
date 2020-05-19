varshithkonakanchi/django

commit 3ec6ea3a1f748265302d0ca9c286b06655c18024

Fixed #30727 -- Made Subquery pickle without evaluating their QuerySet.
Subquery expression objects, when pickled, were evaluating the QuerySet
objects saved in its _constructor_args attribute.

django/db/models/expressions.py
@@ -1019,6 +1019,11 @@ class Subquery(Expression):
         self.extra = extra
         super().__init__(output_field)
 
+    def __getstate__(self):
+        state = super().__getstate__()
+        state.pop('_constructor_args', None)
+        return state
+
     def get_source_expressions(self):
         return [self.query]
 

tests/queryset_pickle/tests.py
@@ -172,6 +172,41 @@ class PickleabilityTestCase(TestCase):
         m2ms = pickle.loads(pickle.dumps(m2ms))
         self.assertSequenceEqual(m2ms, [m2m])
 
+    def test_pickle_exists_queryset_still_usable(self):
+        group = Group.objects.create(name='group')
+        Event.objects.create(title='event', group=group)
+        groups = Group.objects.annotate(
+            has_event=models.Exists(
+                Event.objects.filter(group_id=models.OuterRef('id')),
+            ),
+        )
+        groups2 = pickle.loads(pickle.dumps(groups))
+        self.assertSequenceEqual(groups2.filter(has_event=True), [group])
+
+    def test_pickle_exists_queryset_not_evaluated(self):
+        group = Group.objects.create(name='group')
+        Event.objects.create(title='event', group=group)
+        groups = Group.objects.annotate(
+            has_event=models.Exists(
+                Event.objects.filter(group_id=models.OuterRef('id')),
+            ),
+        )
+        list(groups)  # evaluate QuerySet.
+        with self.assertNumQueries(0):
+            self.assert_pickles(groups)
+
+    def test_pickle_subquery_queryset_not_evaluated(self):
+        group = Group.objects.create(name='group')
+        Event.objects.create(title='event', group=group)
+        groups = Group.objects.annotate(
+            event_title=models.Subquery(
+                Event.objects.filter(group_id=models.OuterRef('id')).values('title'),
+            ),
+        )
+        list(groups)  # evaluate QuerySet.
+        with self.assertNumQueries(0):
+            self.assert_pickles(groups)
+
     def test_annotation_with_callable_default(self):
         # Happening.when has a callable default of datetime.datetime.now.
         qs = Happening.objects.annotate(latest_time=models.Max('when'))

commit 6f2814e45848d44d0f1e3cc00fc35d659b102642

Fixed #31607 -- Fixed evaluated Subquery equality.
Regression in 3ec6ea3a1f748265302d0ca9c286b06655c18024.

django/db/models/expressions.py
@@ -1021,11 +1021,18 @@ class Subquery(Expression):
     def __init__(self, queryset, output_field=None, **extra):
         self.query = queryset.query
         self.extra = extra
+        # Prevent the QuerySet from being evaluated.
+        self.queryset = queryset._chain(_result_cache=[], prefetch_done=True)
         super().__init__(output_field)
 
     def __getstate__(self):
         state = super().__getstate__()
-        state.pop('_constructor_args', None)
+        args, kwargs = state['_constructor_args']
+        if args:
+            args = (self.queryset, *args[1:])
+        else:
+            kwargs['queryset'] = self.queryset
+        state['_constructor_args'] = args, kwargs
         return state
 
     def get_source_expressions(self):

tests/expressions/tests.py
@@ -511,6 +511,25 @@ class BasicExpressionsTests(TestCase):
             Employee.objects.exclude(company_point_of_contact_set=None).values('pk')
         )
 
+    def test_subquery_eq(self):
+        qs = Employee.objects.annotate(
+            is_ceo=Exists(Company.objects.filter(ceo=OuterRef('pk'))),
+            is_point_of_contact=Exists(
+                Company.objects.filter(point_of_contact=OuterRef('pk')),
+            ),
+            small_company=Exists(
+                queryset=Company.objects.filter(num_employees__lt=200),
+            ),
+        ).filter(is_ceo=True, is_point_of_contact=False, small_company=True)
+        self.assertNotEqual(
+            qs.query.annotations['is_ceo'],
+            qs.query.annotations['is_point_of_contact'],
+        )
+        self.assertNotEqual(
+            qs.query.annotations['is_ceo'],
+            qs.query.annotations['small_company'],
+        )
+
     def test_in_subquery(self):
         # This is a contrived test (and you really wouldn't write this query),
         # but it is a succinct way to test the __in=Subquery() construct.