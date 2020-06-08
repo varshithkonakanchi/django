varshithkonakanchi/django

commit 9b390144a9c34f574135a939c42634a5289c5272

Fixed #31584 -- Fixed crash when chaining values()/values_list() after Exists() annotation and aggregation on Oracle.
Oracle requires the EXISTS expression to be wrapped in a CASE WHEN in
the GROUP BY clause.

Regression in bfbee4d3c0743a167e8bf54cdf1d6e298bfd489d.

django/db/models/expressions.py
@@ -1089,7 +1089,8 @@ class Exists(Subquery):
 
     def select_format(self, compiler, sql, params):
         # Wrap EXISTS() with a CASE WHEN expression if a database backend
-        # (e.g. Oracle) doesn't support boolean expression in the SELECT list.
+        # (e.g. Oracle) doesn't support boolean expression in SELECT or GROUP
+        # BY list.
         if not compiler.connection.features.supports_boolean_expr_in_select_clause:
             sql = 'CASE WHEN {} THEN 1 ELSE 0 END'.format(sql)
         return sql, params

tests/annotations/tests.py
@@ -1,9 +1,7 @@
 import datetime
 from decimal import Decimal
-from unittest import skipIf
 
 from django.core.exceptions import FieldDoesNotExist, FieldError
-from django.db import connection
 from django.db.models import (
     BooleanField, CharField, Count, DateTimeField, Exists, ExpressionWrapper,
     F, Func, IntegerField, Max, NullBooleanField, OuterRef, Q, Subquery, Sum,
@@ -623,7 +621,6 @@ class NonAggregateAnnotationTestCase(TestCase):
         ).values('name')
         self.assertCountEqual(publisher_books_qs, [{'name': 'Sams'}, {'name': 'Morgan Kaufmann'}])
 
-    @skipIf(connection.vendor == 'oracle', 'See https://code.djangoproject.com/ticket/31584')
     def test_annotation_exists_aggregate_values_chaining(self):
         qs = Book.objects.values('publisher').annotate(
             has_authors=Exists(Book.authors.through.objects.filter(book=OuterRef('pk'))),

django/db/backends/base/features.py
@@ -297,7 +297,8 @@ class BaseDatabaseFeatures:
     # field(s)?
     allows_multiple_constraints_on_same_fields = True
 
-    # Does the backend support boolean expressions in the SELECT clause?
+    # Does the backend support boolean expressions in SELECT and GROUP BY
+    # clauses?
     supports_boolean_expr_in_select_clause = True
 
     # Does the backend support JSONField?

django/db/models/sql/compiler.py
@@ -139,6 +139,7 @@ class SQLCompiler:
 
         for expr in expressions:
             sql, params = self.compile(expr)
+            sql, params = expr.select_format(self, sql, params)
             params_hash = make_hashable(params)
             if (sql, params_hash) not in seen:
                 result.append((sql, params))

commit a9df7b1b43b0d0b0c05388c37709ae7acdc50242

Fixed #31660 -- Fixed queryset crash when grouping by m2o relation.
Regression in 9b390144a9c34f574135a939c42634a5289c5272.

Thanks Tomasz Szymański for the report.

django/db/models/expressions.py
@@ -383,7 +383,9 @@ class BaseExpression:
         Custom format for select clauses. For example, EXISTS expressions need
         to be wrapped in CASE WHEN on Oracle.
         """
-        return self.output_field.select_format(compiler, sql, params)
+        if hasattr(self.output_field, 'select_format'):
+            return self.output_field.select_format(compiler, sql, params)
+        return sql, params
 
     @cached_property
     def identity(self):

tests/annotations/tests.py
@@ -1,11 +1,13 @@
 import datetime
 from decimal import Decimal
+from unittest import skipIf
 
 from django.core.exceptions import FieldDoesNotExist, FieldError
+from django.db import connection
 from django.db.models import (
-    BooleanField, CharField, Count, DateTimeField, Exists, ExpressionWrapper,
-    F, Func, IntegerField, Max, NullBooleanField, OuterRef, Q, Subquery, Sum,
-    Value,
+    BooleanField, Case, CharField, Count, DateTimeField, Exists,
+    ExpressionWrapper, F, Func, IntegerField, Max, NullBooleanField, OuterRef,
+    Q, Subquery, Sum, Value, When,
 )
 from django.db.models.expressions import RawSQL
 from django.db.models.functions import Length, Lower
@@ -631,4 +633,23 @@ class NonAggregateAnnotationTestCase(TestCase):
             datetime.date(2008, 3, 3),
             datetime.date(2008, 6, 23),
             datetime.date(2008, 11, 3),
+        ])
+
+    @skipIf(
+        connection.vendor == 'mysql' and 'ONLY_FULL_GROUP_BY' in connection.sql_mode,
+        'GROUP BY optimization does not work properly when ONLY_FULL_GROUP_BY '
+        'mode is enabled on MySQL, see #31331.',
+    )
+    def test_annotation_aggregate_with_m2o(self):
+        qs = Author.objects.filter(age__lt=30).annotate(
+            max_pages=Case(
+                When(book_contact_set__isnull=True, then=Value(0)),
+                default=Max(F('book__pages')),
+                output_field=IntegerField(),
+            ),
+        ).values('name', 'max_pages')
+        self.assertCountEqual(qs, [
+            {'name': 'James Bennett', 'max_pages': 300},
+            {'name': 'Paul Bissex', 'max_pages': 0},
+            {'name': 'Wesley J. Chun', 'max_pages': 0},
         ])