varshithkonakanchi/django

commit 7c3ce29082417e30b67eff7001a1002dddc289bb

Fixed #31651 -- Made ExpressionWrapper use grouping columns from wrapped expression.

django/db/models/expressions.py
@@ -863,6 +863,9 @@ class ExpressionWrapper(Expression):
     def get_source_expressions(self):
         return [self.expression]
 
+    def get_group_by_cols(self, alias=None):
+        return self.expression.get_group_by_cols(alias=alias)
+
     def as_sql(self, compiler, connection):
         return self.expression.as_sql(compiler, connection)
 

tests/expressions/tests.py
@@ -1828,3 +1828,13 @@ class CombinableTests(SimpleTestCase):
     def test_reversed_or(self):
         with self.assertRaisesMessage(NotImplementedError, self.bitwise_msg):
             object() | Combinable()
+
+
+class ExpressionWrapperTests(SimpleTestCase):
+    def test_empty_group_by(self):
+        expr = ExpressionWrapper(Value(3), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [])
+
+    def test_non_empty_group_by(self):
+        expr = ExpressionWrapper(Lower(Value('f')), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [expr.expression])

commit fd3b9608bb27094a7c419f9b6c452873c210a3b8

Fixed #31659 -- Made ExpressionWrapper preserve output_field for combined expressions.
Regression in 7c3ce29082417e30b67eff7001a1002dddc289bb.

Thanks Simon Charette for the review.

django/db/models/expressions.py
@@ -857,6 +857,9 @@ class ExpressionWrapper(Expression):
 
     def __init__(self, expression, output_field):
         super().__init__(output_field=output_field)
+        if getattr(expression, '_output_field_or_none', True) is None:
+            expression = expression.copy()
+            expression.output_field = output_field
         self.expression = expression
 
     def set_source_expressions(self, exprs):

tests/annotations/tests.py
@@ -170,6 +170,14 @@ class NonAggregateAnnotationTestCase(TestCase):
             self.assertEqual(book.is_book, 1)
             self.assertEqual(book.rating_count, 1)
 
+    def test_combined_expression_annotation_with_aggregation(self):
+        book = Book.objects.annotate(
+            combined=ExpressionWrapper(Value(3) * Value(4), output_field=IntegerField()),
+            rating_count=Count('rating'),
+        ).first()
+        self.assertEqual(book.combined, 12)
+        self.assertEqual(book.rating_count, 1)
+
     def test_aggregate_over_annotation(self):
         agg = Author.objects.annotate(other_age=F('age')).aggregate(otherage_sum=Sum('other_age'))
         other_agg = Author.objects.aggregate(age_sum=Sum('age'))

tests/expressions/tests.py
@@ -1837,4 +1837,6 @@ class ExpressionWrapperTests(SimpleTestCase):
 
     def test_non_empty_group_by(self):
         expr = ExpressionWrapper(Lower(Value('f')), output_field=IntegerField())
-        self.assertEqual(expr.get_group_by_cols(alias=None), [expr.expression])
+        group_by_cols = expr.get_group_by_cols(alias=None)
+        self.assertEqual(group_by_cols, [expr.expression])
+        self.assertEqual(group_by_cols[0].output_field, expr.output_field)