varshithkonakanchi/django

commit bd676c5c4dadf0ca9c6cef666b05e7f89ffc9f73

Fixed #31773 -- Fixed preserving output_field in ExpressionWrapper for combined expressions.
Thanks Thodoris Sotiropoulos for the report and Simon Charette for the
implementation idea.

Regression in 7c3ce29082417e30b67eff7001a1002dddc289bb.

tests/annotations/tests.py
@@ -6,8 +6,8 @@ from django.core.exceptions import FieldDoesNotExist, FieldError
 from django.db import connection
 from django.db.models import (
     BooleanField, Case, CharField, Count, DateTimeField, Exists,
-    ExpressionWrapper, F, Func, IntegerField, Max, NullBooleanField, OuterRef,
-    Q, Subquery, Sum, Value, When,
+    ExpressionWrapper, F, FloatField, Func, IntegerField, Max,
+    NullBooleanField, OuterRef, Q, Subquery, Sum, Value, When,
 )
 from django.db.models.expressions import RawSQL
 from django.db.models.functions import Length, Lower
@@ -176,6 +176,14 @@ class NonAggregateAnnotationTestCase(TestCase):
             rating_count=Count('rating'),
         ).first()
         self.assertEqual(book.combined, 12)
+        self.assertEqual(book.rating_count, 1)
+
+    def test_combined_f_expression_annotation_with_aggregation(self):
+        book = Book.objects.filter(isbn='159059725').annotate(
+            combined=ExpressionWrapper(F('price') * F('pages'), output_field=FloatField()),
+            rating_count=Count('rating'),
+        ).first()
+        self.assertEqual(book.combined, 13410.0)
         self.assertEqual(book.rating_count, 1)
 
     def test_aggregate_over_annotation(self):

django/db/models/expressions.py
@@ -858,9 +858,6 @@ class ExpressionWrapper(Expression):
 
     def __init__(self, expression, output_field):
         super().__init__(output_field=output_field)
-        if getattr(expression, '_output_field_or_none', True) is None:
-            expression = expression.copy()
-            expression.output_field = output_field
         self.expression = expression
 
     def set_source_expressions(self, exprs):
@@ -870,7 +867,9 @@ class ExpressionWrapper(Expression):
         return [self.expression]
 
     def get_group_by_cols(self, alias=None):
-        return self.expression.get_group_by_cols(alias=alias)
+        expression = self.expression.copy()
+        expression.output_field = self.output_field
+        return expression.get_group_by_cols(alias=alias)
 
     def as_sql(self, compiler, connection):
         return self.expression.as_sql(compiler, connection)

commit e2b1eebcadcdda6eb7dbfc1f01651e418b3487ec

Fixed #32007 -- Fixed queryset crash with Q() annotation and aggregation.
Thanks Gordon Wrigley for the report.

Regression in bd676c5c4dadf0ca9c6cef666b05e7f89ffc9f73.

django/db/models/sql/where.py
@@ -159,6 +159,9 @@ class WhereNode(tree.Node):
         clone.relabel_aliases(change_map)
         return clone
 
+    def copy(self):
+        return self.clone()
+
     @classmethod
     def _contains_aggregate(cls, obj):
         if isinstance(obj, tree.Node):

tests/annotations/tests.py
@@ -183,6 +183,17 @@ class NonAggregateAnnotationTestCase(TestCase):
         self.assertEqual(book.combined, 13410.0)
         self.assertEqual(book.rating_count, 1)
 
+    def test_q_expression_annotation_with_aggregation(self):
+        book = Book.objects.filter(isbn='159059725').annotate(
+            isnull_pubdate=ExpressionWrapper(
+                Q(pubdate__isnull=True),
+                output_field=BooleanField(),
+            ),
+            rating_count=Count('rating'),
+        ).first()
+        self.assertEqual(book.isnull_pubdate, False)
+        self.assertEqual(book.rating_count, 1)
+
     def test_aggregate_over_annotation(self):
         agg = Author.objects.annotate(other_age=F('age')).aggregate(otherage_sum=Sum('other_age'))
         other_agg = Author.objects.aggregate(age_sum=Sum('age'))