varshithkonakanchi/django

commit a59b4ecc9b2c4e4ec7b59ea122781500f4aed16d

Fixed #32292 -- Added support for connection by service name to PostgreSQL.

django/db/backends/postgresql/client.py
@@ -16,6 +16,7 @@ class DatabaseClient(BaseDatabaseClient):
         dbname = settings_dict.get('NAME') or 'postgres'
         user = settings_dict.get('USER')
         passwd = settings_dict.get('PASSWORD')
+        service = options.get('service')
         sslmode = options.get('sslmode')
         sslrootcert = options.get('sslrootcert')
         sslcert = options.get('sslcert')
@@ -33,6 +34,8 @@ class DatabaseClient(BaseDatabaseClient):
         env = {}
         if passwd:
             env['PGPASSWORD'] = str(passwd)
+        if service:
+            env['PGSERVICE'] = str(service)
         if sslmode:
             env['PGSSLMODE'] = str(sslmode)
         if sslrootcert:

tests/dbshell/test_postgresql.py
@@ -67,6 +67,12 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
             )
         )
 
+    def test_service(self):
+        self.assertEqual(
+            self.settings_to_cmd_args_env({'OPTIONS': {'service': 'django_test'}}),
+            (['psql', 'postgres'], {'PGSERVICE': 'django_test'}),
+        )
+
     def test_column(self):
         self.assertEqual(
             self.settings_to_cmd_args_env({

django/db/backends/postgresql/base.py
@@ -152,10 +152,14 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     def get_connection_params(self):
         settings_dict = self.settings_dict
         # None may be used to connect to the default 'postgres' db
-        if settings_dict['NAME'] == '':
+        if (
+            settings_dict['NAME'] == '' and
+            not settings_dict.get('OPTIONS', {}).get('service')
+        ):
             raise ImproperlyConfigured(
                 "settings.DATABASES is improperly configured. "
-                "Please supply the NAME value.")
+                "Please supply the NAME or OPTIONS['service'] value."
+            )
         if len(settings_dict['NAME'] or '') > self.ops.max_name_length():
             raise ImproperlyConfigured(
                 "The database name '%s' (%d characters) is longer than "
@@ -166,10 +170,19 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                     self.ops.max_name_length(),
                 )
             )
-        conn_params = {
-            'database': settings_dict['NAME'] or 'postgres',
-            **settings_dict['OPTIONS'],
-        }
+        conn_params = {}
+        if settings_dict['NAME']:
+            conn_params = {
+                'database': settings_dict['NAME'],
+                **settings_dict['OPTIONS'],
+            }
+        elif settings_dict['NAME'] is None:
+            # Connect to the default 'postgres' db.
+            settings_dict.get('OPTIONS', {}).pop('service', None)
+            conn_params = {'database': 'postgres', **settings_dict['OPTIONS']}
+        else:
+            conn_params = {**settings_dict['OPTIONS']}
+
         conn_params.pop('isolation_level', None)
         if settings_dict['USER']:
             conn_params['user'] = settings_dict['USER']

tests/backends/postgresql/tests.py
@@ -68,6 +68,36 @@ class Tests(TestCase):
         with self.assertRaisesMessage(ImproperlyConfigured, msg):
             DatabaseWrapper(settings).get_connection_params()
 
+    def test_database_name_empty(self):
+        from django.db.backends.postgresql.base import DatabaseWrapper
+        settings = connection.settings_dict.copy()
+        settings['NAME'] = ''
+        msg = (
+            "settings.DATABASES is improperly configured. Please supply the "
+            "NAME or OPTIONS['service'] value."
+        )
+        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+            DatabaseWrapper(settings).get_connection_params()
+
+    def test_service_name(self):
+        from django.db.backends.postgresql.base import DatabaseWrapper
+        settings = connection.settings_dict.copy()
+        settings['OPTIONS'] = {'service': 'my_service'}
+        settings['NAME'] = ''
+        params = DatabaseWrapper(settings).get_connection_params()
+        self.assertEqual(params['service'], 'my_service')
+        self.assertNotIn('database', params)
+
+    def test_service_name_default_db(self):
+        # None is used to connect to the default 'postgres' db.
+        from django.db.backends.postgresql.base import DatabaseWrapper
+        settings = connection.settings_dict.copy()
+        settings['NAME'] = None
+        settings['OPTIONS'] = {'service': 'django_test'}
+        params = DatabaseWrapper(settings).get_connection_params()
+        self.assertEqual(params['database'], 'postgres')
+        self.assertNotIn('service', params)
+
     def test_connect_and_rollback(self):
         """
         PostgreSQL shouldn't roll back SET TIME ZONE, even if the first

commit c02dddd88566f62eb0e3d609c2b66149035065ed

Refs #32292 -- Made dbshell do not use 'postgres' database when service name is set.
Regression in a59b4ecc9b2c4e4ec7b59ea122781500f4aed16d.

django/db/backends/postgresql/client.py
@@ -13,7 +13,7 @@ class DatabaseClient(BaseDatabaseClient):
 
         host = settings_dict.get('HOST')
         port = settings_dict.get('PORT')
-        dbname = settings_dict.get('NAME') or 'postgres'
+        dbname = settings_dict.get('NAME')
         user = settings_dict.get('USER')
         passwd = settings_dict.get('PASSWORD')
         service = options.get('service')
@@ -22,13 +22,17 @@ class DatabaseClient(BaseDatabaseClient):
         sslcert = options.get('sslcert')
         sslkey = options.get('sslkey')
 
+        if not dbname and not service:
+            # Connect to the default 'postgres' db.
+            dbname = 'postgres'
         if user:
             args += ['-U', user]
         if host:
             args += ['-h', host]
         if port:
             args += ['-p', str(port)]
-        args += [dbname]
+        if dbname:
+            args += [dbname]
         args.extend(parameters)
 
         env = {}

tests/dbshell/test_postgresql.py
@@ -70,7 +70,7 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
     def test_service(self):
         self.assertEqual(
             self.settings_to_cmd_args_env({'OPTIONS': {'service': 'django_test'}}),
-            (['psql', 'postgres'], {'PGSERVICE': 'django_test'}),
+            (['psql'], {'PGSERVICE': 'django_test'}),
         )
 
     def test_column(self):