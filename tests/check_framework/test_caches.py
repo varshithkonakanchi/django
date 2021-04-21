varshithkonakanchi/django

commit abce73014a6802ad72a2c1749d5ec04780bbafd5

Fixed #31983 -- Added system check for file system caches location.
Thanks Johannes Maron and Nick Pope for reviews.

django/core/checks/caches.py
@@ -1,7 +1,10 @@
-from django.conf import settings
-from django.core.cache import DEFAULT_CACHE_ALIAS
+import pathlib
 
-from . import Error, Tags, register
+from django.conf import settings
+from django.core.cache import DEFAULT_CACHE_ALIAS, caches
+from django.core.cache.backends.filebased import FileBasedCache
+
+from . import Error, Tags, Warning, register
 
 E001 = Error(
     "You must define a '%s' cache in your CACHES setting." % DEFAULT_CACHE_ALIAS,
@@ -14,3 +17,39 @@ def check_default_cache_is_configured(app_configs, **kwargs):
     if DEFAULT_CACHE_ALIAS not in settings.CACHES:
         return [E001]
     return []
+
+
+@register(Tags.caches, deploy=True)
+def check_cache_location_not_exposed(app_configs, **kwargs):
+    errors = []
+    for name in ('MEDIA_ROOT', 'STATIC_ROOT', 'STATICFILES_DIRS'):
+        setting = getattr(settings, name, None)
+        if not setting:
+            continue
+        if name == 'STATICFILES_DIRS':
+            paths = {
+                pathlib.Path(staticfiles_dir).resolve()
+                for staticfiles_dir in setting
+            }
+        else:
+            paths = {pathlib.Path(setting).resolve()}
+        for alias in settings.CACHES:
+            cache = caches[alias]
+            if not isinstance(cache, FileBasedCache):
+                continue
+            cache_path = pathlib.Path(cache._dir).resolve()
+            if any(path == cache_path for path in paths):
+                relation = 'matches'
+            elif any(path in cache_path.parents for path in paths):
+                relation = 'is inside'
+            elif any(cache_path in path.parents for path in paths):
+                relation = 'contains'
+            else:
+                continue
+            errors.append(Warning(
+                f"Your '{alias}' cache configuration might expose your cache "
+                f"or lead to corruption of your data because its LOCATION "
+                f"{relation} {name}.",
+                id='caches.W002',
+            ))
+    return errors

tests/check_framework/test_caches.py
@@ -1,4 +1,9 @@
-from django.core.checks.caches import E001, check_default_cache_is_configured
+import pathlib
+
+from django.core.checks import Warning
+from django.core.checks.caches import (
+    E001, check_cache_location_not_exposed, check_default_cache_is_configured,
+)
 from django.test import SimpleTestCase
 from django.test.utils import override_settings
 
@@ -28,3 +33,59 @@ class CheckCacheSettingsAppDirsTest(SimpleTestCase):
         Error if 'default' not present in CACHES setting.
         """
         self.assertEqual(check_default_cache_is_configured(None), [E001])
+
+
+class CheckCacheLocationTest(SimpleTestCase):
+    warning_message = (
+        "Your 'default' cache configuration might expose your cache or lead "
+        "to corruption of your data because its LOCATION %s %s."
+    )
+
+    @staticmethod
+    def get_settings(setting, cache_path, setting_path):
+        return {
+            'CACHES': {
+                'default': {
+                    'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+                    'LOCATION': cache_path,
+                },
+            },
+            setting: [setting_path] if setting == 'STATICFILES_DIRS' else setting_path,
+        }
+
+    def test_cache_path_matches_media_static_setting(self):
+        root = pathlib.Path.cwd()
+        for setting in ('MEDIA_ROOT', 'STATIC_ROOT', 'STATICFILES_DIRS'):
+            settings = self.get_settings(setting, root, root)
+            with self.subTest(setting=setting), self.settings(**settings):
+                msg = self.warning_message % ('matches', setting)
+                self.assertEqual(check_cache_location_not_exposed(None), [
+                    Warning(msg, id='caches.W002'),
+                ])
+
+    def test_cache_path_inside_media_static_setting(self):
+        root = pathlib.Path.cwd()
+        for setting in ('MEDIA_ROOT', 'STATIC_ROOT', 'STATICFILES_DIRS'):
+            settings = self.get_settings(setting, root / 'cache', root)
+            with self.subTest(setting=setting), self.settings(**settings):
+                msg = self.warning_message % ('is inside', setting)
+                self.assertEqual(check_cache_location_not_exposed(None), [
+                    Warning(msg, id='caches.W002'),
+                ])
+
+    def test_cache_path_contains_media_static_setting(self):
+        root = pathlib.Path.cwd()
+        for setting in ('MEDIA_ROOT', 'STATIC_ROOT', 'STATICFILES_DIRS'):
+            settings = self.get_settings(setting, root, root / 'other')
+            with self.subTest(setting=setting), self.settings(**settings):
+                msg = self.warning_message % ('contains', setting)
+                self.assertEqual(check_cache_location_not_exposed(None), [
+                    Warning(msg, id='caches.W002'),
+                ])
+
+    def test_cache_path_not_conflict(self):
+        root = pathlib.Path.cwd()
+        for setting in ('MEDIA_ROOT', 'STATIC_ROOT', 'STATICFILES_DIRS'):
+            settings = self.get_settings(setting, root / 'cache', root / 'other')
+            with self.subTest(setting=setting), self.settings(**settings):
+                self.assertEqual(check_cache_location_not_exposed(None), [])

commit e33630ac7ee7bc9adeba2c5843bc0019b004a787

Fixed #32665 -- Fixed caches system check crash when STATICFILES_DIRS is a list of 2-tuples.
Thanks Jared Lockhart for the report.

Regression in abce73014a6802ad72a2c1749d5ec04780bbafd5.

django/core/checks/caches.py
@@ -27,10 +27,11 @@ def check_cache_location_not_exposed(app_configs, **kwargs):
         if not setting:
             continue
         if name == 'STATICFILES_DIRS':
-            paths = {
-                pathlib.Path(staticfiles_dir).resolve()
-                for staticfiles_dir in setting
-            }
+            paths = set()
+            for staticfiles_dir in setting:
+                if isinstance(staticfiles_dir, (list, tuple)):
+                    _, staticfiles_dir = staticfiles_dir
+                paths.add(pathlib.Path(staticfiles_dir).resolve())
         else:
             paths = {pathlib.Path(setting).resolve()}
         for alias in settings.CACHES:

tests/check_framework/test_caches.py
@@ -91,6 +91,35 @@ class CheckCacheLocationTest(SimpleTestCase):
             with self.subTest(setting=setting), self.settings(**settings):
                 self.assertEqual(check_cache_location_not_exposed(None), [])
 
+    def test_staticfiles_dirs_prefix(self):
+        root = pathlib.Path.cwd()
+        tests = [
+            (root, root, 'matches'),
+            (root / 'cache', root, 'is inside'),
+            (root, root / 'other', 'contains'),
+        ]
+        for cache_path, setting_path, msg in tests:
+            settings = self.get_settings(
+                'STATICFILES_DIRS',
+                cache_path,
+                ('prefix', setting_path),
+            )
+            with self.subTest(path=setting_path), self.settings(**settings):
+                msg = self.warning_message % (msg, 'STATICFILES_DIRS')
+                self.assertEqual(check_cache_location_not_exposed(None), [
+                    Warning(msg, id='caches.W002'),
+                ])
+
+    def test_staticfiles_dirs_prefix_not_conflict(self):
+        root = pathlib.Path.cwd()
+        settings = self.get_settings(
+            'STATICFILES_DIRS',
+            root / 'cache',
+            ('prefix', root / 'other'),
+        )
+        with self.settings(**settings):
+            self.assertEqual(check_cache_location_not_exposed(None), [])
+
 
 class CheckCacheAbsolutePath(SimpleTestCase):
     def test_absolute_path(self):