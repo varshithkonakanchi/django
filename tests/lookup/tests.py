varshithkonakanchi/django

commit eec0e247c219fd168d329797ed6b46077314ac66

Refs #27149, #29542 -- Simplified subquery parentheses wrapping logic.

django/db/models/lookups.py
@@ -89,8 +89,7 @@ class Lookup:
             value = self.apply_bilateral_transforms(value)
             value = value.resolve_expression(compiler.query)
         if hasattr(value, 'as_sql'):
-            sql, params = compiler.compile(value)
-            return '(' + sql + ')', params
+            return compiler.compile(value)
         else:
             return self.get_db_prep_lookup(value, connection)
 

django/db/models/expressions.py
@@ -1023,22 +1023,12 @@ class Subquery(Expression):
     def as_sql(self, compiler, connection, template=None, **extra_context):
         connection.ops.check_expression_support(self)
         template_params = {**self.extra, **extra_context}
-        template_params['subquery'], sql_params = self.query.as_sql(compiler, connection)
+        subquery_sql, sql_params = self.query.as_sql(compiler, connection)
+        template_params['subquery'] = subquery_sql[1:-1]
 
         template = template or template_params.get('template', self.template)
         sql = template % template_params
         return sql, sql_params
-
-    def _prepare(self, output_field):
-        # This method will only be called if this instance is the "rhs" in an
-        # expression: the wrapping () must be removed (as the expression that
-        # contains this will provide them). SQLite evaluates ((subquery))
-        # differently than the other databases.
-        if self.template == '(%(subquery)s)':
-            clone = self.copy()
-            clone.template = '%(subquery)s'
-            return clone
-        return self
 
     def get_group_by_cols(self, alias=None):
         if alias:

django/db/models/sql/compiler.py
@@ -5,7 +5,7 @@ from itertools import chain
 
 from django.core.exceptions import EmptyResultSet, FieldError
 from django.db.models.constants import LOOKUP_SEP
-from django.db.models.expressions import OrderBy, Random, RawSQL, Ref, Subquery
+from django.db.models.expressions import OrderBy, Random, RawSQL, Ref
 from django.db.models.query_utils import QueryWrapper, select_related_descend
 from django.db.models.sql.constants import (
     CURSOR, GET_ITERATOR_CHUNK_SIZE, MULTI, NO_RESULTS, ORDER_DIR, SINGLE,
@@ -126,11 +126,6 @@ class SQLCompiler:
 
         for expr in expressions:
             sql, params = self.compile(expr)
-            if isinstance(expr, Subquery) and not sql.startswith('('):
-                # Subquery expression from HAVING clause may not contain
-                # wrapping () because they could be removed when a subquery is
-                # the "rhs" in an expression (see Subquery._prepare()).
-                sql = '(%s)' % sql
             if (sql, tuple(params)) not in seen:
                 result.append((sql, params))
                 seen.add((sql, tuple(params)))

django/db/models/sql/query.py
@@ -1022,7 +1022,10 @@ class Query(BaseExpression):
         return clone
 
     def as_sql(self, compiler, connection):
-        return self.get_compiler(connection=connection).as_sql()
+        sql, params = self.get_compiler(connection=connection).as_sql()
+        if self.subquery:
+            sql = '(%s)' % sql
+        return sql, params
 
     def resolve_lookup_value(self, value, can_reuse, allow_joins, simple_col):
         if hasattr(value, 'resolve_expression'):

commit 265ac6824f0661f935015b3ff3b556b780e7a087

Fixed #32673 -- Fixed lookups crash when comparing against lookups on PostgreSQL.
Regression in eec0e247c219fd168d329797ed6b46077314ac66.

Nonlitteral right-hand-sides of lookups need to be wrapped in
parentheses to avoid operator precedence ambiguities.

Thanks Charles Lirsac for the detailed report.

django/db/backends/oracle/features.py
@@ -87,6 +87,10 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         'Raises ORA-00600: internal error code.': {
             'model_fields.test_jsonfield.TestQuerying.test_usage_in_subquery',
         },
+        "Oracle doesn't allow filters to be compared to another expression in "
+        "the WHERE clause.": {
+            'lookup.tests.LookupTests.test_lookup_rhs',
+        },
     }
     django_test_expected_failures = {
         # A bug in Django/cx_Oracle with respect to string handling (#23843).

django/db/models/lookups.py
@@ -94,7 +94,13 @@ class Lookup:
             value = self.apply_bilateral_transforms(value)
             value = value.resolve_expression(compiler.query)
         if hasattr(value, 'as_sql'):
-            return compiler.compile(value)
+            sql, params = compiler.compile(value)
+            # Ensure expression is wrapped in parentheses to respect operator
+            # precedence but avoid double wrapping as it can be misinterpreted
+            # on some backends (e.g. subqueries on SQLite).
+            if sql and sql[0] != '(':
+                sql = '(%s)' % sql
+            return sql, params
         else:
             return self.get_db_prep_lookup(value, connection)
 

tests/lookup/models.py
@@ -94,6 +94,7 @@ class Product(models.Model):
 
 class Stock(models.Model):
     product = models.ForeignKey(Product, models.CASCADE)
+    short = models.BooleanField(default=False)
     qty_available = models.DecimalField(max_digits=6, decimal_places=2)
 
 

tests/lookup/tests.py
@@ -5,13 +5,16 @@ from operator import attrgetter
 
 from django.core.exceptions import FieldError
 from django.db import connection, models
-from django.db.models import Exists, Max, OuterRef
+from django.db.models import (
+    BooleanField, Exists, ExpressionWrapper, F, Max, OuterRef, Q,
+)
 from django.db.models.functions import Substr
 from django.test import TestCase, skipUnlessDBFeature
 from django.test.utils import isolate_apps
 
 from .models import (
-    Article, Author, Freebie, Game, IsNullWithNoneAsRHS, Player, Season, Tag,
+    Article, Author, Freebie, Game, IsNullWithNoneAsRHS, Player, Product,
+    Season, Stock, Tag,
 )
 
 
@@ -1000,3 +1003,20 @@ class LookupTests(TestCase):
             with self.subTest(qs=qs):
                 with self.assertRaisesMessage(ValueError, msg):
                     qs.exists()
+
+    def test_lookup_rhs(self):
+        product = Product.objects.create(name='GME', qty_target=5000)
+        stock_1 = Stock.objects.create(product=product, short=True, qty_available=180)
+        stock_2 = Stock.objects.create(product=product, short=False, qty_available=5100)
+        Stock.objects.create(product=product, short=False, qty_available=4000)
+        self.assertCountEqual(
+            Stock.objects.filter(short=Q(qty_available__lt=F('product__qty_target'))),
+            [stock_1, stock_2],
+        )
+        self.assertCountEqual(
+            Stock.objects.filter(short=ExpressionWrapper(
+                Q(qty_available__lt=F('product__qty_target')),
+                output_field=BooleanField(),
+            )),
+            [stock_1, stock_2],
+        )