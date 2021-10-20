varshithkonakanchi/django

commit 2617ef01ccf3a330058d8789178d2731f1ceadd4

Refs #29253 -- Fixed method_decorator() crash if decorator sets a new attribute.
Regression in 802658ea6a9c0cd6a6b5d0f3f3aef8e5d819e841.

django/utils/decorators.py
@@ -2,7 +2,7 @@
 
 # For backwards compatibility in Django 2.0.
 from contextlib import ContextDecorator  # noqa
-from functools import WRAPPER_ASSIGNMENTS, update_wrapper, wraps
+from functools import WRAPPER_ASSIGNMENTS, partial, update_wrapper, wraps
 
 
 class classonlymethod(classmethod):
@@ -36,8 +36,10 @@ def _multi_decorate(decorators, method):
 
     def _wrapper(self, *args, **kwargs):
         # bound_method has the signature that 'decorator' expects i.e. no
-        # 'self' argument.
-        bound_method = method.__get__(self, type(self))
+        # 'self' argument, but it's a closure over self so it can call
+        # 'func'. Also, wrap method.__get__() in a function because new
+        # attributes can't be set on bound method objects, only on functions.
+        bound_method = partial(method.__get__(self, type(self)))
         for dec in decorators:
             bound_method = dec(bound_method)
         return bound_method(*args, **kwargs)

tests/decorators/tests.py
@@ -271,6 +271,21 @@ class MethodDecoratorTests(SimpleTestCase):
                 self.assertEqual(Test.method.__doc__, 'A method')
                 self.assertEqual(Test.method.__name__, 'method')
 
+    def test_new_attribute(self):
+        """A decorator that sets a new attribute on the method."""
+        def decorate(func):
+            func.x = 1
+            return func
+
+        class MyClass:
+            @method_decorator(decorate)
+            def method(self):
+                return True
+
+        obj = MyClass()
+        self.assertEqual(obj.method.x, 1)
+        self.assertIs(obj.method(), True)
+
     def test_bad_iterable(self):
         decorators = {myattr_dec_m, myattr2_dec_m}
         msg = "'set' object is not subscriptable"

commit a74d82b14aa11f964afbeffb908bd770ea61ef0b

Fixed #33043 -- Made method_decorator() preserve wrapper assignments.
Regression in 2617ef01ccf3a330058d8789178d2731f1ceadd4.

django/utils/decorators.py
@@ -37,7 +37,7 @@ def _multi_decorate(decorators, method):
         # 'self' argument, but it's a closure over self so it can call
         # 'func'. Also, wrap method.__get__() in a function because new
         # attributes can't be set on bound method objects, only on functions.
-        bound_method = partial(method.__get__(self, type(self)))
+        bound_method = wraps(method)(partial(method.__get__(self, type(self))))
         for dec in decorators:
             bound_method = dec(bound_method)
         return bound_method(*args, **kwargs)

tests/decorators/tests.py
@@ -425,6 +425,29 @@ class MethodDecoratorTests(SimpleTestCase):
                 def __module__(cls):
                     return "tests"
 
+    def test_wrapper_assignments(self):
+        """@method_decorator preserves wrapper assignments."""
+        func_name = None
+        func_module = None
+
+        def decorator(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                nonlocal func_name, func_module
+                func_name = getattr(func, '__name__', None)
+                func_module = getattr(func, '__module__', None)
+                return func(*args, **kwargs)
+            return inner
+
+        class Test:
+            @method_decorator(decorator)
+            def method(self):
+                return 'tests'
+
+        Test().method()
+        self.assertEqual(func_name, 'method')
+        self.assertIsNotNone(func_module)
+
 
 class XFrameOptionsDecoratorsTests(TestCase):
     """