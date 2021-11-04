varshithkonakanchi/django

commit 574093bd21d2eac53a5fbfdd5b08ece01c833a68

Refs #32319 -- Changed HashedFilesMixin to use named groups in patterns.

django/contrib/staticfiles/storage.py
@@ -42,15 +42,21 @@ class StaticFilesStorage(FileSystemStorage):
 
 
 class HashedFilesMixin:
-    default_template = """url("%s")"""
+    default_template = """url("%(url)s")"""
     max_post_process_passes = 5
     patterns = (
         ("*.css", (
-            r"""(url\(['"]{0,1}\s*(.*?)["']{0,1}\))""",
-            (r"""(@import\s*["']\s*(.*?)["'])""", """@import url("%s")"""),
+            r"""(?P<matched>url\(['"]{0,1}\s*(?P<url>.*?)["']{0,1}\))""",
+            (
+                r"""(?P<matched>@import\s*["']\s*(?P<url>.*?)["'])""",
+                """@import url("%(url)s")""",
+            ),
         )),
         ('*.js', (
-            (r'(?m)^(//# (?-i:sourceMappingURL)=(.*))$', '//# sourceMappingURL=%s'),
+            (
+                r'(?P<matched>)^(//# (?-i:sourceMappingURL)=(?P<url>.*))$',
+                '//# sourceMappingURL=%(url)s',
+            ),
         )),
     )
     keep_intermediate_files = True
@@ -163,7 +169,9 @@ class HashedFilesMixin:
             This requires figuring out which files the matched URL resolves
             to and calling the url() method of the storage.
             """
-            matched, url = matchobj.groups()
+            matches = matchobj.groupdict()
+            matched = matches['matched']
+            url = matches['url']
 
             # Ignore absolute/protocol-relative and data-uri URLs.
             if re.match(r'^[a-z]+:', url):
@@ -199,7 +207,8 @@ class HashedFilesMixin:
                 transformed_url += ('?#' if '?#' in url else '#') + fragment
 
             # Return the hashed version to the file
-            return template % unquote(transformed_url)
+            matches['url'] = unquote(transformed_url)
+            return template % matches
 
         return converter
 

tests/staticfiles_tests/storage.py
@@ -84,7 +84,10 @@ class ExtraPatternsStorage(ManifestStaticFilesStorage):
     patterns = tuple(ManifestStaticFilesStorage.patterns) + (
         (
             "*.js", (
-                (r"""(url\(['"]{0,1}\s*(.*?)["']{0,1}\))""", 'JS_URL("%s")'),
+                (
+                    r"""(?P<matched>url\(['"]{0,1}\s*(?P<url>.*?)["']{0,1}\))""",
+                    'JS_URL("%(url)s")',
+                ),
             ),
         ),
     )

commit 9a6bb65126c2bbaa50ff4286fd0b7bfe25fad29c

Fixed #33237 -- Fixed detecting source maps in ManifestStaticFilesStorage for multiline files.
Switched regex to multiline mode in order to match per-line, rather
than against the whole file.

Thanks to Joseph Abrahams for the report.

Regression in 574093bd21d2eac53a5fbfdd5b08ece01c833a68.

django/contrib/staticfiles/storage.py
@@ -54,7 +54,7 @@ class HashedFilesMixin:
         )),
         ('*.js', (
             (
-                r'(?P<matched>)^(//# (?-i:sourceMappingURL)=(?P<url>.*))$',
+                r'(?m)(?P<matched>)^(//# (?-i:sourceMappingURL)=(?P<url>.*))$',
                 '//# sourceMappingURL=%(url)s',
             ),
             (

tests/staticfiles_tests/project/documents/cached/source_map.js
@@ -1,1 +1,2 @@
 //# sourceMappingURL=source_map.js.map
+let a_variable = 1;

tests/staticfiles_tests/test_storage.py
@@ -260,7 +260,7 @@ class TestHashedFiles:
 
     def test_js_source_map(self):
         relpath = self.hashed_file_path('cached/source_map.js')
-        self.assertEqual(relpath, 'cached/source_map.9371cbb02a26.js')
+        self.assertEqual(relpath, 'cached/source_map.cd45b8534a87.js')
         with storage.staticfiles_storage.open(relpath) as relfile:
             content = relfile.read()
             self.assertNotIn(b'//# sourceMappingURL=source_map.js.map', content)