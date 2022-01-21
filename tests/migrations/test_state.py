varshithkonakanchi/django

commit 398251f7484df7cfa45eaf1bc768cb37b2bd1f2f

Fixed #29899 -- Made autodetector use model states instead of model classes.
Thanks Simon Charette and Markus Holtermann for reviews.

django/db/migrations/state.py
@@ -1,5 +1,7 @@
 import copy
+from collections import defaultdict
 from contextlib import contextmanager
+from functools import partial
 
 from django.apps import AppConfig
 from django.apps.registry import Apps, apps as global_apps
@@ -13,6 +15,7 @@ from django.utils.module_loading import import_string
 from django.utils.version import get_docs_version
 
 from .exceptions import InvalidBasesError
+from .utils import resolve_relation
 
 
 def _get_app_label_and_model_name(model, app_label=''):
@@ -87,6 +90,8 @@ class ProjectState:
         # Apps to include from main registry, usually unmigrated ones
         self.real_apps = real_apps or []
         self.is_delayed = False
+        # {remote_model_key: {model_key: [(field_name, field)]}}
+        self.relations = None
 
     def add_model(self, model_state):
         app_label, model_name = model_state.app_label, model_state.name_lower
@@ -188,6 +193,67 @@ class ProjectState:
         # Render all models
         self.apps.render_multiple(states_to_be_rendered)
 
+    def resolve_fields_and_relations(self):
+        # Resolve fields.
+        for model_state in self.models.values():
+            for field_name, field in model_state.fields.items():
+                field.name = field_name
+        # Resolve relations.
+        # {remote_model_key: {model_key: [(field_name, field)]}}
+        self.relations = defaultdict(partial(defaultdict, list))
+        concretes, proxies = self._get_concrete_models_mapping_and_proxy_models()
+
+        real_apps = set(self.real_apps)
+        for model_key in concretes:
+            model_state = self.models[model_key]
+            for field_name, field in model_state.fields.items():
+                remote_field = field.remote_field
+                if not remote_field:
+                    continue
+                remote_model_key = resolve_relation(remote_field.model, *model_key)
+                if remote_model_key[0] not in real_apps and remote_model_key in concretes:
+                    remote_model_key = concretes[remote_model_key]
+                self.relations[remote_model_key][model_key].append((field_name, field))
+
+                through = getattr(remote_field, 'through', None)
+                if not through:
+                    continue
+                through_model_key = resolve_relation(through, *model_key)
+                if through_model_key[0] not in real_apps and through_model_key in concretes:
+                    through_model_key = concretes[through_model_key]
+                self.relations[through_model_key][model_key].append((field_name, field))
+        for model_key in proxies:
+            self.relations[model_key] = self.relations[concretes[model_key]]
+
+    def get_concrete_model_key(self, model):
+        concrete_models_mapping, _ = self._get_concrete_models_mapping_and_proxy_models()
+        model_key = make_model_tuple(model)
+        return concrete_models_mapping[model_key]
+
+    def _get_concrete_models_mapping_and_proxy_models(self):
+        concrete_models_mapping = {}
+        proxy_models = {}
+        # Split models to proxy and concrete models.
+        for model_key, model_state in self.models.items():
+            if model_state.options.get('proxy'):
+                proxy_models[model_key] = model_state
+                # Find a concrete model for the proxy.
+                concrete_models_mapping[model_key] = self._find_concrete_model_from_proxy(
+                    proxy_models, model_state,
+                )
+            else:
+                concrete_models_mapping[model_key] = model_key
+        return concrete_models_mapping, proxy_models
+
+    def _find_concrete_model_from_proxy(self, proxy_models, model_state):
+        for base in model_state.bases:
+            base_key = make_model_tuple(base)
+            base_state = proxy_models.get(base_key)
+            if not base_state:
+                # Concrete model found, stop looking at bases.
+                return base_key
+            return self._find_concrete_model_from_proxy(proxy_models, base_state)
+
     def clone(self):
         """Return an exact copy of this ProjectState."""
         new_state = ProjectState(
@@ -206,11 +272,6 @@ class ProjectState:
     @cached_property
     def apps(self):
         return StateApps(self.real_apps, self.models)
-
-    @property
-    def concrete_apps(self):
-        self.apps = StateApps(self.real_apps, self.models, ignore_swappable=True)
-        return self.apps
 
     @classmethod
     def from_apps(cls, apps):
@@ -391,6 +452,14 @@ class ModelState:
     @cached_property
     def name_lower(self):
         return self.name.lower()
+
+    def get_field(self, field_name):
+        field_name = (
+            self.options['order_with_respect_to']
+            if field_name == '_order'
+            else field_name
+        )
+        return self.fields[field_name]
 
     @classmethod
     def from_model(cls, model, exclude_rels=False):

tests/migrations/test_autodetector.py
@@ -584,9 +584,13 @@ class AutodetectorTests(TestCase):
         return project_state
 
     def get_changes(self, before_states, after_states, questioner=None):
+        if not isinstance(before_states, ProjectState):
+            before_states = self.make_project_state(before_states)
+        if not isinstance(after_states, ProjectState):
+            after_states = self.make_project_state(after_states)
         return MigrationAutodetector(
-            self.make_project_state(before_states),
-            self.make_project_state(after_states),
+            before_states,
+            after_states,
             questioner,
         )._detect_changes()
 
@@ -1646,30 +1650,38 @@ class AutodetectorTests(TestCase):
         """
         # First, we test the default pk field name
         changes = self.get_changes([], [self.author_empty, self.author_proxy_third, self.book_proxy_fk])
-        # The field name the FK on the book model points to
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[2][1].remote_field.field_name, 'id')
+        # The model the FK is pointing from and to.
+        self.assertEqual(
+            changes['otherapp'][0].operations[0].fields[2][1].remote_field.model,
+            'thirdapp.AuthorProxy',
+        )
         # Now, we test the custom pk field name
         changes = self.get_changes([], [self.author_custom_pk, self.author_proxy_third, self.book_proxy_fk])
-        # The field name the FK on the book model points to
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[2][1].remote_field.field_name, 'pk_field')
+        # The model the FK is pointing from and to.
+        self.assertEqual(
+            changes['otherapp'][0].operations[0].fields[2][1].remote_field.model,
+            'thirdapp.AuthorProxy',
+        )
 
     def test_proxy_to_mti_with_fk_to_proxy(self):
         # First, test the pk table and field name.
-        changes = self.get_changes(
-            [],
+        to_state = self.make_project_state(
             [self.author_empty, self.author_proxy_third, self.book_proxy_fk],
         )
+        changes = self.get_changes([], to_state)
+        fk_field = changes['otherapp'][0].operations[0].fields[2][1]
         self.assertEqual(
-            changes['otherapp'][0].operations[0].fields[2][1].remote_field.model._meta.db_table,
-            'testapp_author',
+            to_state.get_concrete_model_key(fk_field.remote_field.model),
+            ('testapp', 'author'),
         )
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[2][1].remote_field.field_name, 'id')
+        self.assertEqual(fk_field.remote_field.model, 'thirdapp.AuthorProxy')
 
         # Change AuthorProxy to use MTI.
-        changes = self.get_changes(
-            [self.author_empty, self.author_proxy_third, self.book_proxy_fk],
+        from_state = to_state.clone()
+        to_state = self.make_project_state(
             [self.author_empty, self.author_proxy_third_notproxy, self.book_proxy_fk],
         )
+        changes = self.get_changes(from_state, to_state)
         # Right number/type of migrations for the AuthorProxy model?
         self.assertNumberMigrations(changes, 'thirdapp', 1)
         self.assertOperationTypes(changes, 'thirdapp', 0, ['DeleteModel', 'CreateModel'])
@@ -1680,30 +1692,39 @@ class AutodetectorTests(TestCase):
         # otherapp should depend on thirdapp.
         self.assertMigrationDependencies(changes, 'otherapp', 0, [('thirdapp', 'auto_1')])
         # Now, test the pk table and field name.
+        fk_field = changes['otherapp'][0].operations[0].field
         self.assertEqual(
-            changes['otherapp'][0].operations[0].field.remote_field.model._meta.db_table,
-            'thirdapp_authorproxy',
+            to_state.get_concrete_model_key(fk_field.remote_field.model),
+            ('thirdapp', 'authorproxy'),
         )
-        self.assertEqual(changes['otherapp'][0].operations[0].field.remote_field.field_name, 'author_ptr')
+        self.assertEqual(fk_field.remote_field.model, 'thirdapp.AuthorProxy')
 
     def test_proxy_to_mti_with_fk_to_proxy_proxy(self):
         # First, test the pk table and field name.
-        changes = self.get_changes(
-            [],
-            [self.author_empty, self.author_proxy, self.author_proxy_proxy, self.book_proxy_proxy_fk],
-        )
+        to_state = self.make_project_state([
+            self.author_empty,
+            self.author_proxy,
+            self.author_proxy_proxy,
+            self.book_proxy_proxy_fk,
+        ])
+        changes = self.get_changes([], to_state)
+        fk_field = changes['otherapp'][0].operations[0].fields[1][1]
         self.assertEqual(
-            changes['otherapp'][0].operations[0].fields[1][1].remote_field.model._meta.db_table,
-            'testapp_author',
+            to_state.get_concrete_model_key(fk_field.remote_field.model),
+            ('testapp', 'author'),
         )
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[1][1].remote_field.field_name, 'id')
+        self.assertEqual(fk_field.remote_field.model, 'testapp.AAuthorProxyProxy')
 
         # Change AuthorProxy to use MTI. FK still points to AAuthorProxyProxy,
         # a proxy of AuthorProxy.
-        changes = self.get_changes(
-            [self.author_empty, self.author_proxy, self.author_proxy_proxy, self.book_proxy_proxy_fk],
-            [self.author_empty, self.author_proxy_notproxy, self.author_proxy_proxy, self.book_proxy_proxy_fk],
-        )
+        from_state = to_state.clone()
+        to_state = self.make_project_state([
+            self.author_empty,
+            self.author_proxy_notproxy,
+            self.author_proxy_proxy,
+            self.book_proxy_proxy_fk,
+        ])
+        changes = self.get_changes(from_state, to_state)
         # Right number/type of migrations for the AuthorProxy model?
         self.assertNumberMigrations(changes, 'testapp', 1)
         self.assertOperationTypes(changes, 'testapp', 0, ['DeleteModel', 'CreateModel'])
@@ -1714,11 +1735,12 @@ class AutodetectorTests(TestCase):
         # otherapp should depend on testapp.
         self.assertMigrationDependencies(changes, 'otherapp', 0, [('testapp', 'auto_1')])
         # Now, test the pk table and field name.
+        fk_field = changes['otherapp'][0].operations[0].field
         self.assertEqual(
-            changes['otherapp'][0].operations[0].field.remote_field.model._meta.db_table,
-            'testapp_authorproxy',
+            to_state.get_concrete_model_key(fk_field.remote_field.model),
+            ('testapp', 'authorproxy'),
         )
-        self.assertEqual(changes['otherapp'][0].operations[0].field.remote_field.field_name, 'author_ptr')
+        self.assertEqual(fk_field.remote_field.model, 'testapp.AAuthorProxyProxy')
 
     def test_unmanaged_create(self):
         """The autodetector correctly deals with managed models."""
@@ -1761,12 +1783,14 @@ class AutodetectorTests(TestCase):
         """
         # First, we test the default pk field name
         changes = self.get_changes([], [self.author_unmanaged_default_pk, self.book])
-        # The field name the FK on the book model points to
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[2][1].remote_field.field_name, 'id')
+        # The model the FK on the book model points to.
+        fk_field = changes['otherapp'][0].operations[0].fields[2][1]
+        self.assertEqual(fk_field.remote_field.model, 'testapp.Author')
         # Now, we test the custom pk field name
         changes = self.get_changes([], [self.author_unmanaged_custom_pk, self.book])
-        # The field name the FK on the book model points to
-        self.assertEqual(changes['otherapp'][0].operations[0].fields[2][1].remote_field.field_name, 'pk_field')
+        # The model the FK on the book model points to.
+        fk_field = changes['otherapp'][0].operations[0].fields[2][1]
+        self.assertEqual(fk_field.remote_field.model, 'testapp.Author')
 
     @override_settings(AUTH_USER_MODEL="thirdapp.CustomUser")
     def test_swappable(self):
@@ -1790,11 +1814,7 @@ class AutodetectorTests(TestCase):
         self.assertOperationTypes(changes, 'testapp', 0, ["AlterField"])
         self.assertOperationAttributes(changes, 'testapp', 0, 0, model_name="author", name='user')
         fk_field = changes['testapp'][0].operations[0].field
-        to_model = '%s.%s' % (
-            fk_field.remote_field.model._meta.app_label,
-            fk_field.remote_field.model._meta.object_name,
-        )
-        self.assertEqual(to_model, 'thirdapp.CustomUser')
+        self.assertEqual(fk_field.remote_field.model, 'thirdapp.CustomUser')
 
     def test_add_field_with_default(self):
         """#22030 - Adding a field with a default should work."""

commit 87f91c5a0e4c3f95dd5341e576cb980526b57b03

Fixed #33449 -- Fixed makemigrations crash on models without Meta.order_with_respect_to but with _order field.
Regression in 398251f7484df7cfa45eaf1bc768cb37b2bd1f2f.

AUTHORS
@@ -307,6 +307,7 @@ answer newbie questions, and generally made Django that much better:
     Étienne Beaulé <beauleetienne0@gmail.com>
     Eugene Lazutkin <http://lazutkin.com/blog/>
     Evan Grim <https://github.com/egrim>
+    Fabian Büchler <fabian.buechler@inoqo.com>
     Fabrice Aneche <akh@nobugware.com>
     Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
     favo@exoweb.net

django/db/migrations/state.py
@@ -685,11 +685,8 @@ class ModelState:
         return self.name.lower()
 
     def get_field(self, field_name):
-        field_name = (
-            self.options['order_with_respect_to']
-            if field_name == '_order'
-            else field_name
-        )
+        if field_name == '_order':
+            field_name = self.options.get('order_with_respect_to', field_name)
         return self.fields[field_name]
 
     @classmethod

tests/migrations/test_state.py
@@ -959,6 +959,44 @@ class StateTests(SimpleTestCase):
             ["id", "author"],
         )
 
+    def test_modelstate_get_field_order_wrt(self):
+        new_apps = Apps()
+
+        class Author(models.Model):
+            name = models.TextField()
+
+            class Meta:
+                app_label = 'migrations'
+                apps = new_apps
+
+        class Book(models.Model):
+            author = models.ForeignKey(Author, models.CASCADE)
+
+            class Meta:
+                app_label = 'migrations'
+                apps = new_apps
+                order_with_respect_to = 'author'
+
+        model_state = ModelState.from_model(Book)
+        order_wrt_field = model_state.get_field('_order')
+        self.assertIsInstance(order_wrt_field, models.ForeignKey)
+        self.assertEqual(order_wrt_field.related_model, 'migrations.author')
+
+    def test_modelstate_get_field_no_order_wrt_order_field(self):
+        new_apps = Apps()
+
+        class HistoricalRecord(models.Model):
+            _order = models.PositiveSmallIntegerField()
+
+            class Meta:
+                app_label = 'migrations'
+                apps = new_apps
+
+        model_state = ModelState.from_model(HistoricalRecord)
+        order_field = model_state.get_field('_order')
+        self.assertIsNone(order_field.related_model)
+        self.assertIsInstance(order_field, models.PositiveSmallIntegerField)
+
     def test_manager_refer_correct_model_version(self):
         """
         #24147 - Managers refer to the correct version of a