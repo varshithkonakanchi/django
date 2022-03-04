varshithkonakanchi/django

commit fed416e3722d487c1c3e57e74b205ec18869d635

Refs #31169 -- Added DatabaseCreation.setup_worker_connection() hook.

django/db/backends/base/creation.py
@@ -369,3 +369,13 @@ class BaseDatabaseCreation:
             settings_dict["ENGINE"],
             self._get_test_db_name(),
         )
+
+    def setup_worker_connection(self, _worker_id):
+        settings_dict = self.get_test_db_clone_settings(str(_worker_id))
+        # connection.settings_dict must be updated in place for changes to be
+        # reflected in django.db.connections. If the following line assigned
+        # connection.settings_dict = settings_dict, new threads would connect
+        # to the default database instead of the appropriate clone.
+        self.connection.settings_dict.update(settings_dict)
+        self.mark_expected_failures_and_skips()
+        self.connection.close()

django/test/runner.py
@@ -407,13 +407,7 @@ def _init_worker(counter):
 
     for alias in connections:
         connection = connections[alias]
-        settings_dict = connection.creation.get_test_db_clone_settings(str(_worker_id))
-        # connection.settings_dict must be updated in place for changes to be
-        # reflected in django.db.connections. If the following line assigned
-        # connection.settings_dict = settings_dict, new threads would connect
-        # to the default database instead of the appropriate clone.
-        connection.settings_dict.update(settings_dict)
-        connection.close()
+        connection.creation.setup_worker_connection(_worker_id)
 
 
 def _run_subsuite(args):

commit cf1290f125134a60086603ba147d08f631e4669a

Refs #31169 -- Prevented infinite loop in tests on failures.
Regression in fed416e3722d487c1c3e57e74b205ec18869d635.

django/db/backends/base/creation.py
@@ -377,5 +377,4 @@ class BaseDatabaseCreation:
         # connection.settings_dict = settings_dict, new threads would connect
         # to the default database instead of the appropriate clone.
         self.connection.settings_dict.update(settings_dict)
-        self.mark_expected_failures_and_skips()
         self.connection.close()