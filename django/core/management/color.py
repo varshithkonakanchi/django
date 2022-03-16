varshithkonakanchi/django

commit f2e1717b483b33f13939e82926a545f2cac483d7

Fixed #31216 -- Added support for colorama terminal colors on Windows.
Modern setups on Windows support terminal colors.
The colorama library may also be used, as an
alternative to the ANSICON library.

django/core/management/color.py
@@ -8,17 +8,52 @@ import sys
 
 from django.utils import termcolors
 
+try:
+    import colorama
+except ImportError:
+    HAS_COLORAMA = False
+else:
+    colorama.init()
+    HAS_COLORAMA = True
+
 
 def supports_color():
     """
     Return True if the running system's terminal supports color,
     and False otherwise.
     """
-    supported_platform = sys.platform != 'win32' or 'ANSICON' in os.environ
+    def vt_codes_enabled_in_windows_registry():
+        """
+        Check the Windows Registry to see if VT code handling has been enabled
+        by default, see https://superuser.com/a/1300251/447564.
+        """
+        try:
+            # winreg is only available on Windows.
+            import winreg
+        except ImportError:
+            return False
+        else:
+            reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Console')
+            try:
+                reg_key_value, _ = winreg.QueryValueEx(reg_key, 'VirtualTerminalLevel')
+            except FileNotFoundError:
+                return False
+            else:
+                return reg_key_value == 1
 
     # isatty is not always implemented, #6223.
     is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
-    return supported_platform and is_a_tty
+
+    return is_a_tty and (
+        sys.platform != 'win32' or
+        HAS_COLORAMA or
+        'ANSICON' in os.environ or
+        # Windows Terminal supports VT codes.
+        'WT_SESSION' in os.environ or
+        # Microsoft Visual Studio Code's built-in terminal supports colors.
+        os.environ.get('TERM_PROGRAM') == 'vscode' or
+        vt_codes_enabled_in_windows_registry()
+    )
 
 
 class Style:

commit 048477961e5ba1b922cfc85e7ebd612cfc11a295

Fixed #33580 -- Fixed crash when checking support for terminal colors on Wine.
Regression in f2e1717b483b33f13939e82926a545f2cac483d7.

django/core/management/color.py
@@ -35,8 +35,8 @@ def supports_color():
         except ImportError:
             return False
         else:
-            reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Console")
             try:
+                reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Console")
                 reg_key_value, _ = winreg.QueryValueEx(reg_key, "VirtualTerminalLevel")
             except FileNotFoundError:
                 return False