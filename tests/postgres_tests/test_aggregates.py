varshithkonakanchi/django

commit 8c6bbd5726afa9b82a332c43d3a46a04792e8ca8

Refs #33304 -- Enclosed aggregate ordering logic in an expression.
This greatly simplifies the implementation of contrib.postgres'
OrderableAggMixin and allows for reuse in Window expressions.

django/db/models/expressions.py
@@ -915,8 +915,8 @@ class Ref(Expression):
 class ExpressionList(Func):
     """
     An expression containing multiple expressions. Can be used to provide a
-    list of expressions as an argument to another expression, like an
-    ordering clause.
+    list of expressions as an argument to another expression, like a partition
+    clause.
     """
     template = '%(expressions)s'
 
@@ -931,6 +931,26 @@ class ExpressionList(Func):
     def as_sqlite(self, compiler, connection, **extra_context):
         # Casting to numeric is unnecessary.
         return self.as_sql(compiler, connection, **extra_context)
+
+
+class OrderByList(Func):
+    template = 'ORDER BY %(expressions)s'
+
+    def __init__(self, *expressions, **extra):
+        expressions = (
+            (
+                OrderBy(F(expr[1:]), descending=True)
+                if isinstance(expr, str) and expr[0] == '-'
+                else expr
+            )
+            for expr in expressions
+        )
+        super().__init__(*expressions, **extra)
+
+    def as_sql(self, *args, **kwargs):
+        if not self.source_expressions:
+            return '', ()
+        return super().as_sql(*args, **kwargs)
 
 
 class ExpressionWrapper(SQLiteNumericMixin, Expression):

django/contrib/postgres/aggregates/mixins.py
@@ -1,48 +1,27 @@
-from django.db.models import F, OrderBy
+from django.db.models.expressions import OrderByList
 
 
 class OrderableAggMixin:
 
     def __init__(self, *expressions, ordering=(), **extra):
-        if not isinstance(ordering, (list, tuple)):
-            ordering = [ordering]
-        ordering = ordering or []
-        # Transform minus sign prefixed strings into an OrderBy() expression.
-        ordering = (
-            (OrderBy(F(o[1:]), descending=True) if isinstance(o, str) and o[0] == '-' else o)
-            for o in ordering
-        )
+        if isinstance(ordering, (list, tuple)):
+            self.order_by = OrderByList(*ordering)
+        else:
+            self.order_by = OrderByList(ordering)
         super().__init__(*expressions, **extra)
-        self.ordering = self._parse_expressions(*ordering)
 
     def resolve_expression(self, *args, **kwargs):
-        self.ordering = [expr.resolve_expression(*args, **kwargs) for expr in self.ordering]
+        self.order_by = self.order_by.resolve_expression(*args, **kwargs)
         return super().resolve_expression(*args, **kwargs)
 
-    def as_sql(self, compiler, connection):
-        if self.ordering:
-            ordering_params = []
-            ordering_expr_sql = []
-            for expr in self.ordering:
-                expr_sql, expr_params = compiler.compile(expr)
-                ordering_expr_sql.append(expr_sql)
-                ordering_params.extend(expr_params)
-            sql, sql_params = super().as_sql(compiler, connection, ordering=(
-                'ORDER BY ' + ', '.join(ordering_expr_sql)
-            ))
-            return sql, (*sql_params, *ordering_params)
-        return super().as_sql(compiler, connection, ordering='')
+    def get_source_expressions(self):
+        return super().get_source_expressions() + [self.order_by]
 
     def set_source_expressions(self, exprs):
-        # Extract the ordering expressions because ORDER BY clause is handled
-        # in a custom way.
-        self.ordering = exprs[self._get_ordering_expressions_index():]
-        return super().set_source_expressions(exprs[:self._get_ordering_expressions_index()])
+        *exprs, self.order_by = exprs
+        return super().set_source_expressions(exprs)
 
-    def get_source_expressions(self):
-        return super().get_source_expressions() + self.ordering
-
-    def _get_ordering_expressions_index(self):
-        """Return the index at which the ordering expressions start."""
-        source_expressions = self.get_source_expressions()
-        return len(source_expressions) - len(self.ordering)
+    def as_sql(self, compiler, connection):
+        order_by_sql, order_by_params = compiler.compile(self.order_by)
+        sql, sql_params = super().as_sql(compiler, connection, ordering=order_by_sql)
+        return sql, (*sql_params, *order_by_params)

commit 8243ee9092566db6bc5f89dd7b56df3ce6ba1a90

Fixed #33898 -- Fixed Window() expression crash with ArrayAgg().
Thanks Kia for the report.

Regression in 8c6bbd5726afa9b82a332c43d3a46a04792e8ca8.

django/db/models/expressions.py
@@ -1669,7 +1669,7 @@ class Window(SQLiteNumericMixin, Expression):
         if not connection.features.supports_over_clause:
             raise NotSupportedError("This backend does not support window expressions.")
         expr_sql, params = compiler.compile(self.source_expression)
-        window_sql, window_params = [], []
+        window_sql, window_params = [], ()
 
         if self.partition_by is not None:
             sql_expr, sql_params = self.partition_by.as_sql(
@@ -1678,24 +1678,23 @@ class Window(SQLiteNumericMixin, Expression):
                 template="PARTITION BY %(expressions)s",
             )
             window_sql.append(sql_expr)
-            window_params.extend(sql_params)
+            window_params += tuple(sql_params)
 
         if self.order_by is not None:
             order_sql, order_params = compiler.compile(self.order_by)
             window_sql.append(order_sql)
-            window_params.extend(order_params)
+            window_params += tuple(order_params)
 
         if self.frame:
             frame_sql, frame_params = compiler.compile(self.frame)
             window_sql.append(frame_sql)
-            window_params.extend(frame_params)
+            window_params += tuple(frame_params)
 
-        params.extend(window_params)
         template = template or self.template
 
         return (
             template % {"expression": expr_sql, "window": " ".join(window_sql).strip()},
-            params,
+            (*params, *window_params),
         )
 
     def as_sqlite(self, compiler, connection):

tests/postgres_tests/test_aggregates.py
@@ -8,6 +8,7 @@ from django.db.models import (
     Q,
     Subquery,
     Value,
+    Window,
 )
 from django.db.models.fields.json import KeyTextTransform, KeyTransform
 from django.db.models.functions import Cast, Concat, Substr
@@ -667,6 +668,22 @@ class TestGeneralAggregate(PostgreSQLTestCase):
         self.assertSequenceEqual(
             qs.first().integers,
             inner_qs.values_list("integer_field", flat=True),
+        )
+
+    def test_window(self):
+        self.assertCountEqual(
+            AggregateTestModel.objects.annotate(
+                integers=Window(
+                    expression=ArrayAgg("char_field"),
+                    partition_by=F("integer_field"),
+                )
+            ).values("integers", "char_field"),
+            [
+                {"integers": ["Foo1", "Foo3"], "char_field": "Foo1"},
+                {"integers": ["Foo1", "Foo3"], "char_field": "Foo3"},
+                {"integers": ["Foo2"], "char_field": "Foo2"},
+                {"integers": ["Foo4"], "char_field": "Foo4"},
+            ],
         )
 
 