varshithkonakanchi/django

commit 467ffd1201bc03c45b5e82077397cc3a4d6efdd7

Fixed #33201 -- Made RenameModel operation a noop for models with db_table.

django/db/migrations/operations/models.py
@@ -320,12 +320,13 @@ class RenameModel(ModelOperation):
         new_model = to_state.apps.get_model(app_label, self.new_name)
         if self.allow_migrate_model(schema_editor.connection.alias, new_model):
             old_model = from_state.apps.get_model(app_label, self.old_name)
+            old_db_table = old_model._meta.db_table
+            new_db_table = new_model._meta.db_table
+            # Don't alter when a table name is not changed.
+            if old_db_table == new_db_table:
+                return
             # Move the main table
-            schema_editor.alter_db_table(
-                new_model,
-                old_model._meta.db_table,
-                new_model._meta.db_table,
-            )
+            schema_editor.alter_db_table(new_model, old_db_table, new_db_table)
             # Alter the fields pointing to us
             for related_object in old_model._meta.related_objects:
                 if related_object.related_model == old_model:

tests/migrations/test_operations.py
@@ -793,6 +793,28 @@ class OperationTests(OperationTestBase):
         self.assertEqual(Rider.objects.count(), 2)
         self.assertEqual(Pony._meta.get_field('riders').remote_field.through.objects.count(), 2)
 
+    def test_rename_model_with_db_table_noop(self):
+        app_label = 'test_rmwdbtn'
+        project_state = self.apply_operations(app_label, ProjectState(), operations=[
+            migrations.CreateModel('Rider', fields=[
+                ('id', models.AutoField(primary_key=True)),
+            ], options={'db_table': 'rider'}),
+            migrations.CreateModel('Pony', fields=[
+                ('id', models.AutoField(primary_key=True)),
+                ('rider', models.ForeignKey('%s.Rider' % app_label, models.CASCADE)),
+            ]),
+        ])
+        new_state = project_state.clone()
+        operation = migrations.RenameModel('Rider', 'Runner')
+        operation.state_forwards(app_label, new_state)
+
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_forwards(app_label, editor, project_state, new_state)
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_backwards(app_label, editor, new_state, project_state)
+
     def test_rename_m2m_target_model(self):
         app_label = "test_rename_m2m_target_model"
         project_state = self.apply_operations(app_label, ProjectState(), operations=[

commit 0111e67ae1693bcc0c6bbc19d66b371cc485e9b5

Fixed #33953 -- Reverted "Fixed #33201 -- Made RenameModel operation a noop for models with db_table."
Regression in 467ffd1201bc03c45b5e82077397cc3a4d6efdd7.
This reverts 467ffd1201bc03c45b5e82077397cc3a4d6efdd7.

Thanks Timothy Thomas for the report.

django/db/migrations/operations/models.py
@@ -371,13 +371,12 @@ class RenameModel(ModelOperation):
         new_model = to_state.apps.get_model(app_label, self.new_name)
         if self.allow_migrate_model(schema_editor.connection.alias, new_model):
             old_model = from_state.apps.get_model(app_label, self.old_name)
-            old_db_table = old_model._meta.db_table
-            new_db_table = new_model._meta.db_table
-            # Don't alter when a table name is not changed.
-            if old_db_table == new_db_table:
-                return
             # Move the main table
-            schema_editor.alter_db_table(new_model, old_db_table, new_db_table)
+            schema_editor.alter_db_table(
+                new_model,
+                old_model._meta.db_table,
+                new_model._meta.db_table,
+            )
             # Alter the fields pointing to us
             for related_object in old_model._meta.related_objects:
                 if related_object.related_model == old_model:

tests/migrations/test_operations.py
@@ -1058,8 +1058,8 @@ class OperationTests(OperationTestBase):
             Pony._meta.get_field("riders").remote_field.through.objects.count(), 2
         )
 
-    def test_rename_model_with_db_table_noop(self):
-        app_label = "test_rmwdbtn"
+    def test_rename_model_with_db_table_rename_m2m(self):
+        app_label = "test_rmwdbrm2m"
         project_state = self.apply_operations(
             app_label,
             ProjectState(),
@@ -1069,32 +1069,28 @@ class OperationTests(OperationTestBase):
                     fields=[
                         ("id", models.AutoField(primary_key=True)),
                     ],
-                    options={"db_table": "rider"},
                 ),
                 migrations.CreateModel(
                     "Pony",
                     fields=[
                         ("id", models.AutoField(primary_key=True)),
-                        (
-                            "rider",
-                            models.ForeignKey("%s.Rider" % app_label, models.CASCADE),
-                        ),
+                        ("riders", models.ManyToManyField("Rider")),
                     ],
+                    options={"db_table": "pony"},
                 ),
             ],
         )
         new_state = project_state.clone()
-        operation = migrations.RenameModel("Rider", "Runner")
+        operation = migrations.RenameModel("Pony", "PinkPony")
         operation.state_forwards(app_label, new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
 
-        with connection.schema_editor() as editor:
-            with self.assertNumQueries(0):
-                operation.database_forwards(app_label, editor, project_state, new_state)
-        with connection.schema_editor() as editor:
-            with self.assertNumQueries(0):
-                operation.database_backwards(
-                    app_label, editor, new_state, project_state
-                )
+        Pony = new_state.apps.get_model(app_label, "PinkPony")
+        Rider = new_state.apps.get_model(app_label, "Rider")
+        pony = Pony.objects.create()
+        rider = Rider.objects.create()
+        pony.riders.add(rider)
 
     def test_rename_m2m_target_model(self):
         app_label = "test_rename_m2m_target_model"