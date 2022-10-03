varshithkonakanchi/django

commit a22050c4140da078c3fd8ea15bd8a419c78d2fc9

Fixed #32987 -- Added system check for template tag modules with the same name.
Co-authored-by: Daniel Fairhead <daniel@dev.ngo>

django/core/checks/templates.py
@@ -1,6 +1,8 @@
 import copy
+from collections import defaultdict
 
 from django.conf import settings
+from django.template.backends.django import get_template_tag_modules
 
 from . import Error, Tags, register
 
@@ -12,6 +14,10 @@ E001 = Error(
 E002 = Error(
     "'string_if_invalid' in TEMPLATES OPTIONS must be a string but got: {} ({}).",
     id="templates.E002",
+)
+E003 = Error(
+    '{} is used for multiple template tag modules: {}',
+    id='templates.E003',
 )
 
 
@@ -32,4 +38,30 @@ def check_string_if_invalid_is_string(app_configs, **kwargs):
             error = copy.copy(E002)
             error.msg = error.msg.format(string_if_invalid, type(string_if_invalid).__name__)
             errors.append(error)
+    return errors
+
+
+@register(Tags.templates)
+def check_for_template_tags_with_the_same_name(app_configs, **kwargs):
+    errors = []
+    libraries = defaultdict(list)
+
+    for conf in settings.TEMPLATES:
+        custom_libraries = conf.get('OPTIONS', {}).get('libraries', {})
+        for module_name, module_path in custom_libraries.items():
+            libraries[module_name].append(module_path)
+
+    for module_name, module_path in get_template_tag_modules():
+        libraries[module_name].append(module_path)
+
+    for library_name, items in libraries.items():
+        if len(items) > 1:
+            errors.append(Error(
+                E003.msg.format(
+                    repr(library_name),
+                    ', '.join(repr(item) for item in items),
+                ),
+                id=E003.id,
+            ))
+
     return errors

tests/check_framework/test_templates.py
@@ -1,8 +1,9 @@
 from copy import copy, deepcopy
 
+from django.core.checks import Error
 from django.core.checks.templates import (
-    E001, E002, check_setting_app_dirs_loaders,
-    check_string_if_invalid_is_string,
+    E001, E002, E003, check_for_template_tags_with_the_same_name,
+    check_setting_app_dirs_loaders, check_string_if_invalid_is_string,
 )
 from django.test import SimpleTestCase
 from django.test.utils import override_settings
@@ -87,3 +88,85 @@ class CheckTemplateStringIfInvalidTest(SimpleTestCase):
         del TEMPLATES[1]['OPTIONS']['string_if_invalid']
         with self.settings(TEMPLATES=TEMPLATES):
             self.assertEqual(check_string_if_invalid_is_string(None), [self.error1])
+
+
+class CheckTemplateTagLibrariesWithSameName(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.error_same_tags = Error(
+            E003.msg.format(
+                "'same_tags'",
+                "'check_framework.template_test_apps.same_tags_app_1."
+                "templatetags.same_tags', "
+                "'check_framework.template_test_apps.same_tags_app_2."
+                "templatetags.same_tags'",
+            ),
+            id=E003.id,
+        )
+
+    @staticmethod
+    def get_settings(module_name, module_path):
+        return {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'OPTIONS': {
+                'libraries': {
+                    module_name: f'check_framework.template_test_apps.{module_path}',
+                },
+            },
+        }
+
+    @override_settings(INSTALLED_APPS=[
+        'check_framework.template_test_apps.same_tags_app_1',
+        'check_framework.template_test_apps.same_tags_app_2',
+    ])
+    def test_template_tags_with_same_name(self):
+        self.assertEqual(
+            check_for_template_tags_with_the_same_name(None),
+            [self.error_same_tags],
+        )
+
+    def test_template_tags_with_same_library_name(self):
+        with self.settings(TEMPLATES=[
+            self.get_settings('same_tags', 'same_tags_app_1.templatetags.same_tags'),
+            self.get_settings('same_tags', 'same_tags_app_2.templatetags.same_tags'),
+        ]):
+            self.assertEqual(
+                check_for_template_tags_with_the_same_name(None),
+                [self.error_same_tags],
+            )
+
+    @override_settings(INSTALLED_APPS=[
+        'check_framework.template_test_apps.same_tags_app_1'
+    ])
+    def test_template_tags_with_same_library_name_and_module_name(self):
+        with self.settings(TEMPLATES=[
+            self.get_settings(
+                'same_tags',
+                'different_tags_app.templatetags.different_tags',
+            ),
+        ]):
+            self.assertEqual(check_for_template_tags_with_the_same_name(None), [Error(
+                E003.msg.format(
+                    "'same_tags'",
+                    "'check_framework.template_test_apps.different_tags_app."
+                    "templatetags.different_tags', "
+                    "'check_framework.template_test_apps.same_tags_app_1."
+                    "templatetags.same_tags'",
+                ),
+                id=E003.id,
+            )])
+
+    def test_template_tags_with_different_library_name(self):
+        with self.settings(TEMPLATES=[
+            self.get_settings('same_tags', 'same_tags_app_1.templatetags.same_tags'),
+            self.get_settings('not_same_tags', 'same_tags_app_2.templatetags.same_tags'),
+        ]):
+            self.assertEqual(check_for_template_tags_with_the_same_name(None), [])
+
+    @override_settings(INSTALLED_APPS=[
+        'check_framework.template_test_apps.same_tags_app_1',
+        'check_framework.template_test_apps.different_tags_app',
+    ])
+    def test_template_tags_with_different_name(self):
+        self.assertEqual(check_for_template_tags_with_the_same_name(None), [])

AUTHORS
@@ -223,6 +223,7 @@ answer newbie questions, and generally made Django that much better:
     Daniel Alves Barbosa de Oliveira Vaz <danielvaz@gmail.com>
     Daniel Duan <DaNmarner@gmail.com>
     Daniele Procida <daniele@vurt.org>
+    Daniel Fairhead <danthedeckie@gmail.com>
     Daniel Greenfeld
     dAniel hAhler
     Daniel Jilg <daniel@breakthesystem.org>
@@ -860,6 +861,7 @@ answer newbie questions, and generally made Django that much better:
     Shai Berger <shai@platonix.com>
     Shannon -jj Behrens <https://www.jjinux.com/>
     Shawn Milochik <shawn@milochik.com>
+    Shreya Bamne <shreya.bamne@gmail.com>
     Silvan Spross <silvan.spross@gmail.com>
     Simeon Visser <http://simeonvisser.com>
     Simon Blanchard

tests/check_framework/template_test_apps/different_tags_app/apps.py
@@ -1,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DifferentTagsAppAppConfig(AppConfig):
+    name = 'check_framework.template_test_apps.different_tags_app'

tests/check_framework/template_test_apps/different_tags_app/templatetags/different_tags.py
@@ -1,0 +1,3 @@
+from django.template import Library
+
+register = Library()

tests/check_framework/template_test_apps/same_tags_app_1/apps.py
@@ -1,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SameTagsApp1AppConfig(AppConfig):
+    name = 'check_framework.template_test_apps.same_tags_app_1'

tests/check_framework/template_test_apps/same_tags_app_1/templatetags/same_tags.py
@@ -1,0 +1,3 @@
+from django.template import Library
+
+register = Library()

tests/check_framework/template_test_apps/same_tags_app_2/apps.py
@@ -1,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SameTagsApp2AppConfig(AppConfig):
+    name = 'check_framework.template_test_apps.same_tags_app_2'

commit 530cd501dc796ff51e37ed5931bb98652ce4c95a

Refs #32987 -- Relaxed system check for template tag modules with the same name by turning into a warning.
Thanks Claude Paroz for the report.

Regression in a22050c4140da078c3fd8ea15bd8a419c78d2fc9.

django/core/checks/templates.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from django.conf import settings
 from django.template.backends.django import get_template_tag_modules
 
-from . import Error, Tags, register
+from . import Error, Tags, Warning, register
 
 E001 = Error(
     "You have 'APP_DIRS': True in your TEMPLATES but also specify 'loaders' "
@@ -15,7 +15,7 @@ E002 = Error(
     "'string_if_invalid' in TEMPLATES OPTIONS must be a string but got: {} ({}).",
     id="templates.E002",
 )
-E003 = Error(
+W003 = Warning(
     "{} is used for multiple template tag modules: {}",
     id="templates.E003",
 )
@@ -63,12 +63,12 @@ def check_for_template_tags_with_the_same_name(app_configs, **kwargs):
     for library_name, items in libraries.items():
         if len(items) > 1:
             errors.append(
-                Error(
-                    E003.msg.format(
+                Warning(
+                    W003.msg.format(
                         repr(library_name),
                         ", ".join(repr(item) for item in sorted(items)),
                     ),
-                    id=E003.id,
+                    id=W003.id,
                 )
             )
 

tests/check_framework/test_templates.py
@@ -1,10 +1,10 @@
 from copy import copy, deepcopy
 
-from django.core.checks import Error
+from django.core.checks import Warning
 from django.core.checks.templates import (
     E001,
     E002,
-    E003,
+    W003,
     check_for_template_tags_with_the_same_name,
     check_setting_app_dirs_loaders,
     check_string_if_invalid_is_string,
@@ -108,15 +108,15 @@ class CheckTemplateTagLibrariesWithSameName(SimpleTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.error_same_tags = Error(
-            E003.msg.format(
+        cls.warning_same_tags = Warning(
+            W003.msg.format(
                 "'same_tags'",
                 "'check_framework.template_test_apps.same_tags_app_1."
                 "templatetags.same_tags', "
                 "'check_framework.template_test_apps.same_tags_app_2."
                 "templatetags.same_tags'",
             ),
-            id=E003.id,
+            id=W003.id,
         )
 
     @staticmethod
@@ -139,7 +139,7 @@ class CheckTemplateTagLibrariesWithSameName(SimpleTestCase):
     def test_template_tags_with_same_name(self):
         self.assertEqual(
             check_for_template_tags_with_the_same_name(None),
-            [self.error_same_tags],
+            [self.warning_same_tags],
         )
 
     def test_template_tags_with_same_library_name(self):
@@ -155,7 +155,7 @@ class CheckTemplateTagLibrariesWithSameName(SimpleTestCase):
         ):
             self.assertEqual(
                 check_for_template_tags_with_the_same_name(None),
-                [self.error_same_tags],
+                [self.warning_same_tags],
             )
 
     @override_settings(
@@ -186,15 +186,15 @@ class CheckTemplateTagLibrariesWithSameName(SimpleTestCase):
             self.assertEqual(
                 check_for_template_tags_with_the_same_name(None),
                 [
-                    Error(
-                        E003.msg.format(
+                    Warning(
+                        W003.msg.format(
                             "'same_tags'",
                             "'check_framework.template_test_apps.different_tags_app."
                             "templatetags.different_tags', "
                             "'check_framework.template_test_apps.same_tags_app_1."
                             "templatetags.same_tags'",
                         ),
-                        id=E003.id,
+                        id=W003.id,
                     )
                 ],
             )