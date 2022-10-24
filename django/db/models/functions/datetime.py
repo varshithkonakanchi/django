varshithkonakanchi/django

commit 4187edf2c14be6be4eb79fee516b2a8dbb1db597

Fixed #34070 -- Added subsecond support to Now() on SQLite and MySQL.

django/db/models/functions/datetime.py
@@ -223,6 +223,19 @@ class Now(Func):
             compiler, connection, template="STATEMENT_TIMESTAMP()", **extra_context
         )
 
+    def as_mysql(self, compiler, connection, **extra_context):
+        return self.as_sql(
+            compiler, connection, template="CURRENT_TIMESTAMP(6)", **extra_context
+        )
+
+    def as_sqlite(self, compiler, connection, **extra_context):
+        return self.as_sql(
+            compiler,
+            connection,
+            template="STRFTIME('%%Y-%%m-%%d %%H:%%M:%%f', 'NOW')",
+            **extra_context,
+        )
+
 
 class TruncBase(TimezoneMixin, Transform):
     kind = None

django/db/backends/mysql/features.py
@@ -81,7 +81,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "swedish_ci": f"{charset}_swedish_ci",
         }
 
-    test_now_utc_template = "UTC_TIMESTAMP"
+    test_now_utc_template = "UTC_TIMESTAMP(6)"
 
     @cached_property
     def django_test_skips(self):

tests/db_functions/datetime/test_now.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 
-from django.db.models.functions import Now
+from django.db import connection
+from django.db.models import TextField
+from django.db.models.functions import Cast, Now
 from django.test import TestCase
 from django.utils import timezone
 
@@ -47,3 +49,17 @@ class NowTests(TestCase):
             ["How to Time Travel"],
             lambda a: a.title,
         )
+
+    def test_microseconds(self):
+        Article.objects.create(
+            title="How to Django",
+            text=lorem_ipsum,
+            written=timezone.now(),
+        )
+        now_string = (
+            Article.objects.annotate(now_string=Cast(Now(), TextField()))
+            .get()
+            .now_string
+        )
+        precision = connection.features.time_cast_precision
+        self.assertRegex(now_string, rf"^.*\.\d{{1,{precision}}}")

commit 26a1869abaa2f4cf07a9c64d38cdca7c772159e8

Refs #34070 -- Fixed date format in Now() on SQLite.
Regression in 4187edf2c14be6be4eb79fee516b2a8dbb1db597.

django/db/models/functions/datetime.py
@@ -232,7 +232,7 @@ class Now(Func):
         return self.as_sql(
             compiler,
             connection,
-            template="STRFTIME('%%Y-%%m-%%d %%H:%%M:%%f', 'NOW')",
+            template="STRFTIME('%%%%Y-%%%%m-%%%%d %%%%H:%%%%M:%%%%f', 'NOW')",
             **extra_context,
         )
 