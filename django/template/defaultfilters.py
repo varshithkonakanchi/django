varshithkonakanchi/django

commit 6c5ebec56179198a261cb222fa539eccff7bd6f7

Fixed #34098 -- Fixed loss of precision for Decimal values in floatformat filter.
Regression in d1d964f3c1a4c63f36a900297c1888663cf7a314.

django/template/defaultfilters.py
@@ -151,7 +151,7 @@ def floatformat(text, arg=-1):
             use_l10n = False
             arg = arg[:-1] or -1
     try:
-        input_val = repr(text)
+        input_val = str(text)
         d = Decimal(input_val)
     except InvalidOperation:
         try:

tests/template_tests/filter_tests/test_floatformat.py
@@ -56,6 +56,10 @@ class FunctionTests(SimpleTestCase):
         self.assertEqual(floatformat(0.12345, 2), "0.12")
         self.assertEqual(floatformat(Decimal("555.555"), 2), "555.56")
         self.assertEqual(floatformat(Decimal("09.000")), "9")
+        self.assertEqual(
+            floatformat(Decimal("123456.123456789012345678901"), 21),
+            "123456.123456789012345678901",
+        )
         self.assertEqual(floatformat("foo"), "")
         self.assertEqual(floatformat(13.1031, "bar"), "13.1031")
         self.assertEqual(floatformat(18.125, 2), "18.13")

commit 4b8264e1282cb138bb88332e30561b70fb2f081e

Fixed #34363 -- Fixed floatformat crash on zero with trailing zeros.
Regression in 6c5ebec56179198a261cb222fa539eccff7bd6f7.
Follow up to 63d4177cf866d719d96feefdc14d439cf0f76a96.

django/template/defaultfilters.py
@@ -3,7 +3,7 @@ import random as random_module
 import re
 import types
 import warnings
-from decimal import ROUND_HALF_UP, Context, Decimal, InvalidOperation
+from decimal import ROUND_HALF_UP, Context, Decimal, InvalidOperation, getcontext
 from functools import wraps
 from inspect import unwrap
 from operator import itemgetter
@@ -184,6 +184,7 @@ def floatformat(text, arg=-1):
     units = len(tupl[1])
     units += -tupl[2] if m else tupl[2]
     prec = abs(p) + units + 1
+    prec = max(getcontext().prec, prec)
 
     # Avoid conversion to scientific notation by accessing `sign`, `digits`,
     # and `exponent` from Decimal.as_tuple() directly.

tests/template_tests/filter_tests/test_floatformat.py
@@ -113,6 +113,10 @@ class FunctionTests(SimpleTestCase):
         )
         self.assertEqual(floatformat("0.00", 0), "0")
         self.assertEqual(floatformat(Decimal("0.00"), 0), "0")
+        self.assertEqual(floatformat("0.0000", 2), "0.00")
+        self.assertEqual(floatformat(Decimal("0.0000"), 2), "0.00")
+        self.assertEqual(floatformat("0.000000", 4), "0.0000")
+        self.assertEqual(floatformat(Decimal("0.000000"), 4), "0.0000")
 
     def test_negative_zero_values(self):
         tests = [