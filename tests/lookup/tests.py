varshithkonakanchi/django

commit 3c4878903ae2ef6532da6709ab0ebd19fbe6cc3f

Fixed #34015 -- Allowed filtering by transforms on relation fields.

tests/lookup/tests.py
@@ -786,10 +786,16 @@ class LookupTests(TestCase):
 
     def test_relation_nested_lookup_error(self):
         # An invalid nested lookup on a related field raises a useful error.
-        msg = "Related Field got invalid lookup: editor"
+        msg = (
+            "Unsupported lookup 'editor' for ForeignKey or join on the field not "
+            "permitted."
+        )
         with self.assertRaisesMessage(FieldError, msg):
             Article.objects.filter(author__editor__name="James")
-        msg = "Related Field got invalid lookup: foo"
+        msg = (
+            "Unsupported lookup 'foo' for ForeignKey or join on the field not "
+            "permitted."
+        )
         with self.assertRaisesMessage(FieldError, msg):
             Tag.objects.filter(articles__foo="bar")
 

tests/queries/tests.py
@@ -7,12 +7,13 @@ from threading import Lock
 
 from django.core.exceptions import EmptyResultSet, FieldError
 from django.db import DEFAULT_DB_ALIAS, connection
-from django.db.models import Count, Exists, F, Max, OuterRef, Q
+from django.db.models import CharField, Count, Exists, F, Max, OuterRef, Q
 from django.db.models.expressions import RawSQL
+from django.db.models.functions import ExtractYear, Length, LTrim
 from django.db.models.sql.constants import LOUTER
 from django.db.models.sql.where import AND, OR, NothingNode, WhereNode
 from django.test import SimpleTestCase, TestCase, skipUnlessDBFeature
-from django.test.utils import CaptureQueriesContext, ignore_warnings
+from django.test.utils import CaptureQueriesContext, ignore_warnings, register_lookup
 from django.utils.deprecation import RemovedInDjango50Warning
 
 from .models import (
@@ -390,6 +391,33 @@ class Queries1Tests(TestCase):
         self.assertIn("OUTER JOIN", str(qs.query))
         qs = qs.order_by("id")
         self.assertNotIn("OUTER JOIN", str(qs.query))
+
+    def test_filter_by_related_field_transform(self):
+        extra_old = ExtraInfo.objects.create(
+            info="extra 12",
+            date=DateTimePK.objects.create(date=datetime.datetime(2020, 12, 10)),
+        )
+        ExtraInfo.objects.create(info="extra 11", date=DateTimePK.objects.create())
+        a5 = Author.objects.create(name="a5", num=5005, extra=extra_old)
+
+        fk_field = ExtraInfo._meta.get_field("date")
+        with register_lookup(fk_field, ExtractYear):
+            self.assertSequenceEqual(
+                ExtraInfo.objects.filter(date__year=2020),
+                [extra_old],
+            )
+            self.assertSequenceEqual(
+                Author.objects.filter(extra__date__year=2020), [a5]
+            )
+
+    def test_filter_by_related_field_nested_transforms(self):
+        extra = ExtraInfo.objects.create(info=" extra")
+        a5 = Author.objects.create(name="a5", num=5005, extra=extra)
+        info_field = ExtraInfo._meta.get_field("info")
+        with register_lookup(info_field, Length), register_lookup(CharField, LTrim):
+            self.assertSequenceEqual(
+                Author.objects.filter(extra__info__ltrim__length=5), [a5]
+            )
 
     def test_get_clears_ordering(self):
         """

django/db/models/sql/query.py
@@ -1278,10 +1278,6 @@ class Query(BaseExpression):
         # supports both transform and lookup for the name.
         lookup_class = lhs.get_lookup(lookup_name)
         if not lookup_class:
-            if lhs.field.is_relation:
-                raise FieldError(
-                    "Related Field got invalid lookup: {}".format(lookup_name)
-                )
             # A lookup wasn't found. Try to interpret the name as a transform
             # and do an Exact lookup against it.
             lhs = self.try_transform(lhs, lookup_name)
@@ -1450,12 +1446,6 @@ class Query(BaseExpression):
             can_reuse.update(join_list)
 
         if join_info.final_field.is_relation:
-            # No support for transforms for relational fields
-            num_lookups = len(lookups)
-            if num_lookups > 1:
-                raise FieldError(
-                    "Related Field got invalid lookup: {}".format(lookups[0])
-                )
             if len(targets) == 1:
                 col = self._get_col(targets[0], join_info.final_field, alias)
             else:

tests/queries/models.py
@@ -1,6 +1,8 @@
 """
 Various complex queries that have been problematic in the past.
 """
+import datetime
+
 from django.db import models
 from django.db.models.functions import Now
 
@@ -64,7 +66,7 @@ class Annotation(models.Model):
 
 
 class DateTimePK(models.Model):
-    date = models.DateTimeField(primary_key=True, auto_now_add=True)
+    date = models.DateTimeField(primary_key=True, default=datetime.datetime.now)
 
 
 class ExtraInfo(models.Model):

commit 0e7db92c5249db2e52572eee2afe61986e2d6579

Fixed #34443 -- Fixed filtering by transforms on reverse relations.
Regression in 3c4878903ae2ef6532da6709ab0ebd19fbe6cc3f.

django/db/models/fields/reverse_related.py
@@ -114,6 +114,12 @@ class ForeignObjectRel(FieldCacheMixin):
     def get_lookup(self, lookup_name):
         return self.field.get_lookup(lookup_name)
 
+    def get_lookups(self):
+        return self.field.get_lookups()
+
+    def get_transform(self, name):
+        return self.field.get_transform(name)
+
     def get_internal_type(self):
         return self.field.get_internal_type()
 

tests/lookup/tests.py
@@ -19,7 +19,7 @@ from django.db.models import (
     Value,
     When,
 )
-from django.db.models.functions import Cast, Length, Substr
+from django.db.models.functions import Abs, Cast, Length, Substr
 from django.db.models.lookups import (
     Exact,
     GreaterThan,
@@ -837,6 +837,31 @@ class LookupTests(TestCase):
         )
         with self.assertRaisesMessage(FieldError, msg):
             Tag.objects.filter(articles__foo="bar")
+
+    def test_unsupported_lookup_reverse_foreign_key(self):
+        msg = (
+            "Unsupported lookup 'title' for ManyToOneRel or join on the field not "
+            "permitted."
+        )
+        with self.assertRaisesMessage(FieldError, msg):
+            Author.objects.filter(article__title="Article 1")
+
+    def test_unsupported_lookup_reverse_foreign_key_custom_lookups(self):
+        msg = (
+            "Unsupported lookup 'abspl' for ManyToOneRel or join on the field not "
+            "permitted, perhaps you meant abspk?"
+        )
+        fk_field = Article._meta.get_field("author")
+        with self.assertRaisesMessage(FieldError, msg):
+            with register_lookup(fk_field, Abs, lookup_name="abspk"):
+                Author.objects.filter(article__abspl=2)
+
+    def test_filter_by_reverse_related_field_transform(self):
+        fk_field = Article._meta.get_field("author")
+        with register_lookup(fk_field, Abs):
+            self.assertSequenceEqual(
+                Author.objects.filter(article__abs=self.a1.pk), [self.au1]
+            )
 
     def test_regex(self):
         # Create some articles with a bit more interesting headlines for