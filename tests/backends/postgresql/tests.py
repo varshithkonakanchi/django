varshithkonakanchi/django

commit 9176ffc126d6e12dfd54378f90cd93f7e4dc16ef

Refs #33308 -- Modernized database wrapper in the PostgreSQL backend.
- Used connection.info instead of connection.get_parameter_status() and
  connection.server_info which don't exist in psycopg 3.
- Set encoding using the client_encoding connection parameter instead
  of connection.set_client_encoding() that doesn't exist in psycopg 3.
- Used the dbname connection parameter instead of deprecated
  alias - database.

django/db/backends/postgresql/base.py
@@ -186,16 +186,16 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                     self.ops.max_name_length(),
                 )
             )
-        conn_params = {}
+        conn_params = {"client_encoding": "UTF8"}
         if settings_dict["NAME"]:
             conn_params = {
-                "database": settings_dict["NAME"],
+                "dbname": settings_dict["NAME"],
                 **settings_dict["OPTIONS"],
             }
         elif settings_dict["NAME"] is None:
             # Connect to the default 'postgres' db.
             settings_dict.get("OPTIONS", {}).pop("service", None)
-            conn_params = {"database": "postgres", **settings_dict["OPTIONS"]}
+            conn_params = {"dbname": "postgres", **settings_dict["OPTIONS"]}
         else:
             conn_params = {**settings_dict["OPTIONS"]}
 
@@ -239,7 +239,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     def ensure_timezone(self):
         if self.connection is None:
             return False
-        conn_timezone_name = self.connection.get_parameter_status("TimeZone")
+        conn_timezone_name = self.connection.info.parameter_status("TimeZone")
         timezone_name = self.timezone_name
         if timezone_name and conn_timezone_name != timezone_name:
             with self.connection.cursor() as cursor:
@@ -249,7 +249,6 @@ class DatabaseWrapper(BaseDatabaseWrapper):
 
     def init_connection_state(self):
         super().init_connection_state()
-        self.connection.set_client_encoding("UTF8")
 
         timezone_changed = self.ensure_timezone()
         if timezone_changed:
@@ -365,7 +364,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     @cached_property
     def pg_version(self):
         with self.temporary_connection():
-            return self.connection.server_version
+            return self.connection.info.server_version
 
     def make_debug_cursor(self, cursor):
         return CursorDebugWrapper(cursor, self)

tests/backends/postgresql/tests.py
@@ -164,7 +164,7 @@ class Tests(TestCase):
         settings["NAME"] = None
         settings["OPTIONS"] = {"service": "django_test"}
         params = DatabaseWrapper(settings).get_connection_params()
-        self.assertEqual(params["database"], "postgres")
+        self.assertEqual(params["dbname"], "postgres")
         self.assertNotIn("service", params)
 
     def test_connect_and_rollback(self):

commit a9d3544719afd514eef9404382d07351a61c6814

Fixed #34470 -- Enforced UTF-8 encoding on PostgreSQL.
Regression in 9176ffc126d6e12dfd54378f90cd93f7e4dc16ef.

django/db/backends/postgresql/base.py
@@ -208,7 +208,6 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                     self.ops.max_name_length(),
                 )
             )
-        conn_params = {"client_encoding": "UTF8"}
         if settings_dict["NAME"]:
             conn_params = {
                 "dbname": settings_dict["NAME"],
@@ -220,6 +219,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
             conn_params = {"dbname": "postgres", **settings_dict["OPTIONS"]}
         else:
             conn_params = {**settings_dict["OPTIONS"]}
+        conn_params["client_encoding"] = "UTF8"
 
         conn_params.pop("assume_role", None)
         conn_params.pop("isolation_level", None)

tests/backends/postgresql/tests.py
@@ -323,6 +323,18 @@ class Tests(TestCase):
         finally:
             new_connection.close()
 
+    def test_client_encoding_utf8_enforce(self):
+        new_connection = connection.copy()
+        new_connection.settings_dict["OPTIONS"]["client_encoding"] = "iso-8859-2"
+        try:
+            new_connection.connect()
+            if is_psycopg3:
+                self.assertEqual(new_connection.connection.info.encoding, "utf-8")
+            else:
+                self.assertEqual(new_connection.connection.encoding, "UTF8")
+        finally:
+            new_connection.close()
+
     def _select(self, val):
         with connection.cursor() as cursor:
             cursor.execute("SELECT %s::text[]", (val,))