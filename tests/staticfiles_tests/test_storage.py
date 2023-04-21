varshithkonakanchi/django

commit 574093bd21d2eac53a5fbfdd5b08ece01c833a68

Refs #32319 -- Changed HashedFilesMixin to use named groups in patterns.

django/contrib/staticfiles/storage.py
@@ -42,15 +42,21 @@ class StaticFilesStorage(FileSystemStorage):
 
 
 class HashedFilesMixin:
-    default_template = """url("%s")"""
+    default_template = """url("%(url)s")"""
     max_post_process_passes = 5
     patterns = (
         ("*.css", (
-            r"""(url\(['"]{0,1}\s*(.*?)["']{0,1}\))""",
-            (r"""(@import\s*["']\s*(.*?)["'])""", """@import url("%s")"""),
+            r"""(?P<matched>url\(['"]{0,1}\s*(?P<url>.*?)["']{0,1}\))""",
+            (
+                r"""(?P<matched>@import\s*["']\s*(?P<url>.*?)["'])""",
+                """@import url("%(url)s")""",
+            ),
         )),
         ('*.js', (
-            (r'(?m)^(//# (?-i:sourceMappingURL)=(.*))$', '//# sourceMappingURL=%s'),
+            (
+                r'(?P<matched>)^(//# (?-i:sourceMappingURL)=(?P<url>.*))$',
+                '//# sourceMappingURL=%(url)s',
+            ),
         )),
     )
     keep_intermediate_files = True
@@ -163,7 +169,9 @@ class HashedFilesMixin:
             This requires figuring out which files the matched URL resolves
             to and calling the url() method of the storage.
             """
-            matched, url = matchobj.groups()
+            matches = matchobj.groupdict()
+            matched = matches['matched']
+            url = matches['url']
 
             # Ignore absolute/protocol-relative and data-uri URLs.
             if re.match(r'^[a-z]+:', url):
@@ -199,7 +207,8 @@ class HashedFilesMixin:
                 transformed_url += ('?#' if '?#' in url else '#') + fragment
 
             # Return the hashed version to the file
-            return template % unquote(transformed_url)
+            matches['url'] = unquote(transformed_url)
+            return template % matches
 
         return converter
 

tests/staticfiles_tests/storage.py
@@ -84,7 +84,10 @@ class ExtraPatternsStorage(ManifestStaticFilesStorage):
     patterns = tuple(ManifestStaticFilesStorage.patterns) + (
         (
             "*.js", (
-                (r"""(url\(['"]{0,1}\s*(.*?)["']{0,1}\))""", 'JS_URL("%s")'),
+                (
+                    r"""(?P<matched>url\(['"]{0,1}\s*(?P<url>.*?)["']{0,1}\))""",
+                    'JS_URL("%(url)s")',
+                ),
             ),
         ),
     )

commit 120fcbf7e8ab170572c911b350077407e3e782ab

Fixed #34496 -- Fixed handling source maps with data URI in ManifestStaticFilesStorage.
Regression in 574093bd21d2eac53a5fbfdd5b08ece01c833a68.

django/contrib/staticfiles/storage.py
@@ -86,7 +86,7 @@ class HashedFilesMixin:
                 ),
                 (
                     (
-                        r"(?m)(?P<matched>)^(/\*#[ \t]"
+                        r"(?m)^(?P<matched>/\*#[ \t]"
                         r"(?-i:sourceMappingURL)=(?P<url>.*)[ \t]*\*/)$"
                     ),
                     "/*# sourceMappingURL=%(url)s */",
@@ -97,7 +97,7 @@ class HashedFilesMixin:
             "*.js",
             (
                 (
-                    r"(?m)(?P<matched>)^(//# (?-i:sourceMappingURL)=(?P<url>.*))$",
+                    r"(?m)^(?P<matched>//# (?-i:sourceMappingURL)=(?P<url>.*))$",
                     "//# sourceMappingURL=%(url)s",
                 ),
             ),

tests/staticfiles_tests/project/documents/cached/source_map_data_uri.css
@@ -1,0 +1,2 @@
+* {outline: 1px solid red;}
+/*# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv*/

tests/staticfiles_tests/project/documents/cached/source_map_data_uri.js
@@ -1,0 +1,2 @@
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv
+let a_variable = 1;

tests/staticfiles_tests/test_storage.py
@@ -269,6 +269,18 @@ class TestHashedFiles:
             )
         self.assertPostCondition()
 
+    def test_css_source_map_data_uri(self):
+        relpath = self.hashed_file_path("cached/source_map_data_uri.css")
+        self.assertEqual(relpath, "cached/source_map_data_uri.3166be10260d.css")
+        with storage.staticfiles_storage.open(relpath) as relfile:
+            content = relfile.read()
+            source_map_data_uri = (
+                b"/*# sourceMappingURL=data:application/json;charset=utf8;base64,"
+                b"eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv*/"
+            )
+            self.assertIn(source_map_data_uri, content)
+        self.assertPostCondition()
+
     def test_js_source_map(self):
         relpath = self.hashed_file_path("cached/source_map.js")
         self.assertEqual(relpath, "cached/source_map.cd45b8534a87.js")
@@ -305,6 +317,18 @@ class TestHashedFiles:
                 b"//# sourceMappingURL=source_map.js.99914b932bd3.map",
                 content,
             )
+        self.assertPostCondition()
+
+    def test_js_source_map_data_uri(self):
+        relpath = self.hashed_file_path("cached/source_map_data_uri.js")
+        self.assertEqual(relpath, "cached/source_map_data_uri.a68d23cbf6dd.js")
+        with storage.staticfiles_storage.open(relpath) as relfile:
+            content = relfile.read()
+            source_map_data_uri = (
+                b"//# sourceMappingURL=data:application/json;charset=utf8;base64,"
+                b"eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv"
+            )
+            self.assertIn(source_map_data_uri, content)
         self.assertPostCondition()
 
     @override_settings(