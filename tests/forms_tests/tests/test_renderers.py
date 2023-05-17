varshithkonakanchi/django

commit 8591cc08060abf2b853ffd40a6b414d93f0a84ef

Refs #32339 -- Deprecated transitional form renderers.

tests/forms_tests/tests/test_renderers.py
@@ -3,11 +3,14 @@ import unittest
 
 from django.forms.renderers import (
     BaseRenderer,
+    DjangoDivFormRenderer,
     DjangoTemplates,
     Jinja2,
+    Jinja2DivFormRenderer,
     TemplatesSetting,
 )
 from django.test import SimpleTestCase
+from django.utils.deprecation import RemovedInDjango60Warning
 
 try:
     import jinja2
@@ -53,3 +56,21 @@ class Jinja2Tests(SharedTests, SimpleTestCase):
 
 class TemplatesSettingTests(SharedTests, SimpleTestCase):
     renderer = TemplatesSetting
+
+
+class DeprecationTests(SimpleTestCase):
+    def test_django_div_renderer_warning(self):
+        msg = (
+            "The DjangoDivFormRenderer transitional form renderer is deprecated. Use "
+            "DjangoTemplates instead."
+        )
+        with self.assertRaisesMessage(RemovedInDjango60Warning, msg):
+            DjangoDivFormRenderer()
+
+    def test_jinja2_div_renderer_warning(self):
+        msg = (
+            "The Jinja2DivFormRenderer transitional form renderer is deprecated. Use "
+            "Jinja2 instead."
+        )
+        with self.assertRaisesMessage(RemovedInDjango60Warning, msg):
+            Jinja2DivFormRenderer()

commit 86052e824925e23d15b36839b478aaeba7fa269a

Refs #32339 -- Fixed super() call in deprecated renderers.
Missing function call `()` leads to:

TypeError: descriptor '__init__' of 'super' object needs an argument

Regression in 8591cc08060abf2b853ffd40a6b414d93f0a84ef.

tests/forms_tests/tests/test_renderers.py
@@ -9,7 +9,7 @@ from django.forms.renderers import (
     Jinja2DivFormRenderer,
     TemplatesSetting,
 )
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, ignore_warnings
 from django.utils.deprecation import RemovedInDjango60Warning
 
 try:
@@ -74,3 +74,11 @@ class DeprecationTests(SimpleTestCase):
         )
         with self.assertRaisesMessage(RemovedInDjango60Warning, msg):
             Jinja2DivFormRenderer()
+
+    @ignore_warnings(category=RemovedInDjango60Warning)
+    def test_deprecation_renderers_can_be_instantiated(self):
+        tests = [DjangoDivFormRenderer, Jinja2DivFormRenderer]
+        for cls in tests:
+            with self.subTest(renderer_class=cls):
+                renderer = cls()
+                self.assertIsInstance(renderer, cls)