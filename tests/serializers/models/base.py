varshithkonakanchi/django

commit dbfdb965afabbcfc806fd06c76e0239b04e8320d

Fixed #33937 -- Optimized serialization of related m2m fields without natural keys.

django/core/serializers/python.py
@@ -70,14 +70,20 @@ class Serializer(base.Serializer):
                 def m2m_value(value):
                     return value.natural_key()
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).iterator()
+
             else:
 
                 def m2m_value(value):
                     return self._value_from_field(value, value._meta.pk)
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).only("pk").iterator()
+
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,
-                getattr(obj, field.name).iterator(),
+                queryset_iterator(obj, field),
             )
             self._current[field.name] = [m2m_value(related) for related in m2m_iter]
 

django/core/serializers/xml_serializer.py
@@ -146,14 +146,20 @@ class Serializer(base.Serializer):
                         self.xml.endElement("natural")
                     self.xml.endElement("object")
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).iterator()
+
             else:
 
                 def handle_m2m(value):
                     self.xml.addQuickElement("object", attrs={"pk": str(value.pk)})
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).only("pk").iterator()
+
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,
-                getattr(obj, field.name).iterator(),
+                queryset_iterator(obj, field),
             )
             for relobj in m2m_iter:
                 handle_m2m(relobj)

tests/serializers/tests.py
@@ -410,6 +410,33 @@ class SerializersTestBase:
         self.assertEqual(self._get_field_values(child_data, "parent_m2m"), [])
         self.assertEqual(self._get_field_values(child_data, "parent_data"), [])
 
+    def test_serialize_only_pk(self):
+        with self.assertNumQueries(5) as ctx:
+            serializers.serialize(
+                self.serializer_name,
+                Article.objects.all(),
+                use_natural_foreign_keys=False,
+            )
+
+        categories_sql = ctx[1]["sql"]
+        self.assertNotIn(connection.ops.quote_name("meta_data_id"), categories_sql)
+        meta_data_sql = ctx[2]["sql"]
+        self.assertNotIn(connection.ops.quote_name("kind"), meta_data_sql)
+
+    def test_serialize_no_only_pk_with_natural_keys(self):
+        with self.assertNumQueries(5) as ctx:
+            serializers.serialize(
+                self.serializer_name,
+                Article.objects.all(),
+                use_natural_foreign_keys=True,
+            )
+
+        categories_sql = ctx[1]["sql"]
+        self.assertNotIn(connection.ops.quote_name("meta_data_id"), categories_sql)
+        # CategoryMetaData has natural_key().
+        meta_data_sql = ctx[2]["sql"]
+        self.assertIn(connection.ops.quote_name("kind"), meta_data_sql)
+
 
 class SerializerAPITests(SimpleTestCase):
     def test_stream_class(self):

AUTHORS
@@ -616,6 +616,7 @@ answer newbie questions, and generally made Django that much better:
     Mario Gonzalez <gonzalemario@gmail.com>
     Mariusz Felisiak <felisiak.mariusz@gmail.com>
     Mark Biggers <biggers@utsl.com>
+    Mark Evans <mark@meltdownlabs.com>
     Mark Gensler <mark.gensler@protonmail.com>
     mark@junklight.com
     Mark Lavin <markdlavin@gmail.com>

commit 67413cc2ae308b8106789f52fa5829ffa58bd2ba

Fixed #34620 -- Fixed serialization crash on m2m fields without natural keys when base querysets use select_related().
Regression in dbfdb965afabbcfc806fd06c76e0239b04e8320d.

Thanks Martin Svoboda for the report.

django/core/serializers/python.py
@@ -79,7 +79,9 @@ class Serializer(base.Serializer):
                     return self._value_from_field(value, value._meta.pk)
 
                 def queryset_iterator(obj, field):
-                    return getattr(obj, field.name).only("pk").iterator()
+                    return (
+                        getattr(obj, field.name).select_related().only("pk").iterator()
+                    )
 
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,

django/core/serializers/xml_serializer.py
@@ -155,7 +155,9 @@ class Serializer(base.Serializer):
                     self.xml.addQuickElement("object", attrs={"pk": str(value.pk)})
 
                 def queryset_iterator(obj, field):
-                    return getattr(obj, field.name).only("pk").iterator()
+                    return (
+                        getattr(obj, field.name).select_related().only("pk").iterator()
+                    )
 
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,

tests/serializers/models/base.py
@@ -53,12 +53,24 @@ class Author(models.Model):
         return self.name
 
 
+class TopicManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().select_related("category")
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, models.CASCADE, null=True)
+    objects = TopicManager()
+
+
 class Article(models.Model):
     author = models.ForeignKey(Author, models.CASCADE)
     headline = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
     categories = models.ManyToManyField(Category)
     meta_data = models.ManyToManyField(CategoryMetaData)
+    topics = models.ManyToManyField(Topic)
 
     class Meta:
         ordering = ("pub_date",)

tests/serializers/test_json.py
@@ -38,7 +38,8 @@ class JsonSerializerTestCase(SerializersTestBase, TestCase):
       %(first_category_pk)s,
       %(second_category_pk)s
     ],
-    "meta_data": []
+    "meta_data": [],
+    "topics": []
   }
 }
 ]

tests/serializers/test_jsonl.py
@@ -27,7 +27,8 @@ class JsonlSerializerTestCase(SerializersTestBase, TestCase):
         '"headline": "Poker has no place on ESPN",'
         '"pub_date": "2006-06-16T11:00:00",'
         '"categories": [%(first_category_pk)s,%(second_category_pk)s],'
-        '"meta_data": []}}\n'
+        '"meta_data": [],'
+        '"topics": []}}\n'
     )
 
     @staticmethod

tests/serializers/test_xml.py
@@ -26,6 +26,7 @@ class XmlSerializerTestCase(SerializersTestBase, TestCase):
     <field name="pub_date" type="DateTimeField">2006-06-16T11:00:00</field>
     <field name="categories" rel="ManyToManyRel" to="serializers.category"><object pk="%(first_category_pk)s"></object><object pk="%(second_category_pk)s"></object></field>
     <field name="meta_data" rel="ManyToManyRel" to="serializers.categorymetadata"></field>
+    <field name="topics" rel="ManyToManyRel" to="serializers.topic"></field>
   </object>
 </django-objects>"""  # NOQA
 

tests/serializers/test_yaml.py
@@ -113,6 +113,7 @@ class YamlSerializerTestCase(SerializersTestBase, TestCase):
         )
         + """
     meta_data: []
+    topics: []
 """
     )
 

tests/serializers/tests.py
@@ -277,14 +277,14 @@ class SerializersTestBase:
     def test_serialize_prefetch_related_m2m(self):
         # One query for the Article table and one for each prefetched m2m
         # field.
-        with self.assertNumQueries(3):
+        with self.assertNumQueries(4):
             serializers.serialize(
                 self.serializer_name,
-                Article.objects.prefetch_related("categories", "meta_data"),
+                Article.objects.prefetch_related("categories", "meta_data", "topics"),
             )
-        # One query for the Article table, and two m2m queries for each
+        # One query for the Article table, and three m2m queries for each
         # article.
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(7):
             serializers.serialize(self.serializer_name, Article.objects.all())
 
     def test_serialize_with_null_pk(self):
@@ -409,7 +409,7 @@ class SerializersTestBase:
         self.assertEqual(self._get_field_values(child_data, "parent_data"), [])
 
     def test_serialize_only_pk(self):
-        with self.assertNumQueries(5) as ctx:
+        with self.assertNumQueries(7) as ctx:
             serializers.serialize(
                 self.serializer_name,
                 Article.objects.all(),
@@ -420,9 +420,11 @@ class SerializersTestBase:
         self.assertNotIn(connection.ops.quote_name("meta_data_id"), categories_sql)
         meta_data_sql = ctx[2]["sql"]
         self.assertNotIn(connection.ops.quote_name("kind"), meta_data_sql)
+        topics_data_sql = ctx[3]["sql"]
+        self.assertNotIn(connection.ops.quote_name("category_id"), topics_data_sql)
 
     def test_serialize_no_only_pk_with_natural_keys(self):
-        with self.assertNumQueries(5) as ctx:
+        with self.assertNumQueries(7) as ctx:
             serializers.serialize(
                 self.serializer_name,
                 Article.objects.all(),
@@ -434,6 +436,8 @@ class SerializersTestBase:
         # CategoryMetaData has natural_key().
         meta_data_sql = ctx[2]["sql"]
         self.assertIn(connection.ops.quote_name("kind"), meta_data_sql)
+        topics_data_sql = ctx[3]["sql"]
+        self.assertNotIn(connection.ops.quote_name("category_id"), topics_data_sql)
 
 
 class SerializerAPITests(SimpleTestCase):