varshithkonakanchi/django

commit 2d9c3b2a2e0bb7a898cd85f55b2007e31e10dd60

Refs #34730 -- Made Message importable from django.contrib.messages.

tests/messages_tests/tests.py
@@ -1,8 +1,7 @@
 from unittest import mock
 
-from django.contrib.messages import constants
+from django.contrib.messages import Message, constants
 from django.contrib.messages.storage import base
-from django.contrib.messages.storage.base import Message
 from django.test import SimpleTestCase, override_settings
 
 

tests/messages_tests/base.py
@@ -1,8 +1,7 @@
-from django.contrib.messages import constants, get_level, set_level
+from django.contrib.messages import Message, constants, get_level, set_level
 from django.contrib.messages.api import MessageFailure
 from django.contrib.messages.constants import DEFAULT_LEVELS
 from django.contrib.messages.storage import default_storage
-from django.contrib.messages.storage.base import Message
 from django.http import HttpRequest, HttpResponse
 from django.test import modify_settings, override_settings
 from django.urls import reverse

django/contrib/messages/__init__.py
@@ -1,2 +1,3 @@
 from django.contrib.messages.api import *  # NOQA
 from django.contrib.messages.constants import *  # NOQA
+from django.contrib.messages.storage.base import Message  # NOQA

tests/messages_tests/test_cookie.py
@@ -3,8 +3,7 @@ import random
 from unittest import TestCase
 
 from django.conf import settings
-from django.contrib.messages import constants
-from django.contrib.messages.storage.base import Message
+from django.contrib.messages import Message, constants
 from django.contrib.messages.storage.cookie import (
     CookieStorage,
     MessageDecoder,

tests/messages_tests/test_session.py
@@ -1,5 +1,4 @@
-from django.contrib.messages import constants
-from django.contrib.messages.storage.base import Message
+from django.contrib.messages import Message, constants
 from django.contrib.messages.storage.session import SessionStorage
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest

commit d33fe8923e56572b3aa5fce080e13defe9071b7c

Fixed #34923 -- Avoided initializing LEVEL_TAGS when importing django.contrib.messages.
Regression in 2d9c3b2a2e0bb7a898cd85f55b2007e31e10dd60.

Thanks James Gillard for the report.

django/contrib/messages/apps.py
@@ -2,12 +2,13 @@ from django.apps import AppConfig
 from django.contrib.messages.storage import base
 from django.contrib.messages.utils import get_level_tags
 from django.core.signals import setting_changed
+from django.utils.functional import SimpleLazyObject
 from django.utils.translation import gettext_lazy as _
 
 
 def update_level_tags(setting, **kwargs):
     if setting == "MESSAGE_TAGS":
-        base.LEVEL_TAGS = get_level_tags()
+        base.LEVEL_TAGS = SimpleLazyObject(get_level_tags)
 
 
 class MessagesConfig(AppConfig):

django/contrib/messages/storage/base.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.contrib.messages import constants, utils
+from django.utils.functional import SimpleLazyObject
 
-LEVEL_TAGS = utils.get_level_tags()
+LEVEL_TAGS = SimpleLazyObject(utils.get_level_tags)
 
 
 class Message:

tests/messages_tests/tests.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+from django.conf import settings
 from django.contrib.messages import Message, add_message, constants
 from django.contrib.messages.storage import base
 from django.contrib.messages.test import MessagesTestMixin
@@ -62,6 +63,26 @@ class TestLevelTags(SimpleTestCase):
     @override_settings(MESSAGE_TAGS=message_tags)
     def test_override_settings_level_tags(self):
         self.assertEqual(base.LEVEL_TAGS, self.message_tags)
+
+    def test_lazy(self):
+        # Don't use @override_settings to avoid calling the setting_changed
+        # signal.
+        old_message_tags = getattr(settings, "MESSAGE_TAGS", None)
+        settings.MESSAGE_TAGS = {constants.ERROR: "bad"}
+        try:
+            self.assertEqual(base.LEVEL_TAGS[constants.ERROR], "bad")
+        finally:
+            if old_message_tags is None:
+                del settings.MESSAGE_TAGS
+            else:
+                settings.MESSAGE_TAGS = old_message_tags
+
+    @override_settings(MESSAGE_TAGS=message_tags)
+    def test_override_settings_lazy(self):
+        # The update_level_tags handler has been called at least once before
+        # running this code when using @override_settings.
+        settings.MESSAGE_TAGS = {constants.ERROR: "very-bad"}
+        self.assertEqual(base.LEVEL_TAGS[constants.ERROR], "very-bad")
 
 
 class FakeResponse: