varshithkonakanchi/django

commit 830b0f0d84f6c782e0c53a1daa7668cbe2f61e1e

Fixed #32442 -- Used converters on returning fields from INSERT statements.

django/db/models/sql/compiler.py
@@ -1405,6 +1405,7 @@ class SQLInsertCompiler(SQLCompiler):
             returning_fields and len(self.query.objs) != 1 and
             not self.connection.features.can_return_rows_from_bulk_insert
         )
+        opts = self.query.get_meta()
         self.returning_fields = returning_fields
         with self.connection.cursor() as cursor:
             for sql, params in self.as_sql():
@@ -1412,13 +1413,21 @@ class SQLInsertCompiler(SQLCompiler):
             if not self.returning_fields:
                 return []
             if self.connection.features.can_return_rows_from_bulk_insert and len(self.query.objs) > 1:
-                return self.connection.ops.fetch_returned_insert_rows(cursor)
-            if self.connection.features.can_return_columns_from_insert:
+                rows = self.connection.ops.fetch_returned_insert_rows(cursor)
+            elif self.connection.features.can_return_columns_from_insert:
                 assert len(self.query.objs) == 1
-                return [self.connection.ops.fetch_returned_insert_columns(cursor, self.returning_params)]
-            return [(self.connection.ops.last_insert_id(
-                cursor, self.query.get_meta().db_table, self.query.get_meta().pk.column
-            ),)]
+                rows = [self.connection.ops.fetch_returned_insert_columns(
+                    cursor, self.returning_params,
+                )]
+            else:
+                rows = [(self.connection.ops.last_insert_id(
+                    cursor, opts.db_table, opts.pk.column,
+                ),)]
+        cols = [field.get_col(opts.db_table) for field in self.returning_fields]
+        converters = self.get_converters(cols)
+        if converters:
+            rows = list(self.apply_converters(rows, converters))
+        return rows
 
 
 class SQLDeleteCompiler(SQLCompiler):

tests/custom_pk/models.py
@@ -7,7 +7,7 @@ this behavior by explicitly adding ``primary_key=True`` to a field.
 
 from django.db import models
 
-from .fields import MyAutoField
+from .fields import MyAutoField, MyWrapperField
 
 
 class Employee(models.Model):
@@ -31,8 +31,12 @@ class Business(models.Model):
 
 
 class Bar(models.Model):
-    id = MyAutoField(primary_key=True, db_index=True)
+    id = MyWrapperField(primary_key=True, db_index=True)
 
 
 class Foo(models.Model):
     bar = models.ForeignKey(Bar, models.CASCADE)
+
+
+class CustomAutoFieldModel(models.Model):
+    id = MyAutoField(primary_key=True)

tests/custom_pk/fields.py
@@ -20,7 +20,7 @@ class MyWrapper:
         return self.value == other
 
 
-class MyAutoField(models.CharField):
+class MyWrapperField(models.CharField):
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 10
@@ -58,3 +58,15 @@ class MyAutoField(models.CharField):
         if isinstance(value, MyWrapper):
             return str(value)
         return value
+
+
+class MyAutoField(models.BigAutoField):
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return None
+        return MyWrapper(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        return int(value)

tests/custom_pk/tests.py
@@ -1,7 +1,8 @@
 from django.db import IntegrityError, transaction
-from django.test import TestCase, skipIfDBFeature
+from django.test import TestCase, skipIfDBFeature, skipUnlessDBFeature
 
-from .models import Bar, Business, Employee, Foo
+from .fields import MyWrapper
+from .models import Bar, Business, CustomAutoFieldModel, Employee, Foo
 
 
 class BasicCustomPKTests(TestCase):
@@ -230,3 +231,13 @@ class CustomPKTests(TestCase):
         with self.assertRaises(IntegrityError):
             with transaction.atomic():
                 Employee.objects.create(first_name="Tom", last_name="Smith")
+
+    def test_auto_field_subclass_create(self):
+        obj = CustomAutoFieldModel.objects.create()
+        self.assertIsInstance(obj.id, MyWrapper)
+
+    @skipUnlessDBFeature('can_return_rows_from_bulk_insert')
+    def test_auto_field_subclass_bulk_create(self):
+        obj = CustomAutoFieldModel()
+        CustomAutoFieldModel.objects.bulk_create([obj])
+        self.assertIsInstance(obj.id, MyWrapper)

commit c7271429015e5c9a943339bed8d77f94fa483f64

Fixed #35024 -- Fixed model instance creation crash on GeneratedField.output_field with backend converters.
Regression in 830b0f0d84f6c782e0c53a1daa7668cbe2f61e1e.

This is a long standing issue, however it caused a crash of
GeneratedFields for all output fields that have backend-specific
converters when the RETURNING clause is not supported
(MySQL and SQLite < 3.35).
That's why severity was exacerbated.

django/db/models/sql/compiler.py
@@ -1819,6 +1819,7 @@ class SQLInsertCompiler(SQLCompiler):
         )
         opts = self.query.get_meta()
         self.returning_fields = returning_fields
+        cols = []
         with self.connection.cursor() as cursor:
             for sql, params in self.as_sql():
                 cursor.execute(sql, params)
@@ -1829,6 +1830,7 @@ class SQLInsertCompiler(SQLCompiler):
                 and len(self.query.objs) > 1
             ):
                 rows = self.connection.ops.fetch_returned_insert_rows(cursor)
+                cols = [field.get_col(opts.db_table) for field in self.returning_fields]
             elif self.connection.features.can_return_columns_from_insert:
                 assert len(self.query.objs) == 1
                 rows = [
@@ -1837,7 +1839,9 @@ class SQLInsertCompiler(SQLCompiler):
                         self.returning_params,
                     )
                 ]
+                cols = [field.get_col(opts.db_table) for field in self.returning_fields]
             else:
+                cols = [opts.pk.get_col(opts.db_table)]
                 rows = [
                     (
                         self.connection.ops.last_insert_id(
@@ -1847,7 +1851,6 @@ class SQLInsertCompiler(SQLCompiler):
                         ),
                     )
                 ]
-        cols = [field.get_col(opts.db_table) for field in self.returning_fields]
         converters = self.get_converters(cols)
         if converters:
             rows = list(self.apply_converters(rows, converters))

tests/model_fields/models.py
@@ -482,6 +482,18 @@ class UUIDGrandchild(UUIDChild):
     pass
 
 
+class GeneratedModelFieldWithConverters(models.Model):
+    field = models.UUIDField()
+    field_copy = models.GeneratedField(
+        expression=F("field"),
+        output_field=models.UUIDField(),
+        db_persist=True,
+    )
+
+    class Meta:
+        required_db_features = {"supports_stored_generated_columns"}
+
+
 class GeneratedModel(models.Model):
     a = models.IntegerField()
     b = models.IntegerField()

tests/model_fields/test_generatedfield.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.apps import apps
 from django.db import IntegrityError, connection
 from django.db.models import (
@@ -14,6 +16,7 @@ from django.test.utils import isolate_apps
 
 from .models import (
     GeneratedModel,
+    GeneratedModelFieldWithConverters,
     GeneratedModelNull,
     GeneratedModelNullVirtual,
     GeneratedModelOutputFieldDbCollation,
@@ -265,6 +268,11 @@ class StoredGeneratedFieldTests(GeneratedFieldTestMixin, TestCase):
     nullable_model = GeneratedModelNull
     output_field_db_collation_model = GeneratedModelOutputFieldDbCollation
     params_model = GeneratedModelParams
+
+    def test_create_field_with_db_converters(self):
+        obj = GeneratedModelFieldWithConverters.objects.create(field=uuid.uuid4())
+        obj = self._refresh_if_needed(obj)
+        self.assertEqual(obj.field, obj.field_copy)
 
 
 @skipUnlessDBFeature("supports_virtual_generated_columns")