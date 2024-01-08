varshithkonakanchi/django

commit 7252d7b3d0ad5a9a354f5d4a5dd7b4dd23ed21b7

Fixed #35020 -- Fixed ModelAdmin.lookup_allowed() for non-autofield primary keys.
Thanks Joshua Goodwin for the report.

Regression in 2b6535691f2c07ce5edd02dae2b651b6dcec1e61.

django/contrib/admin/options.py
@@ -474,24 +474,24 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
                 # Lookups on nonexistent fields are ok, since they're ignored
                 # later.
                 break
-            if not prev_field or (
-                prev_field.is_relation
-                and field not in model._meta.parents.values()
-                and field is not model._meta.auto_field
-                and (
-                    model._meta.auto_field is None
-                    or part not in getattr(prev_field, "to_fields", [])
-                )
-            ):
-                relation_parts.append(part)
             if not getattr(field, "path_infos", None):
                 # This is not a relational field, so further parts
                 # must be transforms.
                 break
+            if (
+                not prev_field
+                or (field.is_relation and field not in model._meta.parents.values())
+                or (
+                    prev_field.is_relation
+                    and model._meta.auto_field is None
+                    and part not in getattr(prev_field, "to_fields", [])
+                )
+            ):
+                relation_parts.append(part)
             prev_field = field
             model = field.path_infos[-1].to_opts.model
 
-        if len(relation_parts) <= 1:
+        if not relation_parts:
             # Either a local field filter, or no fields at all.
             return True
         valid_lookups = {self.date_hierarchy}

tests/modeladmin/tests.py
@@ -163,6 +163,20 @@ class ModelAdminTests(TestCase):
         )
 
     @isolate_apps("modeladmin")
+    def test_lookup_allowed_non_autofield_primary_key(self):
+        class Country(models.Model):
+            id = models.CharField(max_length=2, primary_key=True)
+
+        class Place(models.Model):
+            country = models.ForeignKey(Country, models.CASCADE)
+
+        class PlaceAdmin(ModelAdmin):
+            list_filter = ["country"]
+
+        ma = PlaceAdmin(Place, self.site)
+        self.assertIs(ma.lookup_allowed("country__id__exact", "DE", request), True)
+
+    @isolate_apps("modeladmin")
     def test_lookup_allowed_foreign_primary(self):
         class Country(models.Model):
             name = models.CharField(max_length=256)

commit 1a13b622f9a21dff055e1a193f6fcff1d5ccc7a3

Fixed #35087 -- Reallowed filtering against foreign keys not listed in ModelAdmin.list_filters.
Regression in 7252d7b3d0ad5a9a354f5d4a5dd7b4dd23ed21b7.

django/contrib/admin/options.py
@@ -467,7 +467,8 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
 
         relation_parts = []
         prev_field = None
-        for part in lookup.split(LOOKUP_SEP):
+        parts = lookup.split(LOOKUP_SEP)
+        for part in parts:
             try:
                 field = model._meta.get_field(part)
             except FieldDoesNotExist:
@@ -491,7 +492,7 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
             prev_field = field
             model = field.path_infos[-1].to_opts.model
 
-        if not relation_parts:
+        if not relation_parts or len(parts) == 1:
             # Either a local field filter, or no fields at all.
             return True
         valid_lookups = {self.date_hierarchy}

tests/modeladmin/tests.py
@@ -163,6 +163,20 @@ class ModelAdminTests(TestCase):
         )
 
     @isolate_apps("modeladmin")
+    def test_lookup_allowed_for_local_fk_fields(self):
+        class Country(models.Model):
+            pass
+
+        class Place(models.Model):
+            country = models.ForeignKey(Country, models.CASCADE)
+
+        class PlaceAdmin(ModelAdmin):
+            pass
+
+        ma = PlaceAdmin(Place, self.site)
+        self.assertIs(ma.lookup_allowed("country", "1", request), True)
+
+    @isolate_apps("modeladmin")
     def test_lookup_allowed_non_autofield_primary_key(self):
         class Country(models.Model):
             id = models.CharField(max_length=2, primary_key=True)