varshithkonakanchi/django

commit 39eaf65eca27e90df4cf0f2f1f0d03e59da3ab68

Fixed #35270 -- Optimized model's Options._property_names.
co-authored-by: Nick Pope <nick@nickpope.me.uk>

django/db/models/options.py
@@ -1,6 +1,5 @@
 import bisect
 import copy
-import inspect
 from collections import defaultdict
 
 from django.apps import apps
@@ -969,11 +968,13 @@ class Options:
     @cached_property
     def _property_names(self):
         """Return a set of the names of the properties defined on the model."""
-        names = []
-        for name in dir(self.model):
-            attr = inspect.getattr_static(self.model, name)
-            if isinstance(attr, property):
-                names.append(name)
+        names = set()
+        for klass in self.model.__mro__:
+            names |= {
+                name
+                for name, value in klass.__dict__.items()
+                if isinstance(value, property)
+            }
         return frozenset(names)
 
     @cached_property

commit 93e7f4bfae39460b9890ae8fe1c5230c057efc98

Fixed #35301 -- Fixed Options._property_names for overriden properties.
Regression in 39eaf65eca27e90df4cf0f2f1f0d03e59da3ab68.

django/db/models/options.py
@@ -969,12 +969,14 @@ class Options:
     def _property_names(self):
         """Return a set of the names of the properties defined on the model."""
         names = set()
+        seen = set()
         for klass in self.model.__mro__:
             names |= {
                 name
                 for name, value in klass.__dict__.items()
-                if isinstance(value, property)
+                if isinstance(value, property) and name not in seen
             }
+            seen |= set(klass.__dict__)
         return frozenset(names)
 
     @cached_property

tests/invalid_models_tests/test_models.py
@@ -1343,6 +1343,17 @@ class OtherModelTests(SimpleTestCase):
             ],
         )
 
+    def test_inherited_overriden_property_no_clash(self):
+        class Cheese:
+            @property
+            def filling_id(self):
+                pass
+
+        class Sandwich(Cheese, models.Model):
+            filling = models.ForeignKey("self", models.CASCADE)
+
+        self.assertEqual(Sandwich.check(), [])
+
     def test_single_primary_key(self):
         class Model(models.Model):
             foo = models.IntegerField(primary_key=True)